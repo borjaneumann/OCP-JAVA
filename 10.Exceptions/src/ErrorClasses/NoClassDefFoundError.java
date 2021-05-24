6a410eb70740f2dfedc899d37f49758e5152bd92
Borja <borjaneumann@hotmail.com>
2021-05-24 22:47:28 +0200

    NoClassDefFoundError

---
+++ 10.Exceptions/src/ErrorClasses/NoClassDefFoundError.java
@@ -0,0 +1,6 @@
+package ErrorClasses;
+
+public class NoClassDefFoundError {
+    //Java cannot find the class at runtime. this means a library available when the code was compiled is not
+    // available when the code is executed.
+}
