5b69d18f63b98d95a2f98b0d4ef9eebe12b825b3
Borja <borjaneumann@hotmail.com>
2021-05-26 23:11:39 +0200

    TryAndCatch2

---
+++ 10.Exceptions/src/HandlingExceptions/TryAndCatch2.java
@@ -0,0 +1,31 @@
+package HandlingExceptions;
+
+import javax.management.RuntimeMBeanException;
+
+public class TryAndCatch2 {
+    }
+    public static void main(String[] args) {
+        void explore(){
+            try{
+                fall();
+                System.out.println("never get here");
+
+            }catch (RuntimeMBeanException e){
+                getUp();
+            }
+            seeAnimals();
+        }
+        void fall(){
+            throw new RuntimeException();
+        }
+    }
+
+    private static void seeAnimals() {
+    }
+
+    private static void getUp() {
+    }
+
+    private static void fall() {
+    }
+}
