83b36e4c1b59c4f8d9413f8f4db43863dcc6d5fd
Borja <borjaneumann@hotmail.com>
2021-05-27 22:58:19 +0200

    ifStatement

---
+++ 04.Making_decisions/src/IfStatement.java
@@ -0,0 +1,12 @@
+public class IfStatement {
+
+
+    public static void main(String[] args) {
+        int ticketsTaken = 5;
+        int patrons = 0;
+        if (ticketsTaken>1){
+            patrons++;
+            System.out.println(patrons);
+        }
+    }
+}
