8ec2b8f4cb9d1b156ceec6a611fb616896540edf
Borja <borjaneumann@hotmail.com>
2021-05-28 21:12:56 +0200

    if/else statement

---
+++ 04.Making_decisions/src/elseStatement.java
@@ -0,0 +1,10 @@
+public class elseStatement {
+    public static void main(String[] args) {
+        int hourOfTheDay = 7;
+        if (hourOfTheDay<12){
+            System.out.println("Good morning");
+        }else{
+            System.out.println("Good afternoon");
+        }
+    }
+}
