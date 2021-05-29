b092a463017b8d157e60fa1a790735daf52dc0e3
Borja <borjaneumann@hotmail.com>
2021-05-29 23:46:34 +0200

    WhileLoopsInfiniteLoops

---
+++ 04.Making_decisions/src/WhileLoopsInfiniteLoops.java
@@ -0,0 +1,9 @@
+public class WhileLoopsInfiniteLoops {
+    public static void main(String[] args) {
+        int a = 5;
+        int counter = 0;
+        while (a < 10) {
+            counter++;
+        }
+    }
+}
