a232aeb54ab0bbf922ee08f1faf9e05ba0d4c77c
Borja <borjaneumann@hotmail.com>
2021-05-31 22:17:17 +0200

    WhileLoops

---
+++ 04.Making_decisions/src/WhileLoops.java
@@ -0,0 +1,10 @@
+public class WhileLoops {
+    public static void main(String[] args) {
+        int counter = 0;
+        while(counter<5){  //Termination condition
+            counter++;
+            System.out.println(counter);
+        }
+
+    }
+}
