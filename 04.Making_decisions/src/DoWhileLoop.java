d1222e7e2ecad59e6a7027b3be1de9b0e93f2ea6
Borja <borjaneumann@hotmail.com>
2021-06-01 20:31:54 +0200

    DoWhileLoop

---
+++ 04.Making_decisions/src/DoWhileLoop.java
@@ -0,0 +1,13 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int llama = 16;
+
+        if (llama > 10){
+            do{
+                System.out.println("llama");
+                llama--;
+            }
+            while(llama>10);
+        }
+    }
+}
