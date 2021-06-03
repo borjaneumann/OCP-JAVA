5ebeb72fb4083ec1e6e8db4b1fc574796b23ef41
Borja <borjaneumann@hotmail.com>
2021-06-03 22:59:31 +0200

    update

--- 04.Making_decisions/src/PrintingElementsInReverse.java
+++ 04.Making_decisions/src/PrintingElementsInReverse.java
@@ -5,7 +5,10 @@ public class PrintingElementsInReverse {
             System.out.println(i);
         }
         String name = "Alexander";
-        for (int i = 0; i < name.length(); i++) {
+        for (int i = name.length() - 1; i >= 0; i--) {
+            System.out.println(name.charAt(i));
+        }
+        for (int i = 8 - 1; i >= 0; i--) {
             System.out.println(name.charAt(i));
         }
         System.out.println("Letter at index 5: " + name.charAt(5));
