060929795ab767eb1f70c2f0fd06a8e2aea2805c
Borja <borjaneumann@hotmail.com>
2021-06-09 22:44:17 +0200

    Stringmethods

---
+++ 05_CoreJavaApis/src/StringMethods.java
@@ -0,0 +1,10 @@
+public class StringMethods {
+    public static void main(String[] args) {
+        String word = "Joseph";
+
+        //length
+        System.out.println(word.length());
+        //chartAt(index 0)
+        System.out.println(word.charAt(5));
+    }
+}
