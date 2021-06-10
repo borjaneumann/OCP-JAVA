e9eb65f365b5a123842d92eede04366abc4e4191
Borja <borjaneumann@hotmail.com>
2021-06-10 22:33:24 +0200

    String indexOf

--- 05_CoreJavaApis/src/StringMethods.java
+++ 05_CoreJavaApis/src/StringMethods.java
@@ -1,10 +1,15 @@
 public class StringMethods {
     public static void main(String[] args) {
-        String word = "Joseph";
+        String word = "animals";
 
         //length
         System.out.println(word.length());
         //chartAt(index 0)
         System.out.println(word.charAt(5));
+        //indexOf()
+        System.out.println("IndexOf: " + word.indexOf("a"));
+        System.out.println("IndexOf: " + word.indexOf("al"));
+        System.out.println("IndexOf: " + word.indexOf("a", 4));
+        System.out.println("IndexOf: " + word.indexOf("al", 5));
     }
 }
