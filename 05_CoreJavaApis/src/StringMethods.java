28230a1694dcf0b2b04d69c1bc5574d2091f3d06
Borja <borjaneumann@hotmail.com>
2021-06-11 23:32:56 +0200

    ToUpperCase,ToLowerCase,Equals,EqualsIgnoreCase

--- 05_CoreJavaApis/src/StringMethods.java
+++ 05_CoreJavaApis/src/StringMethods.java
@@ -11,10 +11,22 @@ public class StringMethods {
         System.out.println("IndexOf: " + word.indexOf("al"));
         System.out.println("IndexOf: " + word.indexOf("a", 4));
         System.out.println("IndexOf: " + word.indexOf("al", 5));
+
         //Substring()
         System.out.println("Substring of animals: " + word.substring(3));
 //        System.out.println(word.substring(-3));//does not compile
         System.out.println("Substring of animals: " + word.substring(2,5));
+
+        //ToLowerCase() and toUpperCase()
+        System.out.println("ToUpperCase:" + word.toLowerCase());
+        System.out.println("ToLowerCase:" +word.toUpperCase());
+
+        //Equals
+        System.out.println("Equals (aminals) :" + word.equals("aminals"));
+        System.out.println("Equals (animals) :" + word.equals("aminals"));
+        //EqualsIgnoreCase
+        System.out.println("EqualsIgnoreCase (ANIMALS) :" + word.equalsIgnoreCase("ANIMALS"));
+
 
     }
 }
