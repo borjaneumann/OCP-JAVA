ef87334fce124a466f5f43adb0a42e64f133eefb
Borja <borjaneumann@hotmail.com>
2021-06-14 23:27:17 +0200

    _2_BinaryArithmeticOperators

---
+++ 03.Operators/src/_2_BinaryArithmeticOperators.java
@@ -0,0 +1,24 @@
+public class _2_BinaryArithmeticOperators {
+    public static void main(String[] args) {
+        int price = 2 * 5 + 3 * 4 -8;
+        System.out.println("price: " + price);
+
+        //Parenthesis
+        int price1 = 2 * ((5 + 3) * 4 - 8);
+        System.out.println("price1: " + price1);
+
+        //Division and Modulus
+        System.out.println(9 / 3);
+        System.out.println(9 % 3);
+        System.out.println(10 / 3);
+        System.out.println(10 % 3);
+        System.out.println(11 / 3);
+        System.out.println(11 % 3);
+        System.out.println(12 / 3);
+        System.out.println(12 % 3);
+
+
+
+
+    }
+}
