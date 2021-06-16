38da500362a3583311d1731edc8a65d525bc594a
Borja <borjaneumann@hotmail.com>
2021-06-16 21:10:07 +0200

    This reference

--- 08.Class_Design/src/ThisReference/ThisReference.java
+++ 08.Class_Design/src/ThisReference/ThisReference.java
@@ -1,4 +1,16 @@
 package ThisReference;
 
 public class ThisReference {
+    private String reference;
+    public void setReference(String reference){
+        reference = reference; // it can be solved with the magic word "this"
+        //this.reference = reference;
+    }
+
+    public static void main(String[] args) {
+        ThisReference tR = new ThisReference();
+        tR.setReference("PInk");
+        System.out.println(tR.reference);
+    }
+
 }
