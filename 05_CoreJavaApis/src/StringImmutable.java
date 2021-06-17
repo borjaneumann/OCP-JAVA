8c9dc9c55b3a22599d5a561f71448056df70bedd
Borja <borjaneumann@hotmail.com>
2021-06-17 22:30:44 +0200

    StringImmutable

---
+++ 05_CoreJavaApis/src/StringImmutable.java
@@ -0,0 +1,11 @@
+public class StringImmutable {
+    public static void main(String[] args) {
+    String a = "abc";
+    String b = a.toUpperCase();
+    b = b.replace("B","2").replace( 'C', '3');
+        System.out.println("a =" + a);
+        System.out.println("b =" + b);
+    }
+
+}
+
