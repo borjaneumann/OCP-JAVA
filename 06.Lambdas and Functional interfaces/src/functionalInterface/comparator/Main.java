3d9db4449382b582371c8c361993f8435cef6d6c
Borja <borjaneumann@hotmail.com>
2021-06-23 21:51:53 +0200

    functional interface Comparator.1

---
+++ 06.Lambdas and Functional interfaces/src/functionalInterface/comparator/Main.java
@@ -0,0 +1,15 @@
+package functionalInterface.comparator;
+
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) {
+        String nameA = "Anthony";
+        String nameB = "Robert";
+        Comparator<Integer> ints = (i1,i2) -> i2-i1;
+        System.out.println(compareInt(ints));
+    }
+    private static int compareInt(Comparator<Integer> comparator){
+        return comparator.compare(40,32);
+    }
+}
