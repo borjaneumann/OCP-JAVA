b92f247add0e2250f664808cf3f58937fe2e9639
Borja <borjaneumann@hotmail.com>
2021-06-30 23:23:02 +0200

    CreatingArrayOfPrimitives

---
+++ 05_CoreJavaApis/src/understandingJavaArrays/CreatingArrayOfPrimitives.java
@@ -0,0 +1,24 @@
+package understandingJavaArrays;
+
+public class CreatingArrayOfPrimitives {
+    public static void main(String[] args) {
+        int[] numbers = new int[3];
+        int[] numbers1 = new int[]{ 3,5,6};
+        int[] numbers2 = {8,7,9};
+
+        //other  options
+        int[] numAnimals;
+        int [] numAnimals1;
+        int []numAnimals2;
+        int numAnimals3[];
+        int numAnimals4 [];
+
+        //multiples Arrays in declarations
+        int[] ids, types;
+        int ids1[], types2;
+    }
+
+
+
+
+}
