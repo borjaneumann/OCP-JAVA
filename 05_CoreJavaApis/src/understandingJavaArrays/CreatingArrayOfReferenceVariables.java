ca3e4d492ffb808860b26ecb3fd1625291efde8e
Borja <borjaneumann@hotmail.com>
2021-07-01 21:17:50 +0200

    CreatingArrayOfReferenceVariables

---
+++ 05_CoreJavaApis/src/understandingJavaArrays/CreatingArrayOfReferenceVariables.java
@@ -0,0 +1,16 @@
+package understandingJavaArrays;
+
+import java.util.Arrays;
+
+public class CreatingArrayOfReferenceVariables {
+    public static void main(String[] args) {
+        String[] bugs = {"cricket", "beetle", "ladybug"};
+        String[] alias = bugs;
+        System.out.println(bugs.equals(alias)); //Equals compare whether is an object
+        System.out.println("Reference on the heap: " + bugs.toString());
+        System.out.println("Printing object: " + Arrays.toString(bugs));
+
+        System.out.println("Bugs on position 0: " + bugs[0]);
+
+    }
+}
