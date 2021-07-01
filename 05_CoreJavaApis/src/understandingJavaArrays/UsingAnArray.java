da959a7f97dd5aea2025b5df968287dab60829b1
Borja <borjaneumann@hotmail.com>
2021-07-01 22:32:40 +0200

    UsingAnArray

---
+++ 05_CoreJavaApis/src/understandingJavaArrays/UsingAnArray.java
@@ -0,0 +1,17 @@
+package understandingJavaArrays;
+
+public class UsingAnArray {
+    public static void main(String[] args) {
+        String[] mammals = { "monkey", "chimp", "donkey"};
+        System.out.println("Length: " + mammals.length);
+        System.out.println("Mammals[0]: " + mammals[0]);
+        System.out.println("Mammals[1]: " + mammals[1]);
+        System.out.println("Mammals[2]: " + mammals[2]);
+        mammals[2] = "lizzard";
+        System.out.println("Mammals[2]: " + mammals[2]);
+
+        String[] birds = new String[6];
+        System.out.println("Length of birds: " + birds.length);
+
+    }
+}
