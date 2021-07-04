dcab7bead7f093017cbc83f4308a2320c34c6d27
Borja <borjaneumann@hotmail.com>
2021-07-04 22:19:22 +0200

    ArraysMethodComparing update

--- 05_CoreJavaApis/src/understandingJavaArrays/ArraysMethodComparing.java
+++ 05_CoreJavaApis/src/understandingJavaArrays/ArraysMethodComparing.java
@@ -6,6 +6,19 @@ public class ArraysMethodComparing {
     public static void main(String[] args) {
         int[] numbers1 = {1,2,3};
         int[] numbers2 = {1,2,4};
-        System.out.println(Arrays.compare(numbers1,numbers2));
+        int[] numbers3 = {1,2,3};
+        int[] numbers4 = {1,2,3,4};
+        int[] numbers5 = {5,2,3};
+
+        //--------------------------------------------
+        System.out.println("First Array smaller than second one: " + Arrays.compare(numbers1,
+                        numbers2));
+        System.out.println("Arrays are equal: " + Arrays.compare(numbers1,
+                numbers3));
+        System.out.println("First Array larger than second one: " + Arrays.compare(numbers5,
+                numbers3));
+
+
+
     }
 }
