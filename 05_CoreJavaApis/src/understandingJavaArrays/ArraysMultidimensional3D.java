35ce4b257de7286d979da366535365d4d53cbd54
Borja <borjaneumann@hotmail.com>
2021-07-07 23:18:52 +0200

    ArraysMultidimensional3D update

--- 05_CoreJavaApis/src/understandingJavaArrays/ArraysMultidimensional3D.java
+++ 05_CoreJavaApis/src/understandingJavaArrays/ArraysMultidimensional3D.java
@@ -20,9 +20,11 @@ public class ArraysMultidimensional3D {
         for (int i = 0; i < arr.length; i++){
             for (int j = 0; j < arr[i].length; j++) {
                 for (int k = 0; k < arr[i][j].length; k++) {
-                    System.out.println(arr[i][j][k]);
+                    System.out.print(arr[i][j][k]);
                 }
+                System.out.println();
             }
+            System.out.println();
         }
 
     }
