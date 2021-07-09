016c876e1505ed1deb0015b4e70e7edb08a64759
Borja <borjaneumann@hotmail.com>
2021-07-09 22:48:33 +0200

    UsingArrayListRemove

--- 05_CoreJavaApis/src/UnderstandingArrayList/UsingArrayListRemove.java
+++ 05_CoreJavaApis/src/UnderstandingArrayList/UsingArrayListRemove.java
@@ -1,4 +1,23 @@
 package UnderstandingArrayList;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UsingArrayListRemove {
+    public static void main(String[] args) {
+        //boolean remove (Object object)
+        //E remove (int index)
+
+        List<String> birds = new ArrayList<>();
+        birds.add("hawk");
+        birds.add("hawk");
+        System.out.println(birds.remove("cardinal"));//false
+        System.out.println(birds.remove("hawk"));//true
+        System.out.println(birds.remove(0));//removes hawk
+        System.out.println(birds);//[]
+
+    }
+
+
+
 }
