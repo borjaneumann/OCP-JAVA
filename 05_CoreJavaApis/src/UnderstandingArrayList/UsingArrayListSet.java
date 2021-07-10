32413fe49e955fc58ffc5e2dca5aa7832a50fbb8
Borja <borjaneumann@hotmail.com>
2021-07-10 21:19:51 +0200

    UsingArrayListIsEmptyAndSize

--- 05_CoreJavaApis/src/UnderstandingArrayList/UsingArrayListSet.java
+++ 05_CoreJavaApis/src/UnderstandingArrayList/UsingArrayListSet.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UsingArrayListSet {
+
+    //E set(int index, E newElement)
+
     public static void main(String[] args) {
         List<String> birds = new ArrayList<>();
         birds.add("blackbird");
