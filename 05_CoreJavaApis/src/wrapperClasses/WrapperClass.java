25c2885373ebccec2a3358093ba626b1bca859ca
Borja <borjaneumann@hotmail.com>
2021-07-11 20:57:36 +0200

    wrapperClasses update

--- 05_CoreJavaApis/src/wrapperClasses/WrapperClass.java
+++ 05_CoreJavaApis/src/wrapperClasses/WrapperClass.java
@@ -18,11 +18,19 @@ public class WrapperClass {
         int number = 3;
         Integer integer = number;
         System.out.println(integer);
-        System.out.println(integer);
+
+        boolean s = true;
+        Boolean t = s;
+        System.out.println(t.booleanValue());
+
+        boolean a = Boolean.parseBoolean("true");
+        Boolean booleanWrapper = Boolean.valueOf("true");
+        System.out.println("boolean: " + a);
+        System.out.println("BooleanWrapper: " + booleanWrapper);
 
         int primitive = Integer.parseInt("123");//convert string into an int
         Integer wrapper = Integer.valueOf("123"); // convert String into wrapper class
-        System.out.println("Primitive: " + primitive);
-        System.out.println("Wrapper: " + wrapper);
+        System.out.println("int: " + primitive);
+        System.out.println("IntegerWrapper: " + wrapper);
     }
 }
