46b5aefc5348ccbf560ecad3c8994e4961760c83
Borja <borjaneumann@hotmail.com>
2021-07-15 21:39:45 +0200

    UsingVarargsToCreateAList2

--- 05_CoreJavaApis/src/UnderstandingArrayList/UsingVarargsToCreateAList.java
+++ 05_CoreJavaApis/src/UnderstandingArrayList/UsingVarargsToCreateAList.java
@@ -1,5 +1,6 @@
 package UnderstandingArrayList;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,5 +8,9 @@ public class UsingVarargsToCreateAList {
     public static void main(String[] args) {
         List<String> list1 = Arrays.asList("one","two");
         List<String> list2 = List.of("one","two");
+
+        //if you want to change the number of elements
+        List<String> fixedSizeList =  Arrays.asList("a", "b", "c");
+        List<String> expandableList =  new ArrayList<>(fixedSizeList);
     }
 }
