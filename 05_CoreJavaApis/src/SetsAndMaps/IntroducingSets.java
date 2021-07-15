99a892a315975f838792e959ccbc342474fec8d2
Borja <borjaneumann@hotmail.com>
2021-07-15 22:30:54 +0200

    IntroducingSets

---
+++ 05_CoreJavaApis/src/SetsAndMaps/IntroducingSets.java
@@ -0,0 +1,18 @@
+package SetsAndMaps;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class IntroducingSets {
+    public static void main(String[] args) {
+        //collection of objects that cannot contain duplicates
+        //set isnt ordered so Set uses all Arraylist methods except for the ones with index
+
+        Set<Integer> set = new HashSet<>();
+        System.out.println(set.add(66));//true
+        System.out.println(set.add(66));//false
+        System.out.println(set.size());//1
+        set.remove(66);
+        System.out.println(set.isEmpty());//true
+    }
+}
