9eba91938916f96c0e093ab43f1003f9c7fcbdba
Borja <borjaneumann@hotmail.com>
2021-07-16 22:18:00 +0200

    IntroducingMaps

--- 05_CoreJavaApis/src/SetsAndMaps/IntroducingMaps.java
+++ 05_CoreJavaApis/src/SetsAndMaps/IntroducingMaps.java
@@ -1,7 +1,17 @@
 package SetsAndMaps;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class IntroducingMaps {
     public static void main(String[] args) {
-
+        //Most common implementation is Hashmap
+        Map<String, String> map = new HashMap<>();
+        map.put("koala", "bamboo");
+        String food = map.get("koala");// bamboo
+        String other = map.getOrDefault("ant", "leaf");//leaf
+        for (String key: map.keySet()){
+            System.out.println(key + " " + map.get(key)); //koala bamboo
+        }
     }
 }
