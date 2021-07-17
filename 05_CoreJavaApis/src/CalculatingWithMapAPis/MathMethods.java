098e56c833d21b311f53b096b352037c361aa81a
Borja <borjaneumann@hotmail.com>
2021-07-17 09:41:33 +0200

    MathMethods

---
+++ 05_CoreJavaApis/src/CalculatingWithMapAPis/MathMethods.java
@@ -0,0 +1,11 @@
+package CalculatingWithMapAPis;
+
+public class MathMethods {
+    public static void main(String[] args) {
+        //min() and max()
+        int first = Math.min(7,9); //7
+        int second = Math.max(7,9); //9
+        System.out.println("Min: " + first);
+        System.out.println("Max: " + second);
+    }
+}
--- 05_CoreJavaApis/src/SetsAndMaps/IntroducingMaps.java
+++ 05_CoreJavaApis/src/SetsAndMaps/IntroducingMaps.java
@@ -8,6 +8,7 @@ public class IntroducingMaps {
         //Most common implementation is Hashmap
         Map<String, String> map = new HashMap<>();
         map.put("koala", "bamboo");
+        map.put("monkey", "banana");
         String food = map.get("koala");// bamboo
         String other = map.getOrDefault("ant", "leaf");//leaf
         for (String key: map.keySet()){
