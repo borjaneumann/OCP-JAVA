4b694167ba4fbdace0faea18a9e55651029816a7
Borja <borjaneumann@hotmail.com>
2021-07-20 22:25:29 +0200

    Comments

---
+++ 01_Welcome_to_Java/src/UnderstandingJavaClassStructure/Comments.java
@@ -0,0 +1,25 @@
+package UnderstandingJavaClassStructure;
+
+public class Comments {
+    //comment until the end of line
+
+    /*Multiple
+    line comment
+     */
+
+    /**
+     * Javadoc multiple-line comment
+     * @author Lo Bano
+     */
+
+    /*
+    * // anteater
+     */
+    //bear
+    // // cat
+    // /* dog */
+    /* elephant */
+    /*
+    * /* ferret */
+     */
+}
