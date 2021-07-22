628bc524feb3751987c038d530d327c62ca25b38
Borja <borjaneumann@hotmail.com>
2021-07-22 22:04:04 +0200

    VariablesInParameterList

---
+++ 06.Lambdas and Functional interfaces/src/workingWithVariablesInLambdas/VariablesInParameterList.java
@@ -0,0 +1,23 @@
+package workingWithVariablesInLambdas;
+
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class VariablesInParameterList {
+    //these 3 parameters are interchangeable
+    Predicate<String> p = x -> true;
+//    Predicate<String> p = (var x) -> true;
+//    Predicate<String> p = (String s) -> true;
+
+    public void whatAmI(){
+        consume((var x) -> System.out.println(x),123);
+    }
+    public void consume(Consumer<Integer> c, int num){
+        c.accept(num);
+    }
+    public static void main(String[] args) {
+        VariablesInParameterList variablesInParameterList = new VariablesInParameterList();
+        variablesInParameterList.whatAmI();
+    }
+
+}
