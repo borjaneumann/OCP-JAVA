cf68f90b5f2226a003bb0efdd6b4cb88f26c730b
Borja <borjaneumann@hotmail.com>
2021-07-27 21:49:20 +0200

    ReturnType update

--- 07.MethodsAndEncapsulation/src/designingMethods/ReturnType.java
+++ 07.MethodsAndEncapsulation/src/designingMethods/ReturnType.java
@@ -9,5 +9,20 @@ public class ReturnType {
     public String int walk6(){}; //does not compile
     String walk7 (int a) { if (a==4) return "";}//does not compile
 
+    int integer(){
+        return 9;
+    }
+    int longMethod(){
+        return  9L; //doesn't compile
+    }
+    int integerExpanded(){
+        int temp = 9;
+        return temp;
+    }
+    int longMethodExpanded(){
+        int temp = 9L; //it doesn't compile
+        return temp;
+    }
+
 
 }
