56c1905fdde53374f434eeb158fdaafc7e575659
Borja <borjaneumann@hotmail.com>
2021-07-28 22:45:43 +0200

    OpcionalExceptionList

---
+++ 07.MethodsAndEncapsulation/src/designingMethods/OpcionalExceptionList.java
@@ -0,0 +1,7 @@
+package designingMethods;
+
+public class OpcionalExceptionList {
+    public void zeroExceptions(){}
+    public void oneException() throws IllegalArgumentException{}
+    public void twoExceptions() throws IllegalArgumentException, InterruptedException {}
+}
