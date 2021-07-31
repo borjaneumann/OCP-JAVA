dbc589c50f8d26dd139f6014c6ce0a251f4d68ab
Borja <borjaneumann@hotmail.com>
2021-07-31 09:52:22 +0200

    WorkingWithVarargsExamples

--- 07.MethodsAndEncapsulation/src/designingMethods/WorkingWithVarargs.java
+++ 07.MethodsAndEncapsulation/src/designingMethods/WorkingWithVarargs.java
@@ -5,4 +5,12 @@ public class WorkingWithVarargs {
     public void walk2 (int start, int...nums){}
     public void walk3(int...nums1, int start){} //doesn't compile
     public void walk4(int...start, int...nums){}//doesn't compile
+
+    public static void main(String[] args) {
+        WorkingWithVarargs wwv = new WorkingWithVarargs();
+        wwv.walk2(1);
+        wwv.walk2(1,2);
+        wwv.walk2(1,2,3);
+        wwv.walk2(1, new int[]{4,5});
+    }
 }
