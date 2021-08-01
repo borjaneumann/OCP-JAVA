6179fd08b83842d05b69b21be67398fc0064f5fc
Borja <borjaneumann@hotmail.com>
2021-08-01 13:18:39 +0200

    WorkingWithVarargsupdate

--- 07.MethodsAndEncapsulation/src/designingMethods/WorkingWithVarargs.java
+++ 07.MethodsAndEncapsulation/src/designingMethods/WorkingWithVarargs.java
@@ -3,8 +3,8 @@ package designingMethods;
 public class WorkingWithVarargs {
     public void  walk (int...nums){}
     public void walk2 (int start, int...nums){}
-    public void walk3(int...nums1, int start){} //doesn't compile
-    public void walk4(int...start, int...nums){}//doesn't compile
+//    public void walk3(int...nums1, int start){} //doesn't compile
+//    public void walk4(int...start, int...nums){}//doesn't compile
 
     public static void main(String[] args) {
         WorkingWithVarargs wwv = new WorkingWithVarargs();
@@ -13,4 +13,10 @@ public class WorkingWithVarargs {
         wwv.walk2(1,2,3);
         wwv.walk2(1, new int[]{4,5});
     }
+//    public static void run(int... nums) {
+//            System.out.println(nums[1]);
+//         }
+//         public static void main(String[] args) {
+//            run(11, 22);     // 22
+//         }
 }
