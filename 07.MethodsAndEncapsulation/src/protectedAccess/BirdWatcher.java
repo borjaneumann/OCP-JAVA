a37c39d1adfc7d16dabb5d47ac5f30e2d5180a97
Borja <borjaneumann@hotmail.com>
2021-08-05 22:17:26 +0200

    protected access part2

---
+++ 07.MethodsAndEncapsulation/src/protectedAccess/BirdWatcher.java
@@ -0,0 +1,9 @@
+package protectedAccess;
+
+public class BirdWatcher {
+    public void watchBird(){
+        Bird bird = new Bird();
+        bird.floatInWater();
+        System.out.println(bird.text);
+    }
+}
