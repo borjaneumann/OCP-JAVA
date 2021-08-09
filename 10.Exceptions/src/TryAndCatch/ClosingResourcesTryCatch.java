0a65519d87bd09a1d700776f38bbf27e3e8dd566
Borja <borjaneumann@hotmail.com>
2021-08-09 20:38:56 +0200

    ClosingResourcesTryCatch

---
+++ 10.Exceptions/src/TryAndCatch/ClosingResourcesTryCatch.java
@@ -0,0 +1,28 @@
+package TryAndCatch;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ClosingResourcesTryCatch {
+    public static void main(String[] args) {
+        ClosingResourcesTryCatch cr = new ClosingResourcesTryCatch();
+        cr.readFile("file");
+    }
+    public void readFile(String file) {
+        FileInputStream is = null;
+        try {
+            is = new FileInputStream("myfile.txt");
+            //read file data
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(is != null) {
+                try {
+                    is.close();
+                }catch (IOException e2) {
+                    e2.printStackTrace();
+                }
+            }
+        }
+    }
+}
