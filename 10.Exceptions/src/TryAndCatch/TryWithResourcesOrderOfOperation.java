f5d8ca9a0337c39bf23d5589ce5c1dd203e0d38e
Borja <borjaneumann@hotmail.com>
2021-08-10 22:15:00 +0200

    try with resources order of operation

---
+++ 10.Exceptions/src/TryAndCatch/TryWithResourcesOrderOfOperation.java
@@ -0,0 +1,23 @@
+package TryAndCatch;
+
+public class TryWithResourcesOrderOfOperation implements AutoCloseable{
+    // # Resources are closed after the try clause ends and before any catch/finally clauses.
+    // # Resources are closed in the reverse order from which they were created.
+
+    private final int num;
+    public TryWithResourcesOrderOfOperation(int num) { this.num = num; }
+    public void close() {
+        System.out.println("closing: " + num);
+    }
+
+    public static void main(String[] args) {
+        try (TryWithResourcesOrderOfOperation a1 = new TryWithResourcesOrderOfOperation(1);
+            TryWithResourcesOrderOfOperation a2 = new TryWithResourcesOrderOfOperation(2)) {
+            throw new RuntimeException();
+        } catch (Exception e) {
+            System.out.println("ex");
+        } finally {
+            System.out.println("finally");
+        }
+    }
+}
