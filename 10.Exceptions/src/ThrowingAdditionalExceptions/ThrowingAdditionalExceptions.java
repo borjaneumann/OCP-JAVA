8366a2c06e254f0b41361af3cfa941d438d1671c
Borja <borjaneumann@hotmail.com>
2021-08-11 22:53:40 +0200

    throwing additional exceptions

---
+++ 10.Exceptions/src/ThrowingAdditionalExceptions/ThrowingAdditionalExceptions.java
@@ -0,0 +1,33 @@
+package ThrowingAdditionalExceptions;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ThrowingAdditionalExceptions {
+    //What happens when an exception is thrown inside
+    //of a catch or finally block?
+
+    public static void main(String[] args) {
+        FileReader reader = null;
+        try {
+            reader = read();
+        } catch (IOException e) {
+            try {
+                if (reader != null) reader.close();
+            } catch (IOException inner) {
+            }
+        }
+    }
+    private static FileReader read() throws IOException {
+            return null;
+    }
+    public void exceptionHandlingExamExample () {
+        try {
+            throw new RuntimeException();
+        } catch (RuntimeException e) {
+            throw new RuntimeException();
+        } finally {
+//            throw new Exception();  // This one shows that only the last exception to be thrown matters
+        }
+    }
+}
