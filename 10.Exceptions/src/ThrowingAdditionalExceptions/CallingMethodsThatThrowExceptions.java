5e1728db026aeefabf36a3282c2ca432bc489a9c
Borja <borjaneumann@hotmail.com>
2021-08-12 21:25:54 +0200

    CallingMethodsThatThrowExceptions

---
+++ 10.Exceptions/src/ThrowingAdditionalExceptions/CallingMethodsThatThrowExceptions.java
@@ -0,0 +1,30 @@
+package ThrowingAdditionalExceptions;
+
+public class CallingMethodsThatThrowExceptions extends Exception{
+    // When you’re calling a method that throws an exception, the rules are the
+    //same as within a method.
+
+    public static class Bunny {
+//        public static void main(String[] args) {
+////            eatCarrot(); // does not compile
+//        }
+        private static void eatCarrot() throws CallingMethodsThatThrowExceptions {
+            System.out.println("Happy rabbit");
+        }
+        //This would work
+//        public static void main(String[] args) throws CallingMethodsThatThroughExceptions {
+//                //declare exception
+//            eatCarrot();
+//            }
+        //This also would work
+        public static void main(String[] args) {
+            try {
+                eatCarrot();
+            } catch (CallingMethodsThatThrowExceptions e) {
+                //handle exception
+                System.out.println("Sad rabbit");
+            }
+        }
+    }
+
+}
