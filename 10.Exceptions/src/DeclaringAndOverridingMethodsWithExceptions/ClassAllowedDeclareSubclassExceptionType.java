c5055f306d045a298a50805b9ff2b46eec0ab485
Borja <borjaneumann@hotmail.com>
2021-08-14 20:52:22 +0200

    DeclaringAndOverridingMethodsWithExceptions

---
+++ 10.Exceptions/src/DeclaringAndOverridingMethodsWithExceptions/ClassAllowedDeclareSubclassExceptionType.java
@@ -0,0 +1,11 @@
+package DeclaringAndOverridingMethodsWithExceptions;
+
+public class ClassAllowedDeclareSubclassExceptionType extends Exception {
+    class Hopper {
+        public void hop() throws Exception {}
+    }
+    class Bunny extends Hopper {
+//        public void hop() throws ClassAllowedDeclareSubclassExceptionType {}
+        public void hop() throws IllegalArgumentException {} //also works
+    }
+}
