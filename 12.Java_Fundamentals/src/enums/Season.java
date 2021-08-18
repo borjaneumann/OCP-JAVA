45ab0887dbd444ff22390356f3fa51a46bdb1593
Borja <borjaneumann@hotmail.com>
2021-08-18 22:22:19 +0200

    Enums

---
+++ 12.Java_Fundamentals/src/enums/Season.java
@@ -0,0 +1,19 @@
+package enums;
+
+public enum Season {
+    WINTER, SPRING, SUMMER, FALL;
+
+    public static void main(String[] args) {
+        Season s = Season.FALL;
+        Season t = Season.valueOf("SUMMER");
+        Season e = Season.valueOf("summer"); // Throws an exception at runtime
+
+        System.out.println(Season.SUMMER);
+        System.out.println(s== Season.SUMMER);
+        for (Season season : Season.values()) {
+            System.out.println(season.name() + " " + season.ordinal());
+        }
+    }
+//    public enum ExtendedSeason extends Season {} //Enums cannot be extended
+}
+
