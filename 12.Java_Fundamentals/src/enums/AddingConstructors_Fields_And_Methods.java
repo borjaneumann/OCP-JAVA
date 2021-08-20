4b578b3412cbfb22d14a5858a2de7531c1802276
Borja <borjaneumann@hotmail.com>
2021-08-20 22:05:30 +0200

    EnumsInSwitchStatements

---
+++ 12.Java_Fundamentals/src/enums/AddingConstructors_Fields_And_Methods.java
@@ -0,0 +1,13 @@
+package enums;
+
+public enum AddingConstructors_Fields_And_Methods {
+        WINTER("Low"), SPRING("Medium"), SUMMER("High"), FALL("Medium");
+
+        private  final String expectedVisitors;
+        AddingConstructors_Fields_And_Methods(String expectedVisitors) {
+            this.expectedVisitors = expectedVisitors;
+        }
+        public void printExpectedVisitors() {
+            System.out.println(expectedVisitors);
+    }
+}
