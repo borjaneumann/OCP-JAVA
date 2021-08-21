b97fa8238da908157ce09f9b4346a3f9533fa12c
Borja <borjaneumann@hotmail.com>
2021-08-21 22:27:16 +0200

    OnlyOne

---
+++ 12.Java_Fundamentals/src/enums/OnlyOne.java
@@ -0,0 +1,16 @@
+package enums;
+
+public enum OnlyOne {
+    ONCE(true);
+    private OnlyOne(boolean b) {
+        System.out.println("constructing,");
+    }
+}
+class PrintTheOne {
+    public static void main(String[] args) {
+        System.out.println("begin, ");
+        OnlyOne firstCall = OnlyOne.ONCE; //prints constructing
+        OnlyOne secondCall = OnlyOne.ONCE; //doesn't print anything
+        System.out.println("end");
+    }
+}
