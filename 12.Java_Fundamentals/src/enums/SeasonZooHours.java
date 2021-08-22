f24fa30397820aa93712a75e3f858966a1cb05b7
Borja <borjaneumann@hotmail.com>
2021-08-22 21:28:59 +0200

    SeasonZooHours

---
+++ 12.Java_Fundamentals/src/enums/SeasonZooHours.java
@@ -0,0 +1,25 @@
+package enums;
+
+public enum SeasonZooHours {
+    WINTER {
+        public String getHours() {
+            return "10am-3pm";
+        }
+    },
+    SPRING {
+        public String getHours() {
+            return "9am-5pm";
+        }
+    },
+    SUMMER {
+        public String getHours() {
+            return "9am-7pm";
+        }
+    },
+    FALL {
+        public String getHours() {
+            return "9am-5pm";
+        }
+    };
+    public abstract String getHours();
+}
