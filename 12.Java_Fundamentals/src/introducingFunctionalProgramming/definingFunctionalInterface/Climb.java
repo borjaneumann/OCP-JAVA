95ceebee5e9fc0a34fba06da398d8b06339969d5
Borja <borjaneumann@hotmail.com>
2021-08-24 21:15:45 +0200

    Climb

---
+++ 12.Java_Fundamentals/src/introducingFunctionalProgramming/definingFunctionalInterface/Climb.java
@@ -0,0 +1,8 @@
+package introducingFunctionalProgramming.definingFunctionalInterface;
+
+public interface Climb {
+    void reach(); //This is a functional interface as it has reach() as one abstract method.
+    default void fall() {}
+    static int getBackUp() { return 100;}
+    private static boolean checkHeight() {return true;}
+}
