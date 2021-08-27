432a04b305ab718096d6d5d2aa3688a132c63dca
Borja <borjaneumann@hotmail.com>
2021-08-27 08:05:43 +0200

    Tiger

---
+++ 12.Java_Fundamentals/src/introducingFunctionalProgramming/definingFunctionalInterface/Tiger.java
@@ -0,0 +1,12 @@
+package introFunctionalProgramming.definingFunctionalInterface;
+
+public class Tiger implements Sprint {
+    public void sprint(int speed) {
+        System.out.println("Animal is sprinting fast! " + speed);
+    }
+
+    public static void main(String[] args) {
+        Tiger tiger = new Tiger();
+        tiger.sprint(65);
+    }
+}
