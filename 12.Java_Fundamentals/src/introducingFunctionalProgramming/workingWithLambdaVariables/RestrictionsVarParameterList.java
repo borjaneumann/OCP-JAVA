f41bffe54d4a10561df8630f1f82aff399cb8307
Borja <borjaneumann@hotmail.com>
2021-08-29 06:44:00 +0200

    RestrictionsVarParameterList

---
+++ 12.Java_Fundamentals/src/introducingFunctionalProgramming/workingWithLambdaVariables/RestrictionsVarParameterList.java
@@ -0,0 +1,13 @@
+package introducingFunctionalProgramming.workingWithLambdaVariables;
+
+public class RestrictionsVarParameterList {
+
+//    (var num) -> 1;  //compiles
+//    var w -> 99
+//    (var a, var b) -> "Hello"  //compiles
+//    var a, Integer b) -> true
+//    (String x, var y, Integer z) -> true
+//    (var b, var k, var m) -> 3.14159  //compiles
+//    (var x, y) -> "goodbye"
+
+}
