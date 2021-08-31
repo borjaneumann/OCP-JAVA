b5f7aab0ba95ea9e6689726824a0a989927d9378
Borja Neumann <borjaneumann@hotmail.com>
2021-08-31 13:37:19 +0200

    LocalVariablesInsideLambdaBody

---
+++ 12.Java_Fundamentals/src/introducingFunctionalProgramming/workingWithLambdaVariables/LocalVariablesInsideLambdaBody.java
@@ -0,0 +1,19 @@
+package introducingFunctionalProgramming.workingWithLambdaVariables;
+
+//import java.util.function.Predicate;
+
+public class LocalVariablesInsideLambdaBody {
+//    (a,b) -> { int c = 0; return 5;}   //compile
+//    (a,b) -> { int a = 0; return 5;}   // does not compile, we are redeclaring a.
+
+    //There are three mistakes here
+//    public  void variables(int a) {
+//        int b = 1;
+//        Predicate<Integer> p1 =  a -> {
+//            int b = 0;
+//            int c = 0;
+//            return b == c;}
+//        }
+
+
+}
