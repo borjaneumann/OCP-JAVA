997dde37ab1c0964fa691aa96eeb5578ea8d0c6f
Borja Neumann <borjaneumann@hotmail.com>
2021-08-31 13:42:37 +0200

    VariablesReferencedFromLambdaBody

---
+++ 12.Java_Fundamentals/src/introducingFunctionalProgramming/workingWithLambdaVariables/VariablesReferencedFromLambdaBody.java
@@ -0,0 +1,22 @@
+package introducingFunctionalProgramming.workingWithLambdaVariables;
+
+//import java.util.function.Predicate;
+//
+//public class VariablesReferencedFromLambdaBody {
+//    private String color;
+//    public void caw(String name) {
+//        String volume = "loudly";
+//        Predicate<String> p = s ->
+//                (name + volume + color).length() == 10;
+//    }
+
+    //The version below will not compile. variables are redeclared.
+//    private String color2;
+//    public void caw2(String name2) {
+//        String volume2 = "loudly";
+//        color2 = "allowed";
+//        name2 = "not allowed";
+//        volume2 = "not allowed";
+//        Predicate<String> p = s -> (name2 + volume2 + color2 ).length() == 9; //Does not compile
+//    }
+//}
