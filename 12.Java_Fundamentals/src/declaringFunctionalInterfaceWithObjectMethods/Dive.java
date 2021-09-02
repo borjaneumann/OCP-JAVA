abe1d239f1854ee2ea9252fa5913e3c9aed84fd7
Borja Neumann <borjaneumann@hotmail.com>
2021-09-02 16:56:48 +0200

    Dive

---
+++ 12.Java_Fundamentals/src/declaringFunctionalInterfaceWithObjectMethods/Dive.java
@@ -0,0 +1,8 @@
+package introFunctionalProgramming.declaringFunctionalInterfaceWithObjectMethods;
+
+//public interface Dive {
+//    String toString();
+//    public boolean equals(Object o);
+//    public abstract int hashcode();
+//    public void dive(); //This method makes it a functional interface. The other ones do not count.
+//}
