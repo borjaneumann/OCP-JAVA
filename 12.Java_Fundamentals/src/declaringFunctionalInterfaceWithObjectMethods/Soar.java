115818e11638a728439a7b68aabdbc519c4da6ad
Borja Neumann <borjaneumann@hotmail.com>
2021-09-03 09:05:02 +0200

    Soar

---
+++ 12.Java_Fundamentals/src/declaringFunctionalInterfaceWithObjectMethods/Soar.java
@@ -0,0 +1,5 @@
+package declaringFunctionalInterfaceWithObjectMethods;
+
+public interface Soar {
+    abstract String toString(); //toString does not count so there is no asbtract method, thus no
+}
