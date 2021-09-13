c578ed69a95bcc17e1ed775b8290fb0ddfe583dc
Borja Neumann <borjaneumann@hotmail.com>
2021-09-13 16:03:44 +0200

    calling constructors

---
+++ 02.JavaBuildingBlocks/src/CallingConstructors/Chicken.java
@@ -0,0 +1,13 @@
+package CallingConstructors;
+
+//The purpose of a constructor is to initialize fields, although you can put
+// any code in there. Another way to initialize fields is to do so directly on
+// the line on which they’re declared. T
+
+public class Chicken {
+    int numEggs = 12; // initialize on line
+    String name;
+    public Chicken() {
+        name = "Duke"; // initialize in constructor
+    }
+}
