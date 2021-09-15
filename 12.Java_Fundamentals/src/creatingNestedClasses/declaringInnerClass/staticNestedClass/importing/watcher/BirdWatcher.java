96461a0fa7542bc461e86cae56a9c0a47a203858
Borja Neumann <borjaneumann@hotmail.com>
2021-09-15 08:49:09 +0200

    static nested class-watcher

---
+++ 12.Java_Fundamentals/src/creatingNestedClasses/declaringInnerClass/staticNestedClass/importing/watcher/BirdWatcher.java
@@ -0,0 +1,8 @@
+package creatingNestedClasses.declaringInnerClass.staticNestedClass.importing.watcher;
+
+import static creatingNestedClasses.declaringInnerClass.staticNestedClass.importing.bird.Toucan.Beak;
+//import  creatingNestedClasses.declaringInnerClass.StaticNestedClass.importing.bird.Toucan.Beak; Both imports are ok! static or regular...
+
+public class BirdWatcher {
+    Beak beak;
+}
