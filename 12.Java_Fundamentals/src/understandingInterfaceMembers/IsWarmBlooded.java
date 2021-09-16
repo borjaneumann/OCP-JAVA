1a3f8e152964722bcda60ce122c8b06e37453618
Borja Neumann <borjaneumann@hotmail.com>
2021-09-16 09:01:37 +0200

    Understanding Interface Members

---
+++ 12.Java_Fundamentals/src/understandingInterfaceMembers/IsWarmBlooded.java
@@ -0,0 +1,12 @@
+package understandingInterfaceMembers;
+
+//The class has the option of overriding the default method, but if it does not, then the
+//default implementation will be used
+
+public interface IsWarmBlooded {
+
+    default double getTemperature() {
+        return 10.0;
+    }
+    boolean hasScales();
+}
