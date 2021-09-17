ccb722a31a7b560cc52fcbbfdd879a6c74b3c57b
Borja Neumann <borjaneumann@hotmail.com>
2021-09-17 08:41:06 +0200

    Duplicate default methods

---
+++ 12.Java_Fundamentals/src/understandingInterfaceMembers/duplicateDefaultMethods/Cat.java
@@ -0,0 +1,14 @@
+package understandingInterfaceMembers.duplicateDefaultMethods;
+
+//public class Cat implements Walk, Run { //Does not compile, Java doesnt know which .getSpeed() method to implement.
+//    public static void main(String[] args) {
+//        System.out.println(new Cat().getSpeed());
+//    }
+//}
+
+public class Cat implements Walk, Run {
+    public int getSpeed() { return 1; } //solved by overriding
+    public static void main(String[] args) {
+        System.out.println(new Cat().getSpeed());
+    }
+}
