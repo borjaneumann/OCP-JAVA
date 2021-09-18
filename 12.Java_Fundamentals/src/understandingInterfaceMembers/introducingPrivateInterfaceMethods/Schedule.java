d3495678a8395d79089d251b3b0c64463eb3d0e9
Borja Neumann <borjaneumann@hotmail.com>
2021-09-18 22:56:36 +0200

    introducingPrivateInterfaceMethods

---
+++ 12.Java_Fundamentals/src/understandingInterfaceMembers/introducingPrivateInterfaceMethods/Schedule.java
@@ -0,0 +1,15 @@
+package understandingInterfaceMembers.introducingPrivateInterfaceMethods;
+
+public interface Schedule {
+    default void wakeup()           {checkTime(7);}
+    default void haveBreakfast()    {checkTime(9);}
+    default void haveLunch()        {checkTime(12);}
+    default void WorkOut()          {checkTime(18);}
+    private void checkTime(int hour) {
+        if (hour > 17) {
+            System.out.println("You're late!");
+        } else {
+            System.out.println("You have " + (17 - hour)+ " hours left to make the appointment");
+        }
+    }
+}
