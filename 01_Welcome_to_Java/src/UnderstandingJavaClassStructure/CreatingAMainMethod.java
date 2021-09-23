86cf6c020b4e4016bf037a13e7e0799ad1fa03d9
Borja Neumann <borjaneumann@hotmail.com>
2021-09-23 15:43:54 +0200

    Creating a method additional information

--- 01_Welcome_to_Java/src/UnderstandingJavaClassStructure/CreatingAMainMethod.java
+++ 01_Welcome_to_Java/src/UnderstandingJavaClassStructure/CreatingAMainMethod.java
@@ -3,7 +3,11 @@ package UnderstandingJavaClassStructure;
 public class CreatingAMainMethod {
 
     public static void main(String[] args) {
-
     }
+//    //The following is also allowed:
+//    public static void main(String args []) {
+//    }
+//    public static void main(String... args) {
+//    }
 
 }
