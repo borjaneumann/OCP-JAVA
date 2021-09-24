f7df6a5af2283140fcabb813f9fe3f4dc5dbc2fb
Borja Neumann <borjaneumann@hotmail.com>
2021-09-24 23:45:52 +0200

    introducing annotations

---
+++ 13.Annotations/src/_1_IntroducingAnnotations/ZooSchedule/Lion.java
@@ -0,0 +1,7 @@
+package purposeOfAnnotations.ZooSchedule;
+
+public class Lion {
+//    @ZooSchedule(hours={"9am", "5pm", "10pm"}) void feedLions() {
+//        System.out.println("Time to feed the lions!");
+//    }
+}
