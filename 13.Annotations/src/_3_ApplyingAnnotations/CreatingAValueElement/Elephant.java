c9b5af8a6c0aa8b78d27145874fe8f8b65365412
Borja Neumann <borjaneumann@hotmail.com>
2021-10-04 10:22:16 +0200

    Creating a value element

---
+++ 13.Annotations/src/_3_ApplyingAnnotations/CreatingAValueElement/Elephant.java
@@ -0,0 +1,7 @@
+package _3_ApplyingAnnotations.CreatingAValueElement;
+
+public abstract class Elephant {
+    @Injured("Legs") public void fallDown() {}
+    @Injured(value="Legs") public abstract int trip();
+    @Injured String injuries;
+}
