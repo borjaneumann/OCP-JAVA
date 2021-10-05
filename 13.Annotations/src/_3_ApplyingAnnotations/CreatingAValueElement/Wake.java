881c03ddd5aded06167fd434eb8a205033d54682
Borja Neumann <borjaneumann@hotmail.com>
2021-10-05 09:23:27 +0200

    Creating A Value Element 3

---
+++ 13.Annotations/src/_3_ApplyingAnnotations/CreatingAValueElement/Wake.java
@@ -0,0 +1,7 @@
+package _3_ApplyingAnnotations.CreatingAValueElement;
+
+ //the following annotation declarations cannot be used with a shorthand annotation:
+ // it does not include an element named value.
+public @interface Wake {
+    String hours();
+}
