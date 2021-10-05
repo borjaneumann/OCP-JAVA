74660261c5492dd4743a13688e6bd3274a4a557d
Borja Neumann <borjaneumann@hotmail.com>
2021-10-05 16:03:56 +0200

    This reference use 1

---
+++ 08.Class_Design/src/ThisReference/uses/CurrentClassInstanceVariable.java
@@ -0,0 +1,19 @@
+package ThisReference.uses;
+
+public class CurrentClassInstanceVariable {
+    String name;
+    String surname;
+
+    public CurrentClassInstanceVariable(String name, String surname){
+        this.name = name;
+        this.surname = surname;
+    }
+    void display(){
+        System.out.println(name + " " + surname);
+    }
+
+    public static void main(String[] args) {
+        CurrentClassInstanceVariable c = new CurrentClassInstanceVariable("John", "Smith");
+        c.display();
+    }
+}
