282bbf17df729b15f532bdbb6826697bbc859fea
Borja Neumann <borjaneumann@hotmail.com>
2021-10-05 16:04:59 +0200

    This reference use 2

---
+++ 08.Class_Design/src/ThisReference/uses/InvokeCurrentClassMethod.java
@@ -0,0 +1,15 @@
+package ThisReference.uses;
+
+public class InvokeCurrentClassMethod {
+    void m(){
+        System.out.println("Hello m");
+    }
+    void n(){
+        this.m();
+    }
+
+    public static void main(String[] args) {
+        InvokeCurrentClassMethod i = new InvokeCurrentClassMethod();
+        i.n();
+    }
+}
