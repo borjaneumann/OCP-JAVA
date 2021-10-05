30198c418a5120fbe477fbb029601975ab3346b9
Borja Neumann <borjaneumann@hotmail.com>
2021-10-05 16:41:39 +0200

    This reference use 2

---
+++ 08.Class_Design/src/ThisReference/uses/InvokeClassConstructor2.java
@@ -0,0 +1,19 @@
+package ThisReference.uses;
+
+//Chaining constructors
+//Calling parameterized constructor from default constructor:
+
+public class InvokeClassConstructor2 {
+    InvokeClassConstructor2(){
+        this(5);
+        System.out.println("hello Invoke Class Constructor");
+    }
+    InvokeClassConstructor2(int x){
+        System.out.println(x);
+    }
+
+    public static void main(String[] args) {
+        InvokeClassConstructor2 icc = new InvokeClassConstructor2();
+
+    }
+}
