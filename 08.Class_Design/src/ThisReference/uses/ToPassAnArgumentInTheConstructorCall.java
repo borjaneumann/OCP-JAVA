9b6e8849aad40e59523c72cc86a4e3161cee5fbb
Borja Neumann <borjaneumann@hotmail.com>
2021-10-06 12:28:51 +0200

    This reference 6

---
+++ 08.Class_Design/src/ThisReference/uses/ToPassAnArgumentInTheConstructorCall.java
@@ -0,0 +1,25 @@
+package ThisReference.uses;
+
+public class ToPassAnArgumentInTheConstructorCall {
+
+    //We can pass the this keyword in the constructor also. It is useful if we have to use one object in multiple classes.
+    A4 obj;
+
+    ToPassAnArgumentInTheConstructorCall (A4 obj){
+        this.obj=obj;
+    }
+    void display(){
+        System.out.println(obj.data);
+    }
+
+    public static void main(String[] args) {
+        A4 a = new A4();
+    }
+}
+class A4 {
+    int data = 10;
+    A4(){
+        ToPassAnArgumentInTheConstructorCall b = new ToPassAnArgumentInTheConstructorCall(this);
+        b.display();
+    }
+}
--- 08.Class_Design/src/ThisReference/uses/ToPassAnArgumentInTheMethod.java
+++ 08.Class_Design/src/ThisReference/uses/ToPassAnArgumentInTheMethod.java
@@ -4,6 +4,7 @@ public class ToPassAnArgumentInTheMethod {
     void m(ToPassAnArgumentInTheMethod obj){
         System.out.println("Method is invoked");
     }
+    //The this keyword can also be passed as an argument in the method. It is mainly used in the event handling.
     void p(){
         m(this);
     }
