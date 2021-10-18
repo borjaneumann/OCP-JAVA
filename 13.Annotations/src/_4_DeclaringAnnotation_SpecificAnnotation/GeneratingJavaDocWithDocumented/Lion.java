5013df94e5656ee09e69b31c8a0128c777af080f
Borja Neumann <borjaneumann@hotmail.com>
2021-10-18 08:57:13 +0200

    Generating JavaDoc With Documented 2

---
+++ 13.Annotations/src/_4_DeclaringAnnotation_SpecificAnnotation/GeneratingJavaDocWithDocumented/Lion.java
@@ -0,0 +1,8 @@
+package _4_DeclaringAnnotation_SpecificAnnotation.GeneratingJavaDocWithDocumented;
+
+//In this example, the @Hunter annotation would be published with the Lion
+//Javadoc information because it's marked with the @Documented annotation.
+
+@Hunter
+public @interface Lion {
+}
