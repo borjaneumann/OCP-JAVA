16fd3dea5eb83f4dfd0c131ba4fd3b67efc44ff3
Borja Neumann <borjaneumann@hotmail.com>
2021-10-23 20:57:00 +0200

    Limiting Usage With Target

---
+++ 13.Annotations/src/_4_DeclaringAnnotation_SpecificAnnotation/LimitingUsageWithTarget/RollerCoaster.java
@@ -0,0 +1,12 @@
+package _4_DeclaringAnnotation_SpecificAnnotation.LimitingUsageWithTarget;
+
+//@ZooAttraction class RollerCoaster {
+//    class Event {
+//        @ZooAttraction String rideTrain() { //compiles
+//            return (@ZooAttraction String) "Fun!";
+//        }
+//        @ZooAttraction Events(@ZooAttraction String description) // only @ZooAttraction Events compiles
+//            super();
+//    }
+//    @ZooAttraction int numPassengers;
+//}
