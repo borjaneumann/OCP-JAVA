d1f4c690c39ec0b0430c4f592db4ff3e713a2f55
Borja Neumann <borjaneumann@hotmail.com>
2021-10-28 09:22:38 +0200

    Storing Annotations With Retention

---
+++ 13.Annotations/src/_4_DeclaringAnnotation_SpecificAnnotation/StoringAnnotationsWithRetention/Swimmer.java
@@ -0,0 +1,9 @@
+package _4_DeclaringAnnotation_SpecificAnnotation.StoringAnnotationsWithRetention;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Swimmer {
+
+}
