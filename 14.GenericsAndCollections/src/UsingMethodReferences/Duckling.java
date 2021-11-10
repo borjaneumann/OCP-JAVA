2514c86bc65757af17f8a1ff501b0b3e496d991e
Borja Neumann <borjaneumann@hotmail.com>
2021-11-10 09:13:20 +0100

    Using Method references intro

--- 14.GenericsAndCollections/src/UsingMethodReferences/Duckling.java
+++ 14.GenericsAndCollections/src/UsingMethodReferences/Duckling.java
@@ -2,7 +2,8 @@ package UsingMethodReferences;
 
 public class Duckling {
     public static void makeSound(String sound) {
-        LearnToSpeak learner = s -> System.out.println(s);
+//        LearnToSpeak learner = s -> System.out.println(s);
+        LearnToSpeak learner = System.out::println;
         DuckHelper.teacher(sound, learner);
     }
 }
