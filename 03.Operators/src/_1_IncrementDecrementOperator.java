94efd83285e5a6ee60dd876bb756b2c43ee18c71
Borja Neumann <borjaneumann@hotmail.com>
2021-11-10 10:04:07 +0100

    Increment Decrement Operator update

--- 03.Operators/src/_1_IncrementDecrementOperator.java
+++ 03.Operators/src/_1_IncrementDecrementOperator.java
@@ -1,17 +1,22 @@
 public class _1_IncrementDecrementOperator {
     public static void main(String[] args) {
         int guests = 0;
-        System.out.println("Pre-increment: " + ++guests);
-        System.out.println(guests);
+        System.out.println("Original guests value: " + guests);
+        System.out.println("Guests with Pre-increment: " + ++guests);
+        System.out.println("Guests after Pre-increment: " + guests);
+        System.out.println();
 
         int employess = 0;
-        System.out.println("Post-increment: " + employess++);//the value is updated after is shown.
-        System.out.println(employess);
+        System.out.println("Original employees value: " + employess);
+        System.out.println("Employees with Post-increment: " + employess++);//the value is updated after is shown.
+        System.out.println("Employees after Post-increment: " + employess);
+        System.out.println();
 
-        int lion = 3;
-        int tiger = ++lion * 5 / lion--;
-        System.out.println("lion is: " + lion);
-        System.out.println("tiger is: " + tiger);
+        int lion = 4;
+        System.out.println("Original Lion value: " + lion);
+        int tiger = ++lion * 6 / lion--; //keep a good eye on this example. The second time lion is used the value has been already updated to 5.
+        System.out.println("Lion is: " + lion);
+        System.out.println("Tiger is: " + tiger);
 
     }
 
