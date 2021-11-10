751c06ba6394f83aadc05f165a73e651426525ab
Borja Neumann <borjaneumann@hotmail.com>
2021-11-10 09:31:24 +0100

    Calling Instance Methods On A Particular Obj 2

--- 14.GenericsAndCollections/src/UsingMethodReferences/_02_CallingInstanceMethodsOnAParticularObj/Example.java
+++ 14.GenericsAndCollections/src/UsingMethodReferences/_02_CallingInstanceMethodsOnAParticularObj/Example.java
@@ -1,6 +1,8 @@
 package UsingMethodReferences._02_CallingInstanceMethodsOnAParticularObj;
 
+import java.util.Random;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class Example {
 
@@ -8,6 +10,12 @@ public class Example {
         var str = "abc";
         Predicate<String> methodRef = str::startsWith;
         Predicate<String> lambda = s -> str.startsWith(s);
+
+        //A method reference doesn't have to take any parameters.
+        var random =  new Random();
+        Supplier<Integer> methodRef1 = random::nextInt;
+        Supplier<Integer> lambda1 = () -> random.nextInt();
+
     }
 
 
