fea9fcc7404e2ddae02ea1b01ac716c1e8e8a1bc
Borja Neumann <borjaneumann@hotmail.com>
2021-11-11 09:55:57 +0100

    Calling Instance Methods On A Parameter

---
+++ 14.GenericsAndCollections/src/UsingMethodReferences/_03_CallingInstanceMethodsOnAParameter/Example.java
@@ -0,0 +1,19 @@
+package UsingMethodReferences._03_CallingInstanceMethodsOnAParameter;
+
+
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+
+//we are going to call an instance method that doesn't take any
+//parameters
+public class Example {
+
+    Predicate<String> methodRef = String::isEmpty;
+    Predicate<String> lambda = s -> s.isEmpty();
+
+    BiPredicate<String, String> methodRef = String::startsWith;
+    BiPredicate<String, String> lambda = (s, p) -> s.startsWith(p);
+
+
+
+}
