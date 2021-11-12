1e6df8ea26bbad0a183888f105f3a45bbb9fca50
Borja Neumann <borjaneumann@hotmail.com>
2021-11-12 09:57:24 +0100

    Method references/Calling constructors 2

--- 14.GenericsAndCollections/src/UsingMethodReferences/_04_CallingConstructors/CallingConstructors.java
+++ 14.GenericsAndCollections/src/UsingMethodReferences/_04_CallingConstructors/CallingConstructors.java
@@ -2,11 +2,17 @@ package UsingMethodReferences._04_CallingConstructors;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class CallingConstructors {
 
     Supplier<List<String>> methodRef = ArrayList::new;
     Supplier<List<String>> lambda = () -> new ArrayList();
+
+    Function<Integer, List<String>> methodRef1 =
+            ArrayList::new;
+    Function<Integer, List<String>> lambda1 = x -> new
+            ArrayList(x);
 
 }
