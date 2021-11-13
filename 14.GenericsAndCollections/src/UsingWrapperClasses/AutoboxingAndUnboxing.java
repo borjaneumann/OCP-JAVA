846fff5c81faa4c91593a4bf7259dee400d17f4f
Borja <borjaneumann@hotmail.com>
2021-11-13 21:45:16 +0100

    Using Wrapper Classes

---
+++ 14.GenericsAndCollections/src/UsingWrapperClasses/AutoboxingAndUnboxing.java
@@ -0,0 +1,9 @@
+package UsingWrapperClasses;
+
+public class AutoboxingAndUnboxing {
+
+    //Spot autoboxing and unboxing
+    Integer pounds = 120;
+    Character letter = "robot".charAt(0);
+    char r = letter;
+}
