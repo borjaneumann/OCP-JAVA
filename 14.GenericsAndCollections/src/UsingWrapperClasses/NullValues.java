38e03c73c8e2b319e562a1a931dead14fa868ec7
Borja <borjaneumann@hotmail.com>
2021-11-14 14:41:23 +0100

    Autoboxing and Unboxing 2

--- 14.GenericsAndCollections/src/UsingWrapperClasses/NullValues.java
+++ 14.GenericsAndCollections/src/UsingWrapperClasses/NullValues.java
@@ -1,13 +1,22 @@
 package UsingWrapperClasses;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class NullValues {
 
     public static void main(String[] args) {
-        var heights = new ArrayList<Integer>();
-        heights.add(null);
-        int h = heights.get(0); // NullPointerException
+//        var heights = new ArrayList<Integer>();
+//        heights.add(null);
+//        int h = heights.get(0); // NullPointerException
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(Integer.valueOf(3));
+        numbers.add(Integer.valueOf(5));
+        numbers.remove(1);
+        numbers.remove(Integer.valueOf(5));
+        System.out.println(numbers);
     }
 
 }
