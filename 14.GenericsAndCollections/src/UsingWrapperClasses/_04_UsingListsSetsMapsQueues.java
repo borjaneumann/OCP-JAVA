4b8508f9da2c8d2635750f3595055e842679ecdc
Borja Neumann <borjaneumann@hotmail.com>
2021-11-15 09:25:56 +0100

    Using Lists Sets Maps Queues

--- 14.GenericsAndCollections/src/UsingWrapperClasses/_04_UsingListsSetsMapsQueues.java
+++ 14.GenericsAndCollections/src/UsingWrapperClasses/_04_UsingListsSetsMapsQueues.java
@@ -1,4 +1,26 @@
 package UsingWrapperClasses;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
+
 public class _04_UsingListsSetsMapsQueues {
+
+    //List: A list is an ordered collection of elements that allows duplicate entries. Elements in a list can be accessed by an int index.
+
+    //Set: A set is a collection that does not allow duplicate entries.
+
+    //Queue: A queue is a collection that orders its elements in a specific order for processing. A typical queue processes its elements in a first‐in, first‐
+    //out order, but other orderings are possible.
+
+    //Map: A map is a collection that maps keys to values, with no duplicate keys allowed. The elements in a map are key/value pairs.
+
+
+    //Some examples below
+    Collection<String> list = new ArrayList<>();
+    Collection<String> set = new HashSet<>();
+    Collection<String> queue = new LinkedList<>();
+
+
 }
