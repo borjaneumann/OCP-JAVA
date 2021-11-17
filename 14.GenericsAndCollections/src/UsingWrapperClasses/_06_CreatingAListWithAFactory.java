4080502f872a63d8b9ade2d70ee2d658ca81efed
Borja Neumann <borjaneumann@hotmail.com>
2021-11-17 09:47:52 +0100

    Creating a List with a factory

---
+++ 14.GenericsAndCollections/src/UsingWrapperClasses/_06_CreatingAListWithAFactory.java
@@ -0,0 +1,29 @@
+package UsingWrapperClasses;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class _06_CreatingAListWithAFactory {
+
+    public static void main(String[] args) {
+
+        //Arrays.asList(varargs) --> Returns fixed size list backed by an array
+        //List.of(varargs) --> Returns immutable
+        //List.copyOf(collection) --> Returns immutable list with copy of original collection's values
+
+        String[] array = new String[]{"a", "b", "c"};
+        List<String> asList = Arrays.asList(array); // [a, b, c]
+        List<String> of = List.of(array); // [a, b, c]
+        List<String> copy = List.copyOf(asList); // [a, b, c]
+
+        array[0] = "z";
+
+        System.out.println(asList); // [z, b, c]
+        System.out.println(of); // [a, b, c]
+        System.out.println(copy); // [a, b, c]
+
+        asList.set(0, "x");
+        System.out.println(Arrays.toString(array)); // [x, b, c]
+        copy.add("y"); // throws UnsupportedOperationException
+    }
+}
