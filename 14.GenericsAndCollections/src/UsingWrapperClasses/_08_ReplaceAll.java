10d0551c4ba4f29239f0c247310fc51aa3c76603
Borja Neumann <borjaneumann@hotmail.com>
2021-11-17 11:13:00 +0100

    ReplaceAll()

---
+++ 14.GenericsAndCollections/src/UsingWrapperClasses/_08_ReplaceAll.java
@@ -0,0 +1,14 @@
+package UsingWrapperClasses;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class _08_ReplaceAll {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList(1,2,3);
+        numbers.replaceAll (x -> x * 2);
+        System.out.println(numbers);
+    }
+}
