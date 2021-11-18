156b4b772ef21732ef55b5a001306fe3cc929b6a
Borja Neumann <borjaneumann@hotmail.com>
2021-11-18 10:21:28 +0100

    Hashset example

--- 14.GenericsAndCollections/src/UsingWrapperClasses/_10_UsingTheSetInterface.java
+++ 14.GenericsAndCollections/src/UsingWrapperClasses/_10_UsingTheSetInterface.java
@@ -1,5 +1,6 @@
 package UsingWrapperClasses;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class _10_UsingTheSetInterface {
@@ -9,8 +10,17 @@ public class _10_UsingTheSetInterface {
         //Like List, you can create an immutable Set in one line or make a copy of
         //an existing one.
 
-        Set<Character> letters = Set.of('z','o','o');
+        Set<Character> letters = Set.of('z','o','e'); //no repetions please!
         Set<Character> copy = Set.copyOf(letters);
+
+        //Hashset
+        Set<Integer> setOfNumbers = new HashSet<>();
+        boolean b1 = setOfNumbers.add(66); //true
+        boolean b2 = setOfNumbers.add(10); //true
+        boolean b3 = setOfNumbers.add(66); //false, no repetions please!
+        boolean b4 = setOfNumbers.add(8); //true
+
+        setOfNumbers.forEach(System.out::println);
 
     }
 }
