f321aae864aca4c58232c566c78660d271546c2e
Borja <borjaneumann@hotmail.com>
2021-11-20 20:41:48 +0100

    Using The Map Interface

---
+++ 14.GenericsAndCollections/src/UsingWrapperClasses/_12_UsingTheMapInterface.java
@@ -0,0 +1,10 @@
+package UsingWrapperClasses;
+
+import java.util.Map;
+
+public class _12_UsingTheMapInterface {
+    public static void main(String[] args) {
+        Map<Integer, String> map = Map.of(101, "PP", 102, "QQ", 103, "RR");
+    }
+
+}
