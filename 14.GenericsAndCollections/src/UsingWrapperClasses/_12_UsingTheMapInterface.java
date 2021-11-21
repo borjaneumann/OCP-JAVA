73eadbfc75d6797812a202fec3f43530b097168f
Borja <borjaneumann@hotmail.com>
2021-11-21 20:39:49 +0100

    Map of entries

--- 14.GenericsAndCollections/src/UsingWrapperClasses/_12_UsingTheMapInterface.java
+++ 14.GenericsAndCollections/src/UsingWrapperClasses/_12_UsingTheMapInterface.java
@@ -5,6 +5,12 @@ import java.util.Map;
 public class _12_UsingTheMapInterface {
     public static void main(String[] args) {
         Map<Integer, String> map = Map.of(101, "PP", 102, "QQ", 103, "RR");
+
+        //easier to read
+        Map<Integer, String> map1 = Map.ofEntries(
+                Map.entry(1, "value1"),
+                Map.entry(2, "value2")
+        );
     }
 
 }
