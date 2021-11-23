c9b9a0b9b22437764c18e0498cd944d489b35424
Borja Neumann <borjaneumann@hotmail.com>
2021-11-23 09:03:45 +0100

    Map Methods 2

--- 14.GenericsAndCollections/src/UsingWrapperClasses/_12_UsingTheMapInterface.java
+++ 14.GenericsAndCollections/src/UsingWrapperClasses/_12_UsingTheMapInterface.java
@@ -38,6 +38,17 @@ public class _12_UsingTheMapInterface {
         //V put(K key, V value) Adds or replaces key/value pair. Returns previous value or null.
 
         //V putIfAbsent(K key, V value) --> Adds value if key not present and returns null. Otherwise, returns existing value.
+
+        // V remove(Object key) --> Removes and returns value mapped to key. Returns null if none.
+
+        // V replace(K key, V value) --> Replaces the value for a given key if the key is set. Returns the original value or null if none.
+
+        // void replaceAll(BiFunction<K, V, V> func) --> Replaces each value with the results of the function.
+
+        // int size() --> Returns the number of entries (key/value pairs) in the map.
+
+        // Collection<V>values() --> Returns Collection of all values.
+
     }
 
 }
