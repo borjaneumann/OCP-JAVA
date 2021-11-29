e7cc16d4ed445bec3087a240acf5837563ff4094
Borja Neumann <borjaneumann@hotmail.com>
2021-11-29 09:38:50 +0100

    CompareTo possible results

---
+++ 14.GenericsAndCollections/src/_3_SortingData/Animal.java
@@ -0,0 +1,21 @@
+package _3_SortingData;
+
+public class Animal implements Comparable<Animal>{
+    private int id;
+
+    @Override
+    public int compareTo(Animal a) {
+        return id - a.id;
+    }
+
+    public static void main(String[] args) {
+        var a1 = new Animal();
+        var a2 = new Animal();
+        a1.id = 5;
+        a2.id = 7;
+
+        System.out.println(a1.compareTo(a2)); // 5 -7 = -2
+        System.out.println(a1.compareTo(a1)); // 5 -5 = 0
+        System.out.println(a2.compareTo(a1)); // 7 -5 = 2
+    }
+}
