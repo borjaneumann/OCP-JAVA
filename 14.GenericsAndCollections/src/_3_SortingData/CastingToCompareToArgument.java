41bf6791a03a055736abb14d40a0cd9f05f7587a
Borja Neumann <borjaneumann@hotmail.com>
2021-11-30 09:53:10 +0100

    Casting To CompareTo Argument

--- 14.GenericsAndCollections/src/_3_SortingData/CastingToCompareToArgument.java
+++ 14.GenericsAndCollections/src/_3_SortingData/CastingToCompareToArgument.java
@@ -1,2 +1,14 @@
-package _3_SortingData;public class CastingToCompareToArgument {
+package _3_SortingData;
+
+public class CastingToCompareToArgument implements Comparable {
+    private String name;
+    @Override
+    public int compareTo(Object o) {
+        CastingToCompareToArgument d = (CastingToCompareToArgument) o; // cast because no generics
+        return name.compareTo(d.name);
+    }
+
+    //Since we don't specify a generic type for Comparable, Java assumes that
+    //we want an Object, which means that we have to cast to LegacyDuck
+    //before accessing instance variables on it.
 }
