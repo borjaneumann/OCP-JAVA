ec3393af92fced5197beed138111e9adb1ab550a
Borja Neumann <borjaneumann@hotmail.com>
2021-12-01 09:20:23 +0100

    Keeping compareTo() And equals() Consistent extra text

--- 14.GenericsAndCollections/src/_3_SortingData/KeepingcompareToAndequalsConsistent.java
+++ 14.GenericsAndCollections/src/_3_SortingData/KeepingcompareToAndequalsConsistent.java
@@ -19,4 +19,9 @@ public class KeepingcompareToAndequalsConsistent implements Comparable<Keepingco
     public int compareTo(KeepingcompareToAndequalsConsistent o) {
         return this.name.compareTo(o.name);
     }
+    //You might be sorting Product objects by name, but names are not unique.
+    //Therefore, the return value of compareTo() might not be 0 when
+    //comparing two equal Product objects, so this compareTo() method is not
+    //consistent with equals. One way to fix that is to use a Comparator to
+    //define the sort elsewhere.
 }
