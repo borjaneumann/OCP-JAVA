65722a1af7b202b277888565bb9c9edb40cc7a92
Borja Neumann <borjaneumann@hotmail.com>
2021-12-03 09:47:05 +0100

    Compare Multiple Fields 2

--- 14.GenericsAndCollections/src/_3_SortingData/CompareMultipleFields.java
+++ 14.GenericsAndCollections/src/_3_SortingData/CompareMultipleFields.java
@@ -1,5 +1,7 @@
 package _3_SortingData;
 
+import java.util.Comparator;
+
 public class CompareMultipleFields {
     /*When writing a Comparator that compares multiple instance variables, the
     code gets a little messy. Suppose that we have a Squirrel class, as shown
@@ -27,5 +29,17 @@ public class CompareMultipleFields {
     /*We want to write a Comparator to sort by species name. If two squirrels
     are from the same species, we want to sort the one that weighs the least
     first. We could do this with code that looks like this*/
+    public class MultiFieldComparator implements Comparator<Squirrel> {
+        @Override
+        public int compare(Squirrel o1, Squirrel o2) {
+            int result = o1.getSpecies().compareTo(o2.getSpecies());
+            if (result != 0) return result;
+            return o1.getWeight()- o2.getWeight();
+        }
+        //This works assuming no species names are null. It checks one field. If
+        //they don't match, we are finished sorting. If they do match, it looks at the
+        //next field. This isn't that easy to read, though. It is also easy to get wrong.
+        //Changing != to == breaks the sort completely.
+    }
 
 }
