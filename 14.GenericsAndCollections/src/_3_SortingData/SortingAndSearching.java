e26ad5ba260bc281349d2b344400839de9ea1565
Borja <borjaneumann@hotmail.com>
2021-12-04 21:47:41 +0100

    Collections.sort() 2

--- 14.GenericsAndCollections/src/_3_SortingData/SortingAndSearching.java
+++ 14.GenericsAndCollections/src/_3_SortingData/SortingAndSearching.java
@@ -2,6 +2,7 @@ package _3_SortingData;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class SortingAndSearching {
@@ -14,5 +15,9 @@ public class SortingAndSearching {
         rabbits.add(new Rabbit());
         Collections.sort(rabbits);//does not compile
         // Java knows that the Rabbit class is not Comparable.
+        Comparator<Rabbit> rabbits2 = new ArrayList<>();
+        rabbits.add(new Rabbit());
+        Comparator<Rabbit> c = (r1, r2) -> r1.id- r2.id;
+        Collections.sort(rabbits,c);
     }
 }
