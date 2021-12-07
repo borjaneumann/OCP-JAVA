d90a004e9f05aa32310755b7a33c6f8441549040
Borja Neumann <borjaneumann@hotmail.com>
2021-12-07 09:32:56 +0100

    Not implementing comparable, TreeSet 2

--- 14.GenericsAndCollections/src/_3_SortingData/NotImplementingComparable.java
+++ 14.GenericsAndCollections/src/_3_SortingData/NotImplementingComparable.java
@@ -13,7 +13,14 @@ public class NotImplementingComparable {
         Set<Duck> ducks = new TreeSet<>();
         ducks.add(new Duck("Puddles"));
 
-        Set<Rabbit> rabbits = new TreeSet<>();
-        rabbits.add(new Rabbit()); // ClassCastException
+//        Set<Rabbit> rabbits = new TreeSet<>();
+//        rabbits.add(new Rabbit()); // ClassCastException
+
+        Set<Rabbit> rabbits = new TreeSet<>((r1,r2) -> r1.id- r2.id);
+        rabbits.add(new Rabbit());
+
+        //Now Java knows that you want to sort by id and all is well. Comparators
+        //are helpful objects. They let you separate sort order from the object to be
+        //sorted.
     }
 }
