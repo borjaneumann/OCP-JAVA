09bd3857be0b0da8e76145b0d5e65aa9fa55d982
Borja Neumann <borjaneumann@hotmail.com>
2021-12-13 09:13:19 +0100

    Optional Syntax For Invoking a Generic Method

--- 14.GenericsAndCollections/src/_6_GenericMethods/_1_Handler.java
+++ 14.GenericsAndCollections/src/_6_GenericMethods/_1_Handler.java
@@ -1,5 +1,7 @@
 package _6_GenericMethods;
 
+import javax.swing.*;
+
 import _4_WorkingWithGenerics._2_GenericClasses.Crate;
 
 public class _1_Handler {
@@ -18,4 +20,16 @@ public class More {
     public static <T> void sink(T t) { }
     public static <T> T identity(T t) { return t; }
     public static T noGood(T t) { return t; } // DOES NOT COMPILE
+
+    //OPTIONAL SYNTAX FOR INVOKING A GENERIC METHOD
+    /*You can call a generic method normally, and the compiler will try to
+    figure out which one you want. Alternatively, you can specify the
+    type explicitly to make it obvious what the type is.*/
+
+//    Box .<String>ship("package");
+//    Box.<String[]>ship(args);
+
+    //As to whether this makes things clearer, it is up to you. You should
+    //at least be aware that this syntax exists.
+
 }
