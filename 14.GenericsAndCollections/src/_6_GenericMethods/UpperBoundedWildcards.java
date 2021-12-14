1d844e0534c0199bd2694572a07a7fbb4372415e
Borja Neumann <borjaneumann@hotmail.com>
2021-12-14 09:44:16 +0100

    Upper Bounded Wildcards

---
+++ 14.GenericsAndCollections/src/_6_GenericMethods/UpperBoundedWildcards.java
@@ -0,0 +1,18 @@
+package _6_GenericMethods;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UpperBoundedWildcards {
+
+    // Let's try to write a method that adds up the total of a list of numbers.
+    // ArrayList<Number> list = new ArrayList<Integer>();// DOES NOT COMPILE
+
+    //Instead we need to use wildcard
+    List< ? extends Number> list = new ArrayList<Integer>();
+
+    //IMPORTANT!!
+    //The upper‐bounded wildcard says that any class that extends Number or
+    //Number itself can be used as the formal parameter type:
+
+}
