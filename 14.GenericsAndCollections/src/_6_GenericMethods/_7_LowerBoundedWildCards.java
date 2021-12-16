d15bbfb840c6876c8cca50f5b42e44b847dc02b1
Borja Neumann <borjaneumann@hotmail.com>
2021-12-16 09:16:04 +0100

    Lower Bounded Wildcards 2

--- 14.GenericsAndCollections/src/_6_GenericMethods/_7_LowerBoundedWildCards.java
+++ 14.GenericsAndCollections/src/_6_GenericMethods/_7_LowerBoundedWildCards.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class _7_LowerBoundedWildCards {
+
+    public static void addSound(List<? super String> list) {
+    list.add("quack");
+}
     public static void main(String[] args) {
         List<String> strings = new ArrayList<>();
         strings.add("tweet");
@@ -11,6 +15,12 @@ public class _7_LowerBoundedWildCards {
         List<Object> objects = new ArrayList<Object>(strings);
         addSound(strings);
         addSound(objects);
+
+        //The problem is that we want to pass a List<String> and a List<Object>
+        //to the same method.
+
+        //To solve this problem, we need to use a lower bound.
+
 
     }
 
