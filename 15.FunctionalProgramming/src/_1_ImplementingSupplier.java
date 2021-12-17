bcd19d5a9bc1427c161abdb844a57cee272e4b54
Borja Neumann <borjaneumann@hotmail.com>
2021-12-17 10:16:18 +0100

    Implementing Supplier

---
+++ 15.FunctionalProgramming/src/_1_ImplementingSupplier.java
@@ -0,0 +1,18 @@
+import java.time.LocalDate;
+import java.util.function.Supplier;
+
+public class _1_ImplementingSupplier {
+    public static void main(String[] args) {
+
+        Supplier<LocalDate> s1 = LocalDate::now;
+        Supplier<LocalDate> s2 = () -> LocalDate.now();
+
+        LocalDate d1 = s1.get();
+        LocalDate d2 = s2.get();
+
+        System.out.println(d1);
+        System.out.println(d2);
+    }
+
+
+}
