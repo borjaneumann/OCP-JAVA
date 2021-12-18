d73760c70a778321f088da2d6d896f699f52ea02
Borja <borjaneumann@hotmail.com>
2021-12-18 22:34:07 +0100

    Implementing Function And BiFunction/ Bifunction with the same types

--- 15.FunctionalProgramming/src/_4_ImplementingFunctionAndBiFunction.java
+++ 15.FunctionalProgramming/src/_4_ImplementingFunctionAndBiFunction.java
@@ -1,3 +1,4 @@
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class _4_ImplementingFunctionAndBiFunction {
@@ -10,6 +11,12 @@ public class _4_ImplementingFunctionAndBiFunction {
 
         //This function turns a String into an Integer. Well, technically it turns the
         //String into an int, which is autoboxed into an Integer.
-    }
 
+        /*The types don't have to be different. The following combines two String objects and
+        produces another String:*/
+        BiFunction<String, String, String> b1 = String::concat;
+        BiFunction<String, String, String> b2 = (string, toAdd) -> string.concat(toAdd);
+        System.out.println(b1.apply("baby ", "chick")); // baby chick
+        System.out.println(b2.apply("baby ", "shark")); // baby shark
+    }
 }
