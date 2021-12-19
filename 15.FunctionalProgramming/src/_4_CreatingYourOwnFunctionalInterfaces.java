eaae700ddb38dea871aa6921db838fe3d0d7ea28
Borja <borjaneumann@hotmail.com>
2021-12-19 21:31:29 +0100

    Creating your own functional interface 2

--- 15.FunctionalProgramming/src/_4_CreatingYourOwnFunctionalInterfaces.java
+++ 15.FunctionalProgramming/src/_4_CreatingYourOwnFunctionalInterfaces.java
@@ -2,4 +2,15 @@
 @FunctionalInterface
 public interface _4_CreatingYourOwnFunctionalInterfaces <T,U,V,R> {
     R apply(T t, U u, V v);
+    //The first three supply the types of
+    //the three wheel speeds. The fourth is the return type
 }
+
+@FunctionalInterface
+interface QuadFunction<T,U,V,W,R> {
+    R apply(T t, U u, V v, W w);
+    //There are five type parameters here. The first four supply the types
+    //of the four motors. Ideally these would be the same type, but you
+    //never know. The fifth is the return type in this example.
+}
+
