aa514e9528f6165144cf41f6cbd90b966fb1eab7
Borja Neumann <borjaneumann@hotmail.com>
2021-12-20 11:51:36 +0100

    Convenience Methods Funcional Interfaces

---
+++ 15.FunctionalProgramming/src/_6_ConvenienceMethodsOnFunctionalInterfaces.java
@@ -0,0 +1,24 @@
+import java.util.function.Predicate;
+
+public class _6_ConvenienceMethodsOnFunctionalInterfaces {
+
+    /*By definition, all functional interfaces have a single abstract method. This
+    doesn't mean they can have only one method, though. Several of the
+    common functional interfaces provide a number of helpful default methods.*/
+
+    //All of these facilitate
+    //modifying or combining functional interfaces of the same type
+
+    public static void main(String[] args) {
+        Predicate<String> egg = s -> s.contains("egg");
+        Predicate<String> brown = s -> s.contains("brown");
+
+        /*Now we want a Predicate for brown eggs and another for all other colors
+        of eggs. We could write this by hand, as shown here:*/
+
+        Predicate<String> brownEggs = s -> s.contains("egg") && s.contains("brown");
+        Predicate<String> otherEggs = s -> s.contains("egg") && ! s.contains("brown");
+
+        //It's a bit long to read
+    }
+}
