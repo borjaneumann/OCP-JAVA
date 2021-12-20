b5b177a4ccfcd8ce75f4e44869509551362fcefb
Borja Neumann <borjaneumann@hotmail.com>
2021-12-20 12:02:31 +0100

    Convenience Methods Funcional Interfaces: Consumer example

--- 15.FunctionalProgramming/src/_6_ConvenienceMethodsOnFunctionalInterfaces.java
+++ 15.FunctionalProgramming/src/_6_ConvenienceMethodsOnFunctionalInterfaces.java
@@ -1,3 +1,4 @@
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class _6_ConvenienceMethodsOnFunctionalInterfaces {
@@ -10,6 +11,8 @@ public class _6_ConvenienceMethodsOnFunctionalInterfaces {
     //modifying or combining functional interfaces of the same type
 
     public static void main(String[] args) {
+
+        //PREDICATE
         Predicate<String> egg = s -> s.contains("egg");
         Predicate<String> brown = s -> s.contains("brown");
 
@@ -22,8 +25,14 @@ public class _6_ConvenienceMethodsOnFunctionalInterfaces {
         //It's a bit long to read
         //A better way to deal with this situation is to use two of the default
         //methods on Predicate.
-
         Predicate<String> brownEggs1 = egg.and(brown);
         Predicate<String> otherEggsColors1 = egg.and(brown.negate());
+
+        //CONSUMER
+        Consumer<String> c1 = x -> System.out.print("1: " + x);
+        Consumer<String> c2 = x -> System.out.print(",2: " + x);
+        Consumer<String> combined = c1.andThen(c2);
+        combined.accept("Annie"); // 1: Annie,2: Annie
+
     }
 }
