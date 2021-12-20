ee2de52abcfbf6e22c0f1beb3fe256399ee4510c
Borja <borjaneumann@hotmail.com>
2021-12-20 22:46:05 +0100

    adjusting consumer example

--- 06.LambdasAndFunctionalInterfaces/src/_3_functionalInterface/consumer/ConsumerExample.java
+++ 06.LambdasAndFunctionalInterfaces/src/_3_functionalInterface/consumer/ConsumerExample.java
@@ -6,6 +6,7 @@ public class ConsumerExample {
     public static void main(String[] args) {
         Consumer<String> consumer = x -> System.out.println(x);
         print(consumer, "Hello world");
+        consumer.accept("Hello Universe");
     }
     public static void print(Consumer<String> consumer, String value){
         consumer.accept(value);
