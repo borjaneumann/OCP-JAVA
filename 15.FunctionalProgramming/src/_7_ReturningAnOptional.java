9fcaed3d95c764de81c9872c62f9142a62adb445
Borja Neumann <borjaneumann@hotmail.com>
2021-12-21 09:36:44 +0100

    Optional Example 2

--- 15.FunctionalProgramming/src/_7_ReturningAnOptional.java
+++ 15.FunctionalProgramming/src/_7_ReturningAnOptional.java
@@ -5,6 +5,7 @@ public class _7_ReturningAnOptional {
     //How do we express this “we don't know” or “not applicable” answer in
     //Java? We use the Optional type.
     public static Optional<Double> average (int... scores) {
+
         if (scores.length == 0) return Optional.empty();
         int sum = 0;
         for (int score: scores) {
@@ -12,10 +13,19 @@ public class _7_ReturningAnOptional {
         }
         return Optional.of((double) sum / scores.length);
     }
+    //You can see that one Optional contains a value and the other is empty.
+    //Normally, we want to check whether a value is there and/or get it out of
+    //the box. Here's one way to do that:
+
     public static void main(String[] args) {
+        //EXAMPLE 1
+        System.out.println("Example 1: " + average(90, 100)); // Optional[95.0]
+        System.out.println("Example 1: " + average()); // Optional.empty
 
-
-        System.out.println(average(90, 100)); // Optional[95.0]
-        System.out.println(average()); // Optional.empty
+        //EXAMPLE 2
+        Optional<Double> opt = average(90,100);
+        if (opt.isPresent()){
+            System.out.println("Example 2: " + opt.get());//95.0
+        }
     }
 }
