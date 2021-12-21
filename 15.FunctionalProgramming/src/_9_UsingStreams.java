d5576ac4090c6519f63da6016c4b421ab5e6ee69
Borja Neumann <borjaneumann@hotmail.com>
2021-12-21 16:21:04 +0100

    Creating a parallel stream

--- 15.FunctionalProgramming/src/_9_UsingStreams.java
+++ 15.FunctionalProgramming/src/_9_UsingStreams.java
@@ -9,8 +9,8 @@ public class _9_UsingStreams {
     //In Java, the streams we have been talking about are represented by the
     //Stream<T> interface, defined in the java.util.stream package.
 
-    //CREATE FINITE STREAMS
     public static void main(String[] args) {
+        //CREATE FINITE STREAMS
         Stream<String> empty = Stream.empty();//count = 0
         Stream<Integer> singleElement = Stream.of(1);//count = 1
         Stream<Integer> fromArray = Stream.of(1,2,3);//count = 3 The method signature uses varargs, which let you specify an
@@ -18,7 +18,17 @@ public class _9_UsingStreams {
 
         //Java also provides a convenient way of converting a Collection to a stream
         var list = List.of("a","b","c");
-        Stream<String> fromList = list.stream();
+        Stream<String> fromList = list.stream();//This conversions are common
+
+        //CREATING A PARALLEL STREAM
+        var list2 = List.of("a", "b", "c");
+        Stream<String> fromListParallel = list2.parallelStream();//Using parallel
+        //streams is like setting up multiple tables of workers who are able to do the same task.
+
+
+
+
+
     }
 
 }
