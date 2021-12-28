b7343a7488ce2273dcd619da65001141da744476
Borja Neumann <borjaneumann@hotmail.com>
2021-12-28 09:44:45 +0100

    Functional interfaces for boolean(BooleanSupplier)

---
+++ 15.FunctionalProgramming/src/_22_LearningTheFunctionalInterfacesForPrimitives.java
@@ -0,0 +1,17 @@
+import java.util.function.BooleanSupplier;
+
+public class _22_LearningTheFunctionalInterfacesForPrimitives {
+    public static void main(String[] args) {
+        //Functional Interfaces for boolean
+        //Boolean Supplier
+        //boolean getAsBoolean()
+        BooleanSupplier b1 = () -> true;
+        BooleanSupplier b2 = () -> Math.random()> .5;
+        System.out.println(b1.getAsBoolean()); //true, since it is the result of b1
+        System.out.println(b2.getAsBoolean()); //false, prints out true or false, depending on the random value
+        //generated.
+
+
+    }
+
+}
