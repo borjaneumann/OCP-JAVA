50f4bdc55501e2a704d8cfec2ac0d2ae4bec11c2
Borja Neumann <borjaneumann@hotmail.com>
2021-12-28 10:52:45 +0100

    Working With Advanced Stream Pipeline Concepts

--- 15.FunctionalProgramming/src/_23_WorkingWithAdvancedStreamPipelineConcepts.java
+++ 15.FunctionalProgramming/src/_23_WorkingWithAdvancedStreamPipelineConcepts.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class _23_WorkingWithAdvancedStreamPipelineConcepts {
     public static void main(String[] args) {
@@ -13,6 +14,41 @@ public class _23_WorkingWithAdvancedStreamPipelineConcepts {
         cats.add("KC");
         System.out.println(stream.count()); // 3
 
+        threeDigit(Optional.of(234456));//doesn't print anything
+        threeDigit(Optional.of(123));
+        threeDigit(Optional.of(4));
+        threeDigit(Optional.of(null));
+
+        threeDigitFunctional(Optional.of(456));
+        threeDigitFunctional(Optional.of(567890));
+        threeDigitFunctional(Optional.of(4));
     }
+    //CHAINING OPTIONALS
+    /*Suppose that you are given an Optional<Integer> and asked to print the
+    value, but only if it is a three‐digit number. Without functional
+    programming, you could write the following:*/
+    private static void threeDigit(Optional<Integer> optional) {
+        if (optional.isPresent()) { // outer if
+            var num = optional.get();
+            var string = "" + num;
+            if (string.length() == 3) // inner if
+                System.out.println(string);
+        }
+    }
+    // It works, but it contains nested if statements. That's extra complexity.
+    // Let's try this again with functional programming.
+    private static void threeDigitFunctional(Optional<Integer> optional) {
+        optional.map(n -> "" + n) // part 1
+                .filter(s -> s.length() == 3) // part 2
+                .ifPresent(System.out::println); // part 3
+    }
+//    Now suppose that we wanted to get an Optional<Integer> representing
+//    the length of the String contained in another Optional. Easy enough.
+//    Optional<Integer> result = optional.map(String::length);
+
+
+
+
+
 
 }
