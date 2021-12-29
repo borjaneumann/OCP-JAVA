b3239859a776700e82a85022350bcb5d71742913
Borja Neumann <borjaneumann@hotmail.com>
2021-12-29 10:52:18 +0100

    Throw and throws

--- 16.ExceptionsAssertionsAndLocalization/src/_1_ReviewingExceptions/_2_ThrowAndThrows.java
+++ 16.ExceptionsAssertionsAndLocalization/src/_1_ReviewingExceptions/_2_ThrowAndThrows.java
@@ -1,2 +1,14 @@
-package _1_ReviewingExceptions;public class _2_ThrowAndThrows {
+package _1_ReviewingExceptions;
+
+import java.sql.SQLException;
+
+public class _2_ThrowAndThrows {
+    /*
+    The throw keyword means an exception is actually being thrown,
+    while the throws keyword indicates that the method merely has the potential to
+    throw that exception
+     */
+    public String getDataFromDatabase() throws SQLException {
+        throw new UnsupportedOperationException();
+    }
 }
