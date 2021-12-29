f84547b7f501d0ef4c43840c76941d67240f78ca
Borja Neumann <borjaneumann@hotmail.com>
2021-12-29 15:39:41 +0100

    Constructing Try With Catch Resources Management 3 rules

--- 16.ExceptionsAssertionsAndLocalization/src/_3_AutomatingResourceManagement/_9_ConstructingTryWithCatchResourcesManagement.java
+++ 16.ExceptionsAssertionsAndLocalization/src/_3_AutomatingResourceManagement/_9_ConstructingTryWithCatchResourcesManagement.java
@@ -1,14 +1,23 @@
 package _3_AutomatingResourceManagement;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
 public class _9_ConstructingTryWithCatchResourcesManagement {
 
-    //The first rule you should know is: try‐with‐resources statements require
-    //resources that implement the AutoCloseable interface.
+    /*The first rule you should know is: try‐with‐resources statements require
+    resources that implement the AutoCloseable interface.*/
 
     /*The second rule you should be familiar with is: a try‐with‐resources
     statement can include multiple resources, which are closed in the reverse
     order in which they are declared. Resources are terminated by a
     semicolon ( ;), with the last one being optional.*/
+
+    /*resources declared within a try‐with‐
+    resources statement are in scope only within the try block.*/
 
     public static void main(String[] args) {
 //        try (String reptile = "lizard"){ //does not implement theAutoCloseable interface
@@ -34,6 +43,14 @@ public class _9_ConstructingTryWithCatchResourcesManagement {
         } finally {
             System.out.println("Finally Block multiple resources");
         }
+        //FINAL RULE EXAMPLE (SCOPE)
+        try (Scanner s = new Scanner(System.in)) {
+           s.nextLine();
+        } catch(Exception e) {
+//           s.nextInt(); // DOES NOT COMPILE
+        } finally {
+//           s.nextInt(); // DOES NOT COMPILE
+        }
 
     }
     public static class MyFileReader implements AutoCloseable {
@@ -48,6 +65,35 @@ public class _9_ConstructingTryWithCatchResourcesManagement {
     }
 
     //WHY USING TRY-WITH-RESOURCES STATEMENTS
+    public void copyData(Path path1, Path path2) throws Exception {
+      BufferedReader in = null;
+      BufferedWriter out = null;
+      try {
+           in = Files.newBufferedReader(path1);
+           out = Files.newBufferedWriter(path2);
+           out.write(in.readLine());
+      } finally {
+            if (out != null) {
+                out.close();
+            }
+            if (in != null) {
+                in.close();
+            }
+      }
+    }
+    /*Switching to the try‐with‐resources syntax, we can replace it with
+    the following, much shorter implementation:*/
+    public void copyData2(Path path1, Path path2) throws Exception {
+        try (var in = Files.newBufferedReader(path1);
+            var out = Files.newBufferedWriter(path2))
+        {
+            out.write(in.readLine());
+        }
+    }
+
+
+
+
 
 
 }
