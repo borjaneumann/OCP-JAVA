158cf050ce03b1e1b9a00ba8b381ca35c0a9c4bd
Borja Neumann <borjaneumann@hotmail.com>
2021-12-29 14:25:18 +0100

    Automating Resource Management

---
+++ 16.ExceptionsAssertionsAndLocalization/src/_2_CreatingCustomExceptions/_8_AutomatingResourceManagement.java
@@ -0,0 +1,13 @@
+package _2_CreatingCustomExceptions;
+
+public class _8_AutomatingResourceManagement {
+    /*
+    As previously described, a try‐with‐resources statement ensures that any
+    resources declared in the try clause are automatically closed at the
+    conclusion of the try block. This feature is also known as automatic
+    resource management, because Java automatically takes care of closing
+    the resources for you.
+    For the exam, a resource is typically a file or a database that requires some
+    kind of stream or connection to read or write data. I
+     */
+}
