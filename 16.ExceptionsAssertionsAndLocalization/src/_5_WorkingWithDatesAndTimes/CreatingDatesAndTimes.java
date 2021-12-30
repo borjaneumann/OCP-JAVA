684be0e128a101f1352cb134309d9335a2135ac9
Borja Neumann <borjaneumann@hotmail.com>
2021-12-30 11:42:36 +0100

    Creating Dates And Times of() method and combining date and time

--- 16.ExceptionsAssertionsAndLocalization/src/_5_WorkingWithDatesAndTimes/CreatingDatesAndTimes.java
+++ 16.ExceptionsAssertionsAndLocalization/src/_5_WorkingWithDatesAndTimes/CreatingDatesAndTimes.java
@@ -3,6 +3,7 @@ package _5_WorkingWithDatesAndTimes;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.Month;
 import java.time.ZonedDateTime;
 
 public class CreatingDatesAndTimes {
@@ -25,7 +26,20 @@ public class CreatingDatesAndTimes {
         System.out.println(LocalDateTime.now()); //2021-12-30T11:29:53.665172600
         System.out.println(ZonedDateTime.now()); //2021-12-30T11:29:53.665172600+01:00[Europe/Berlin]
 
+        //Using the of() methods. Creating Dates
+        LocalDate date1 = LocalDate.of(2020, Month.DECEMBER,30);
+        LocalDate date2 = LocalDate.of(2020, 12,30);
 
+        LocalTime time1 = LocalTime.of(6, 15); // hour and minute
+        LocalTime time2 = LocalTime.of(6, 15, 30); // + seconds
+        LocalTime time3 = LocalTime.of(6, 15, 30, 200); // + nanoseconds
+
+        //You can combine dates and times in multiple ways.
+        var dt1 = LocalDateTime.of(2020, Month.OCTOBER, 20, 6, 15,
+                30);
+        LocalDate date = LocalDate.of(2020, Month.OCTOBER, 20);
+        LocalTime time = LocalTime.of(6, 15);
+        var dt2 = LocalDateTime.of(date, time);
 
 
     }
