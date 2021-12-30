1694a47a6c81c5f28308f684f7cc3e9b712857e7
Borja Neumann <borjaneumann@hotmail.com>
2021-12-30 11:01:51 +0100

    Enabling and Disabling assertions

--- 16.ExceptionsAssertionsAndLocalization/src/_4_DeclaringAssertions/_13_EnablingAssertions.java
+++ 16.ExceptionsAssertionsAndLocalization/src/_4_DeclaringAssertions/_13_EnablingAssertions.java
@@ -2,4 +2,30 @@ package _4_DeclaringAssertions;
 
 public class _13_EnablingAssertions {
 
+    /*
+    ENABLING ASSERTIONS
+
+    By default, assert statements are ignored by the JVM at runtime. To
+    enable assertions, use the ‐enableassertions flag on the command line.
+    java -enableassertions Rectangle
+    You can also use the shortcut ‐ea flag.
+    java -ea Rectangle
+
+    the following command enables assertions only for classes in the com.demos package
+    and any subpackages: java -ea:com.demos… my.programs.Mai
+
+    The ellipsis ( …) means any class in the specified package or subpackages.
+    You can also enable assertions for a specific class.
+    java -ea:com.demos.TestColors my.programs.Main
+
+    DISABLING ASSERTIONS
+    Sometimes you want to enable assertions for the entire application but
+    disable it for select packages or classes. Java offers the
+    ‐disableassertions or ‐da flag for just such an occasion.
+    The following command enables assertions for the com.demos package but disables
+    assertions for the TestColors class:
+    java -ea:com.demos… -da:com.demos.TestColors my.programs.Main
+
+     */
+
 }
