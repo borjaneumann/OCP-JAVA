ca0d3e62c41a258bcf818eeea5b4756dc1041f09
Borja Neumann <borjaneumann@hotmail.com>
2021-12-31 10:48:54 +0100

    Formatting Numbers and Currencies

--- 16.ExceptionsAssertionsAndLocalization/src/_6_SupportingInternationalizationAndLocalization/_19_LocalizingNumbers.java
+++ 16.ExceptionsAssertionsAndLocalization/src/_6_SupportingInternationalizationAndLocalization/_19_LocalizingNumbers.java
@@ -1,11 +1,15 @@
 package _6_SupportingInternationalizationAndLocalization;
 
-public class _19_LocalizingNumbers {
-    //The java.text package includes the needed classes.
-    // The following sections cover how to format numbers, currency, and dates based on the locale.
+import java.text.NumberFormat;
+import java.util.Locale;
 
-    //The first step to formatting or parsing data is the same:
-    // obtain an instance of a NumberFormat (factory methods).
+public class _19_LocalizingNumbers {
+    public static void main(String[] args) {
+        //The java.text package includes the needed classes.
+        // The following sections cover how to format numbers, currency, and dates based on the locale.
+
+        //The first step to formatting or parsing data is the same:
+        // obtain an instance of a NumberFormat (factory methods).
 
     /*Description                               Using default Locale and a specified Locale
     ---------------------------------------------------------------------------------------
@@ -29,5 +33,42 @@ public class _19_LocalizingNumbers {
     number.
     The format classes are not thread‐safe. Do not store them in instance
     variables or static variables*/
+
+        //FORMATTING NUMBERS
+        //When we format data, we convert it from a structured object or primitive
+        //value into a String. The NumberFormat.format() method formats the
+        //given number based on the locale associated with the NumberFormat
+        //object.
+
+        int attendeesPerYear = 3_200_000;
+        int attendeesPerMonth = 3_200_000 / 12;
+
+        var us = NumberFormat.getInstance(Locale.US);
+        System.out.println(us.format(attendeesPerMonth)); //266,666
+        var gr = NumberFormat.getInstance(Locale.GERMANY);
+        System.out.println(gr.format(attendeesPerMonth)); //266.666
+        var ca = NumberFormat.getInstance(Locale.CANADA_FRENCH);
+        System.out.println(ca.format(attendeesPerMonth)); //266 666
+
+        //FORMATTING CURRENCY
+
+        double price = 48;
+        var myLocale = NumberFormat.getCurrencyInstance();
+        System.out.println(myLocale.format(price));
+        var myLocale2 = NumberFormat.getCurrencyInstance(Locale.JAPAN);
+        System.out.println(myLocale2.format(price));
+        Locale es = new Locale.Builder()
+                .setRegion("ES")
+                .setLanguage("es")
+                .build();
+        var myLocale3 = NumberFormat.getCurrencyInstance(es);
+        System.out.println(myLocale3.format(price));
+
+        //In the real world, use int or BigDecimal for money and not double.
+
+
+
+    }
+
 
 }
