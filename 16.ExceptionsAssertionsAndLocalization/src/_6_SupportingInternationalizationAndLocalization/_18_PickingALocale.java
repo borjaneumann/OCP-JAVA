b14e53a7bfffaae9117fc28fa6b379e1d16e0746
Borja Neumann <borjaneumann@hotmail.com>
2021-12-31 09:56:05 +0100

    Selecting Locale rather than default

--- 16.ExceptionsAssertionsAndLocalization/src/_6_SupportingInternationalizationAndLocalization/_18_PickingALocale.java
+++ 16.ExceptionsAssertionsAndLocalization/src/_6_SupportingInternationalizationAndLocalization/_18_PickingALocale.java
@@ -19,5 +19,37 @@ public class _18_PickingALocale {
         // US_en --> The country and language are reversed
         // EN --> Language must be lowercase
 
+        //As a developer, you often need to write code that selects a locale other
+        //than the default one. There are three common ways of doing this.
+
+        // 1. The first is to use the built‐in constants in the Locale class, available for some
+        //common locales.
+        System.out.println(Locale.JAPANESE); //ja
+        System.out.println(Locale.JAPAN);  // ja_JP
+        /* 2.The second way of selecting a Locale is to use the constructors to create a
+        new object. You can pass just a language, or both a language and country:*/
+        System.out.println(new Locale("fr")); // fr
+        System.out.println(new Locale("hi", "IN")); // hi_IN
+
+        /*Java will let you create a Locale with an invalid language or country, such
+        as xx_XX. However, it will not match the Locale that you want to use, and
+        your program will not behave as expected.*/
+
+        // 3.There's a third way that is the builder design pattern that lets you set
+        // all of the properties that you care about and then build it at the end.
+        // This means that you can specify the properties in any order.
+        // The following two Locale values both represent en_US:
+
+        Locale l1 = new Locale.Builder()
+                .setLanguage("en")
+                .setRegion("US")
+                .build();
+        Locale l2 = new Locale.Builder()
+                .setRegion("US")
+                .setLanguage("en")
+                .build();
+
+
+
     }
 }
