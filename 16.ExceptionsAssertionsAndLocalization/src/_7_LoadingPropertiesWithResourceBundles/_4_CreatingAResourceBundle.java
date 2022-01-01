dc725a5fe89319bdf9701ebdb042e68944b8477c
Borja Neumann <borjaneumann@hotmail.com>
2022-01-01 17:43:32 +0100

    Creating a REsource Bundle

---
+++ 16.ExceptionsAssertionsAndLocalization/src/_7_LoadingPropertiesWithResourceBundles/_4_CreatingAResourceBundle.java
@@ -0,0 +1,13 @@
+package _7_LoadingPropertiesWithResourceBundles;
+
+import java.util.Locale;
+
+public class _4_CreatingAResourceBundle {
+    public static void main(String[] args) {
+        Locale us = new Locale("en", "US");
+        Locale france = new Locale("fr", "FR");
+        Locale englishCanada = new Locale("en", "CA");
+        Locale frenchCanada = new Locale("fr", "CA");
+
+    }
+}
