08c72633dc46eb93c1aa43799da7305234c16eb8
Borja Neumann <borjaneumann@hotmail.com>
2022-01-03 11:50:05 +0100

    Properties of modules types

---
+++ 17.ModularApplications/src/_3_ComparingModuleTypes.java
@@ -0,0 +1,24 @@
+public class _3_ComparingModuleTypes {
+
+    //A key point to remember is that:
+    // 1. code on the classpath can access the module path.
+    // 2. By contrast, code on the module path is unable to read from the classpath.
+
+    /*
+    Properties of modules types
+    Property                                                            Named           Automatic           Unnamed
+    =========================================================================================================================
+    - A ______ module contains a module‐info file?                      Yes             No                  Ignored if present
+    ---------------------------------------------------------------------------------------------------------------------------
+    - A ______ module exports which packages                            Those in the    All packages        No packages
+        to other modules?                                               module‐info file
+    ---------------------------------------------------------------------------------------------------------------------------
+    - A ______ module is readable by other modules on                   Yes             Yes                 No
+    the module path?
+    ---------------------------------------------------------------------------------------------------------------------------
+    - A ______ module is readable by other JARs on the classpath?       Yes             Yes                 Yes
+
+    ==========================================================================================================================
+     */
+
+}
