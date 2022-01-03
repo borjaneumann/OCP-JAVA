a6a104fc2c8490e301b8957b84f4c83875e684ea
Borja Neumann <borjaneumann@hotmail.com>
2022-01-03 10:57:55 +0100

    Reviewing Modules Directives

---
+++ 17.ModularApplications/src/_1_ReviewingModuleDirectives.java
@@ -0,0 +1,12 @@
+public class _1_ReviewingModuleDirectives {
+
+    /*Derivative                        Description
+    ----------------------------------------------------------------------------------------
+    exports <package>                   Allows all modules to access the package
+    exports <package> to <module>       Allows a specific module to access the package
+    requires <module>                   Indicates module is dependent on another module
+    requires transitive <module>        Indicates the module and that all modules that use this module are dependent on another module
+    uses <interface>                    Indicates that a module uses a service
+    provides<interface> with <class>    Indicates that a module provides an implementation of a service */
+
+}
