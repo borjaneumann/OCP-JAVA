acef60030ff9f803b6f63dc1193ccb4d199665c0
Borja Neumann <borjaneumann@hotmail.com>
2022-01-03 13:15:38 +0100

    Using JDPES Animatronic compile with warnings

---
+++ 17.ModularApplications/src/_4_UsingJDEPS/zoo/dinos/Animatronic.java
@@ -0,0 +1,30 @@
+package _4_UsingJDEPS.zoo.dinos;
+
+import java.time.*;
+import java.util.*;
+
+import sun.misc.Unsafe;
+
+public class Animatronic {
+
+    /*The jdeps command gives you information about dependencies. Luckily,
+    you are not expected to memorize all the options for the exam.
+    You are expected to understand how to use jdeps with projects that have
+    not yet been modularized to assist in identifying dependencies and
+    problems.*/
+
+    private List<String> names;
+    private LocalDate visitDate;
+
+    public Animatronic(List<String> names, LocalDate visitDate) {
+        this.names = names;
+        this.visitDate = visitDate;
+    }
+
+    public void unsafeMethod() {
+        Unsafe unsafe = Unsafe.getUnsafe();
+    }
+
+    //We are looking into what dependencies we will need when we do modularize this JAR.
+    //javac 17.ModularApplications/src/_4_UsingJDEPS/zoo/dinos/*.java on the terminal
+}
