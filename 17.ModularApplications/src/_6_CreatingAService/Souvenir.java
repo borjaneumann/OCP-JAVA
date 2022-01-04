85a0d9405d400cf45c37cb3641f69edd6a7e28c5
Borja Neumann <borjaneumann@hotmail.com>
2022-01-04 11:19:14 +0100

    class Souvenir

---
+++ 17.ModularApplications/src/_6_CreatingAService/Souvenir.java
@@ -0,0 +1,13 @@
+package _6_CreatingAService;
+
+
+public class Souvenir {
+    private String description;
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
