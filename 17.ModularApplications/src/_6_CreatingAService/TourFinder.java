0d1f5db0031ef4970bf444d5b78266418c3d2e60
Borja Neumann <borjaneumann@hotmail.com>
2022-01-04 11:58:11 +0100

    module-info.java adjusted

--- 17.ModularApplications/src/_6_CreatingAService/TourFinder.java
+++ 17.ModularApplications/src/_6_CreatingAService/TourFinder.java
@@ -8,18 +8,17 @@ import java.util.ServiceLoader;
 //import java.util.*;
 //import zoo.tours.api.*;
 public class TourFinder {
+    //lookup methods
     public static Tour findSingleTour() {
-        ServiceLoader<Tour> loader =
-                ServiceLoader.load(Tour.class);
+        ServiceLoader<Tour> loader = ServiceLoader.load(Tour.class);
         for (Tour tour : loader)
             return tour;
         return null;
     }
-
+    //lookup methods
     public static List<Tour> findAllTours() {
         List<Tour> tours = new ArrayList<>();
-        ServiceLoader<Tour> loader =
-                ServiceLoader.load(Tour.class);
+        ServiceLoader<Tour> loader = ServiceLoader.load(Tour.class);
         for (Tour tour : loader)
             tours.add(tour);
                return tours;
--- 17.ModularApplications/src/_6_CreatingAService/module-info.java
+++ 17.ModularApplications/src/_6_CreatingAService/module-info.java
@@ -1,3 +1,11 @@
+//First
 //module zoo.tours.api {
-////    exports zoo.tours.api;
+//    exports zoo.tours.api;
+//}
+
+//Second
+//module zoo.tours.reservations {
+//    exports zoo.tours.reservations;
+//    requires zoo.tours.api;
+//    uses zoo.tours.api.Tour;
 //}
