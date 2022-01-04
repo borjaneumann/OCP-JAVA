55d06b9cca27cc4ca8ec71afbb5c5b6dbbc84e39
Borja Neumann <borjaneumann@hotmail.com>
2022-01-04 13:13:51 +0100

    module-info.java visitor module commands

--- 17.ModularApplications/src/_6_CreatingAService/zoo/visitor/module-info.java
+++ 17.ModularApplications/src/_6_CreatingAService/zoo/visitor/module-info.java
@@ -2,3 +2,12 @@
 //    requires zoo.tours.api;
 //    requires zoo.tours.reservations;
 //}
+
+/*
+Commands
+javac -p mods -d consumerModule
+consumerModule/zoo/visitor/*.java
+consumerModule/module-info.java
+jar -cvf mods/zoo.visitor.jar -C consumerModule/ .
+java -p mods -m zoo.visitor/zoo.visitor.Tourist
+ */
