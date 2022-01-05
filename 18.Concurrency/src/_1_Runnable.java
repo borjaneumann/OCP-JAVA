e3d0c79c90e72d93d84fd4eef2cac55f8bed7bdb
Borja Neumann <borjaneumann@hotmail.com>
2022-01-05 12:06:54 +0100

    Runnable interface

---
+++ 18.Concurrency/src/_1_Runnable.java
@@ -0,0 +1,12 @@
+public class _1_Runnable {
+
+    /*java.lang.Runnable is a functional interface that takes no arguments and returns no data.
+    The following is the definition of the Runnable interface:
+    @FunctionalInterface public interface Runnable {
+    void run();
+    }
+    The Runnable interface is commonly used to define the task or work a
+    thread will execute, separate from the main application thread.
+     */
+
+}
