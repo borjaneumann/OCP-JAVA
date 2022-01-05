f8501637669dc32dc474abc49f080accac472e35
Borja Neumann <borjaneumann@hotmail.com>
2022-01-05 12:17:40 +0100

    Runnable implemented in classes

---
+++ 18.Concurrency/src/_2_CreatingRunnableClasses.java
@@ -0,0 +1,20 @@
+public class _2_CreatingRunnableClasses {
+    //implemented in classes
+    /*
+    public class CalculateAverage implements Runnable {
+        public void run() {
+        // Define work here
+        }
+    }
+    public class CalculateAverages implements Runnable {
+        private double[] scores;
+        public CalculateAverages(double[] scores) {
+            this.scores = scores;
+        }
+        public void run() {
+        // Define work here that uses the scores object
+        }
+    }
+     */
+
+}
