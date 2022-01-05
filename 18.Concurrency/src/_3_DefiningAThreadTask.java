befd7542ad0e93f47773cf81f45c4bb1bcd69f62
Borja Neumann <borjaneumann@hotmail.com>
2022-01-05 14:01:03 +0100

    Main and 3 more threads

--- 18.Concurrency/src/_3_DefiningAThreadTask.java
+++ 18.Concurrency/src/_3_DefiningAThreadTask.java
@@ -24,5 +24,14 @@ public class _3_DefiningAThreadTask {
         }
     }
 
+    public static void main(String[] args) {
+        System.out.println("begin");
+        (new ReadInventoryThread()).start();
+        (new Thread(new PrintData())).start();
+        (new ReadInventoryThread()).start();
+        System.out.println("end");
+    }
+
+
 
 }
