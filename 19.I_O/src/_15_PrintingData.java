e37ba50197950834f8eff18cc2605834786dde4c
Borja Neumann <borjaneumann@hotmail.com>
2022-01-10 11:17:15 +0100

    Printing Data: Using format() with flags

--- 19.I_O/src/_15_PrintingData.java
+++ 19.I_O/src/_15_PrintingData.java
@@ -104,6 +104,27 @@ public class _15_PrintingData {
     System.out.format("Food: %d tons", 2.0); //
     IllegalFormatConversionException
 
+    USING FORMAT WITH FLAGS
+    =======================
+    By default, %f displays exactly six digits past the decimal. If you want to display
+    only one digit after the decimal, you could use %.1f instead of %f.
+    The format() method relies on rounding, rather than truncating
+    when shortening numbers. For example, 90.250000 will be
+    displayed as 90.3 (not 90.2) when passed to format() with %.1f.
+    The format() method also supports two additional features. You
+    can specify the total length of output by using a number before the
+    decimal symbol. By default, the method will fill the empty space
+    with blank spaces. You can also fill the empty space with zeros, by
+    placing a single zero before the decimal symbol. The following
+    examples use brackets, [], to show the start/end of the formatted
+    value:
+    var pi = 3.14159265359;
+
+    System.out.format("[%f]",pi); // [3.141593]
+    System.out.format("[%12.8f]",pi); // [ 3.14159265]
+    System.out.format("[%012f]",pi); // [00003.141593]
+    System.out.format("[%12.2f]",pi); // [ 3.14]
+    System.out.format("[%.3f]",pi); // [3.142]
 
      */
 
