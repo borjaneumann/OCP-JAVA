6e7e90066e51c326dde0877e7ca6ebb22fa07beb
Borja Neumann <borjaneumann@hotmail.com>
2022-01-10 09:32:48 +0100

    serialize example(provious commit was deserialize

--- 19.I_O/src/_13_StoringDataWithObjectOutputStreamAndObjectInputStream.java
+++ 19.I_O/src/_13_StoringDataWithObjectOutputStreamAndObjectInputStream.java
@@ -1,8 +1,13 @@
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.EOFException;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class _13_StoringDataWithObjectOutputStreamAndObjectInputStream {
@@ -27,9 +32,32 @@ public class _13_StoringDataWithObjectOutputStreamAndObjectInputStream {
     */
     void saveToFile(List<_11_SerializingData.Gorilla> gorillas, File dataFile) throws IOException {
         try (var out = new ObjectOutputStream(
-            new BufferedOutputStream(new FileOutputStream(dataFile)))) {
+                new BufferedOutputStream(new FileOutputStream(dataFile)))) {
             for (_11_SerializingData.Gorilla gorilla : gorillas)
                 out.writeObject(gorilla);
         }
     }
+
+    /*Notice we start with a file stream, wrap it in a buffered
+    stream to improve performance, and then wrap that with an object stream.
+    Serializing the data is as simple as passing it to writeObject().
+    Once the data is stored in a file, we can deserialize it using the following
+    method:
+    */
+
+    List<_11_SerializingData.Gorilla> readFromFile(File dataFile) throws IOException, ClassNotFoundException {
+        var gorillas = new ArrayList<_11_SerializingData.Gorilla>();
+        try (var in = new ObjectInputStream(
+                new BufferedInputStream( new FileInputStream(dataFile)))) {
+                    while (true) {
+                        var object = in.readObject();
+                        if (object instanceof _11_SerializingData.Gorilla)
+                            gorillas.add((_11_SerializingData.Gorilla) object);
+                    }
+        } catch (EOFException e) {
+            // File end reached
+        }
+        return gorillas;
+    }
+
 }
