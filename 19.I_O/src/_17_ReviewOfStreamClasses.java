fc27eec1045de374a7389fafb0b85029913658a7
Borja Neumann <borjaneumann@hotmail.com>
2022-01-10 11:50:39 +0100

    Review Of Stream Classes

---
+++ 19.I_O/src/_17_ReviewOfStreamClasses.java
@@ -0,0 +1,17 @@
+public class _17_ReviewOfStreamClasses {
+
+    /*
+    The InputStreamReader class wraps an InputStream with a Reader,
+    while the OutputStreamWriter class wraps an OutputStream with a Writer.
+
+    try (Reader r = new InputStreamReader(System.in);
+    Writer w = new OutputStreamWriter(System.out)) {
+    }
+
+    These classes are incredibly convenient and are also unique in that
+    they are the only I/O stream classes to have both InputStream/
+    OutputStream and Reader/ Writer in their name.
+
+     */
+
+}
