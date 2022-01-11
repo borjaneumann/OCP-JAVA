290ce3280203808b17f5289779b42ecc14c6a920
Borja Neumann <borjaneumann@hotmail.com>
2022-01-11 11:10:35 +0100

    Absolute VS Relative Paths

---
+++ 20.NIO.2/src/_2_AbsoluteVSRelativePaths.java
@@ -0,0 +1,17 @@
+public class _2_AbsoluteVSRelativePaths {
+
+    /*
+    Determining whether a path is relative or absolute is actually file‐
+    system dependent. To match the exam, we adopt the following
+    conventions:
+
+    If a path starts with a forward slash ( /), it is absolute, with / as the
+    root directory. Examples: /bird/parrot.png and /bird/../data/./info
+
+    If a path starts with a drive letter ( c:), it is absolute, with the drive
+    letter as the root directory. Examples: c:/bird/parrot.png and d:/bird/../data/./info
+
+    Otherwise, it is a relative path. Examples: bird/parrot.png and bird/../data/./info
+     */
+
+}
