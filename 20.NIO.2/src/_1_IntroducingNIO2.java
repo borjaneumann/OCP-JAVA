48ed6c3b806864de41a70578d5c6c91d515d758a
Borja Neumann <borjaneumann@hotmail.com>
2022-01-11 10:53:49 +0100

    Introducing NIO2

---
+++ 20.NIO.2/src/_1_IntroducingNIO2.java
@@ -0,0 +1,18 @@
+public class _1_IntroducingNIO2 {
+
+    /*
+     In this chapter, we focus on the java.nio version 2 API, or NIO.2 for short,
+     to interact with files. NIO.2 is an acronym that stands for the second version
+     of the Non‐blocking Input/Output API, and it is sometimes referred to as the “New I/O.
+     When talking about streams we refered to functional programming.
+
+    By legacy, we mean that the preferred approach for working with files and
+    directories with newer software applications is to use NIO.2, rather than
+    java.io.File.
+
+    People sometimes refer to NIO.2 as just NIO, although for clarity
+    and to distinguish it from the first version of NIO, we will refer to it
+    as NIO.2 throughout the chapter.
+
+     */
+}
