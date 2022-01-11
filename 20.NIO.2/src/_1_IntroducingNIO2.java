7964057d7c11b8b6a57fcd7fdb696f18ad662535
Borja Neumann <borjaneumann@hotmail.com>
2022-01-11 11:01:48 +0100

    Introducing Paths

--- 20.NIO.2/src/_1_IntroducingNIO2.java
+++ 20.NIO.2/src/_1_IntroducingNIO2.java
@@ -13,6 +13,26 @@ public class _1_IntroducingNIO2 {
     People sometimes refer to NIO.2 as just NIO, although for clarity
     and to distinguish it from the first version of NIO, we will refer to it
     as NIO.2 throughout the chapter.
+     */
 
+    /*
+    INTRODUCING PATH
+    The cornerstone of NIO.2 is the java.nio.file.Path interface. You can think
+    of a Path as the NIO.2 replacement for the java.io.File class, although how you
+    use it is a bit different.
+
+    You can think of a Path as the NIO.2 replacement for the
+    java.io.File class, although how you use it is a bit different.
+    As we did in Chapter 19 and continue to do in this chapter, we treat an instance
+    that points to a directory as a file since it is stored in the file system with similar properties.
+
+    IMPORTANT!
+    Now for something completely different. Unlike the java.io.File class,
+    the Path interface contains support for symbolic links. A symbolic link is a
+    special file within a file system that serves as a reference or pointer to
+    another file or directory.
+
+    /zoo/cats/lion/Cubs.java
+    /zoo/favorite/Cubs.java
      */
 }
