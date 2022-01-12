42b13e5e26314a75ecfb495713ef6096f9d20299
Borja Neumann <borjaneumann@hotmail.com>
2022-01-12 09:40:59 +0100

    Connecting to Remote File Systems

--- 20.NIO.2/src/_3_ObtainingAPathFromTheFileSystemClass.java
+++ 20.NIO.2/src/_3_ObtainingAPathFromTheFileSystemClass.java
@@ -17,6 +17,27 @@ public class _3_ObtainingAPathFromTheFileSystemClass {
     Path path2 = FileSystems.getDefault().getPath("c:\\zooinfo\\November\\employees.txt");
     Path path3 = FileSystems.getDefault().getPath("/home/zoodirectory");
 
+    /*
+    CONNECTING TO REMOTE FILE SYSTEMS
+    While most of the time we want access to a Path object that is
+    within the local file system, the FileSystems class does give us the
+    freedom to connect to a remote file system, as follows:
+
+    // FileSystems factory method
+    public static FileSystem getFileSystem(URI uri)
+
+    The following shows how such a method can be used:
+
+    FileSystem fileSystem = FileSystems.getFileSystem(
+    new URI("http://www.selikoff.net"));
+    Path path = fileSystem.getPath("duck.txt");
+
+    This code is useful when we need to construct Path objects
+    frequently for a remote file system. NIO.2 gives us the power to
+    connect to both local and remote file systems, which is a major
+    improvement over the legacy java.io.File class.
+     */
+
 
 
 }
