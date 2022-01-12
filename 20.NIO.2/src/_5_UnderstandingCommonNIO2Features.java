7a6b4fa2ca716d0d33bf20c62411ed9a716d2ecf
Borja Neumann <borjaneumann@hotmail.com>
2022-01-12 10:47:39 +0100

    Providing Optional arguments

--- 20.NIO.2/src/_5_UnderstandingCommonNIO2Features.java
+++ 20.NIO.2/src/_5_UnderstandingCommonNIO2Features.java
@@ -17,4 +17,24 @@ public class _5_UnderstandingCommonNIO2Features {
       |_clownfish                   ../../clownfish
 
      */
+
+    /* Providing Optional arguments
+    Enum type           Interface inherited       Enum value            Details
+    ========================================================================================================
+    LinkOption          CopyOption                NOFOLLOW_LINKS        Do not follow symbolic links.
+                        OpenOption                NOFOLLOW_LINKS        Do not follow symbolic links.
+    --------------------------------------------------------------------------------------------------------
+    Standard            CopyOption                ATOMIC_MOVE           Move file as atomic file system operation.
+    CopyOption                                    COPY_ATTRIBUTES       Copy existing attributes to new file.
+                                                  REPLACE_EXISTING      Overwrite file if it already exists.
+    -------------------------------------------------------------------------------------------------------
+    Standard            OpenOption                APPEND                If file is already open for write, then append to the end.
+    OpenOption                                    CREATE                Create a new file if it does not exist.
+                                                  CREATE_NEW            Create a new file only if it does not exist, fail otherwise.
+                                                  READ                  Open for read access.
+                                                  TRUNCATE_EXISTING     If file is already open for write, then erase file and append to beginning.
+                                                  WRITE                 Open for write access.
+    --------------------------------------------------------------------------------------------------------
+    FileVisitOption     N/A                       FOLLOW_LINKS          Follow symbolic links.
+     */
 }
