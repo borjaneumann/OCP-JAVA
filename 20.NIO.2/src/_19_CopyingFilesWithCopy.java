e99fd0504b711a463cba004337bb26b1936a2e60
Borja Neumann <borjaneumann@hotmail.com>
2022-01-13 12:00:40 +0100

    Copying and Replacing files

--- 20.NIO.2/src/_19_CopyingFilesWithCopy.java
+++ 20.NIO.2/src/_19_CopyingFilesWithCopy.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class _19_CopyingFilesWithCopy {
 
@@ -16,7 +17,7 @@ public class _19_CopyingFilesWithCopy {
 
         Files.copy(Paths.get("/panda/bamboo.txt"), Paths.get("/panda-save/bamboo.txt"));
         Files.copy(Paths.get("/turtle"), Paths.get("/turtleCopy")) ;
-    }
+
     /*
     When directories are copied, the copy is shallow. A shallow copy means
     that the files and subdirectories within the directory are not copied.
@@ -24,6 +25,30 @@ public class _19_CopyingFilesWithCopy {
     A deep copy means that the entire tree is copied, including all of its content and
     subdirectories.
      */
+
+    //COPYING AND REPLACING FILES
+    /*
+    By default, if the target already exists, the copy() method will throw an
+    exception. You can change this behavior by providing the
+    StandardCopyOption enum value REPLACE_EXISTING to the method. The
+    following method call will overwrite the movie.txt file if it already exists:
+    */
+
+    Files.copy(Paths.get("book.txt"), Paths.get("movie.txt"),
+    StandardCopyOption.REPLACE_EXISTING);
+
+    //COPYING FILES WITH I/O STREAMS
+    /*
+    The Files class includes two copy() methods that operate with I/O
+    streams.
+
+    public static long copy(InputStream in, Path target, CopyOption… options) throws IOException
+    public static long copy(Path source, OutputStream out) throws IOException
+
+     */
+
+
+    }
 
 
 
