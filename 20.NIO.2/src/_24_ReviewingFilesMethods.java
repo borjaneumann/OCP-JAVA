9a4910146b6c74c2a835c3dff46b2941d3ef9368
Borja Neumann <borjaneumann@hotmail.com>
2022-01-14 11:08:14 +0100

    Reviewing Files Methods

---
+++ 20.NIO.2/src/_24_ReviewingFilesMethods.java
@@ -0,0 +1,17 @@
+public class _24_ReviewingFilesMethods {
+    /*
+    Below the static methods in the Files class for the exam.
+
+    Files methods
+    boolean exists(Path, LinkOption…)               Path move(Path, Path, CopyOption…)
+    boolean isSameFile(Path, Path)                  void delete(Path)
+    Path createDirectory(Path, FileAttribute<?>…)   boolean deleteIfExists(Path)
+    Path createDirectories(Path,FileAttribute<?>…)  BufferedReader newBufferedReader(Path)
+    Path copy(Path, Path, CopyOption…)              BufferedWriter newBufferedWriter(Path, OpenOption…)
+    long copy(InputStream, Path, CopyOption…)       List<String> readAllLines(Path)
+    long copy(Path,OutputStream)
+
+    All of these methods except exists() declare IOException.
+
+     */
+}
