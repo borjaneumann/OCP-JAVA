e8beb6acb92b1afdad25bd61562258bed702fad0
Borja <borjaneumann@hotmail.com>
2022-01-15 22:17:52 +0100

    Reading File Size With size()

---
+++ 20.NIO.2/src/_27_ReadingFileSizeWithSize.java
@@ -0,0 +1,15 @@
+public class _27_ReadingFileSizeWithSize {
+
+    /*
+    The Files class includes a method to determine the size of the file in
+    bytes.
+    public static long size(Path path) throws IOException
+
+    The size returned by this method represents the conceptual size of the
+    data, and this may differ from the actual size on the persistent storage
+    device. The following is a sample call to the size() method:
+
+    System.out.print(Files.size(Paths.get("/zoo/animals.txt")));
+     */
+
+}
