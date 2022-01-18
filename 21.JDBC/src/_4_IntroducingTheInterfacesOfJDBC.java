41ba1b6d2370df4fee7fad1c76e64f2190a3951b
Borja Neumann <borjaneumann@hotmail.com>
2022-01-18 11:53:42 +0100

    JDBC: IntroducingTheInterfacesOfJDBC

---
+++ 21.JDBC/src/_4_IntroducingTheInterfacesOfJDBC.java
@@ -0,0 +1,11 @@
+public class _4_IntroducingTheInterfacesOfJDBC {
+    /*
+    For the exam you need to know five key interfaces of JDBC.
+    With JDBC, the concrete classes come from the JDBC driver. Each
+    database has a different JAR file with these classes.
+
+    This driver JAR contains an implementation of these key interfaces along
+    with a number of other interfaces. The key is that the provided
+    implementations know how to communicate with a database.
+     */
+}
