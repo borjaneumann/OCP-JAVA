9af8093f04484c5d493fe087fa3d6ef2eb3d1ca2
Borja Neumann <borjaneumann@hotmail.com>
2022-01-18 10:14:18 +0100

    JDBC: Introducing Relational Databases And SQL

---
+++ 21.JDBC/src/_1_IntroducingRelationalDatabasesAndSQL.java
@@ -0,0 +1,40 @@
+public class _1_IntroducingRelationalDatabasesAndSQL {
+
+    /*
+    JDBC stands for Java Database Connectivity.
+
+    If you are new to JDBC, note that this chapter covers only the basics of
+    JDBC and working with databases. What we cover is enough for the
+    exam. To be ready to use JDBC on the job, we recommend that you read
+    books on SQL along with Java and databases. For example, you might try
+    SQL for Dummies, 9th edition, Allen G. Taylor (Wiley, 2018) and
+    Practical Database Programming with Java, Ying Bai (Wiley‐IEEE
+    Press, 2011).
+     */
+    /*
+    Introducing Relational Databases and SQL
+    ========================================
+    A database is an organized collection of data.
+
+    A relational database is a database that is organized into tables, which
+    consist of rows and columns. You can think of a table as a spreadsheet.
+    There are two main ways to access a relational database from Java.
+
+    1) Java Database Connectivity Language (JDBC): Accesses data as rows and
+    columns. JDBC is the API covered in this chapter.
+    2) Java Persistence API (JPA): Accesses data through Java objects using a
+    concept called object‐relational mapping (ORM). The idea is that you
+    don't have to write as much code, and you get your data in Java objects.
+    JPA is not on the exam, and therefore it is not covered in this chapter.
+
+    A relational database is accessed through Structured Query Language
+    (SQL). SQL is a programming language used to interact with database
+    records. JDBC works by sending a SQL command to the database and
+    then processing the response.
+    In addition to relational databases, there is another type of database called
+    a NoSQL database. This is for databases that store their data in a format
+    other than tables, such as key/value, document stores, and graph‐based
+    databases. NoSQL is out of scope for the exam as well.
+     */
+
+}
