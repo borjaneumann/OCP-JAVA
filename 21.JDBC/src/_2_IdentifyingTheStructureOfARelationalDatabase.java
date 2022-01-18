e1defddf45d6eb83275354543b436338b3f42294
Borja Neumann <borjaneumann@hotmail.com>
2022-01-18 10:32:50 +0100

    JDBC: Introducing The Structure Of A Relational Database

---
+++ 21.JDBC/src/_2_IdentifyingTheStructureOfARelationalDatabase.java
@@ -0,0 +1,25 @@
+public class _2_IdentifyingTheStructureOfARelationalDatabase {
+    /*
+    Our sample database has two tables. One has a row for each species that is
+    in our zoo. The other has a row for each animal. These two relate to each
+    other because an animal belongs to a species. These relationships are why
+    this type of database is called a relational database.
+
+    Each table has a primary key, which gives us a unique way to reference each row.
+    After all, two animals might have the same name, but they can't have the same ID.
+
+    In some situations, it is a combination of columns called a
+    compound key. For example, a student identifier and year might be a
+    compound key.
+    There are two rows and three columns in the exhibits table and five rows
+    and three columns in the names table. You do need to know about rows
+    and columns for the exam.
+
+    We provide a program to download, install, and set up Derby to run
+    the examples in this chapter. You don't have to understand it for the
+    exam.
+    Parts of SQL called database definition language (DDL) and
+    database manipulation language (DML) are used to do so.
+     */
+
+}
