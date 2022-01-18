1360d498ed1d5382ca3a486a5ac5ee8880f5b632
Borja Neumann <borjaneumann@hotmail.com>
2022-01-18 11:23:19 +0100

    JDBC: CRUD Operations

---
+++ 21.JDBC/src/_3_WritingBasicSQLStatements.java
@@ -0,0 +1,17 @@
+public class _3_WritingBasicSQLStatements {
+
+    /*
+    They are referred to as CRUD (Create, Read, Update, Delete).
+    The SQL keywords don't match the acronym, so pay attention to the SQL keyword of each in the below Table
+
+    CRUD operations
+    Operation           SQL Keyword         Description
+    Create              INSERT              Adds a new row to the table
+    Read                SELECT              Retrieves data from the table
+    Update              UPDATE              Changes zero or more rows in the table
+    Delete              DELETE              Removes zero or more rows from the table
+
+     */
+
+
+}
