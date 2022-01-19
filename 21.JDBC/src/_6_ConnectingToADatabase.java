6e2a373ecad6549c65682b415a02946cfd5340b8
Borja Neumann <borjaneumann@hotmail.com>
2022-01-19 09:19:56 +0100

    JDBC: Connecting to a database, building a JDBC URL

---
+++ 21.JDBC/src/_6_ConnectingToADatabase.java
@@ -0,0 +1,27 @@
+public class _6_ConnectingToADatabase {
+
+    /*
+    The first step in doing anything with a database is connecting to it. First,
+    we will show you how to build the JDBC URL. Then, we will show you
+    how to use it to get a Connection to the database.
+
+    BUILDING A JDBC URL
+    ===================
+    To access a website, you need to know the URL of the website. To access
+    your email, you need to know your username and password. JDBC is no
+    different. To access a database, you need to know this information about
+    it.
+
+    A JDBC URL has a variety of formats. They have three
+    parts in common.
+    The first piece is always the same. It is the protocol jdbc.
+    The second part is the subprotocol, which is the name of the database such as derby, mysql, or postgres.
+    The third part is the subname, which is a database‐specific format.
+
+    Colons ( :) separate the three parts.
+
+    jdbc:postgres://localhost:5432/zoo
+
+     */
+
+}
