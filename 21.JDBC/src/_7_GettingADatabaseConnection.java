c60b7f6b37b167285af5d3d2078ccd580c6dda73
Borja Neumann <borjaneumann@hotmail.com>
2022-01-19 11:16:43 +0100

    JDBC: Getting A Database Connection

--- 21.JDBC/src/_7_GettingADatabaseConnection.java
+++ 21.JDBC/src/_7_GettingADatabaseConnection.java
@@ -1,3 +1,7 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class _7_GettingADatabaseConnection {
 
     /*
@@ -24,8 +28,53 @@ public class _7_GettingADatabaseConnection {
     2) A DataSource maintains a connection pool so that you can keep reusing the
     same connection rather than needing to get a new one each time.
     Even the Javadoc says DataSource is preferred over DriverManager.
+     */
 
+    /*The DriverManager class is in the JDK, as it is an API that comes with
+            Java. It uses the factory pattern, which means that you call a static
+    method to get a Connection, rather than calling a constructor. The factory
+    pattern means that you can get any implementation of the interface when
+calling the method. The good news is that the method has an easy‐to‐
+    remember name— getConnection().
+    To get a Connection from the Derby database, you write the following:
+            import java.sql.*;
 
      */
+        public static void main(String[] args) throws SQLException {
+
+            Connection conn = DriverManager.getConnection("jdbc:derby:zoo");
+            System.out.println(conn);
+        }
+    /*
+    Exception in thread "main" java.sql.SQLException: No suitable driver found for jdbc:derby:zoo
+	at java.sql/java.sql.DriverManager.getConnection(DriverManager.java:702)
+	at java.sql/java.sql.DriverManager.getConnection(DriverManager.java:251)
+	at _7_GettingADatabaseConnection$TestConnect.main(_7_GettingADatabaseConnection.java:46)
+
+	 SQLException is a checked exception.
+
+	 java -cp "C:\db-derby-10.15.2.0-bin\db-derby-10.15.2.0-bin\lib\derby.jar" _7_GettingADatabaseConnection.java
+
+     it will return: org.apache.derby.impl.jdbc.EmbedConnection@1979080261 (XID = 210), (SESSIONID = 1), (DATABASE = zoo), (DRDAID = null)
+
+    There is also a signature that takes a username and password.
+    */
+//    public static void main(String[] args) throws SQLException {
+//        Connection conn = DriverManager.getConnection(
+//        "jdbc:postgresql://localhost:5432/ocp-book",
+//        "username",
+//        "Password20182");
+//        System.out.println(conn); //org.postgresql.jdbc4.Jdbc4Connection@eed1f14
+//    }
+    /*
+    Unless the exam specifies a command line, you can assume that the
+    correct JDBC driver JAR is in the classpath. The exam creators explicitly
+    ask about the driver JAR if they want you to think about it.
+    The nice thing about the factory pattern is that it takes care of the logic of
+    creating a class for you. You don't need to know the name of the class that
+    implements Connection, and you don't need to know how it is created.
+    You are probably a bit curious, though.
+     */
+
 
 }
