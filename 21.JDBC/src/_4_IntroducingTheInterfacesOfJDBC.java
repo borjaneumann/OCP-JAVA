873ba327180274584d985b07407afde07c90bbcd
Borja Neumann <borjaneumann@hotmail.com>
2022-01-19 09:12:23 +0100

    JDBC: 5 Key Interfaces example

--- 21.JDBC/src/_4_IntroducingTheInterfacesOfJDBC.java
+++ 21.JDBC/src/_4_IntroducingTheInterfacesOfJDBC.java
@@ -1,3 +1,9 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class _4_IntroducingTheInterfacesOfJDBC {
     /*
     For the exam you need to know five key interfaces of JDBC.
@@ -26,5 +32,29 @@ public class _4_IntroducingTheInterfacesOfJDBC {
     All database interfaces are in the package java.sql, so we will often omit
     the imports.
 
+    In this next example, we show you what JDBC code looks like end to end.
+    If you are new to JDBC, just notice that three of the five interfaces are in
+    the code. If you are experienced, remember that the exam uses
+    DriverManager instead of DataSource.
+    */
+    public static class MyFirstDatabaseConnection {
+        public static void main(String[] args) throws SQLException {
+            String url = "jdbc:derby:zoo";
+            try (Connection conn = DriverManager.getConnection(url);
+                 PreparedStatement ps = conn.prepareStatement(
+            "SELECT name FROM animal");
+                 ResultSet rs = ps.executeQuery()) {
+            while (rs.next())
+            System.out.println(rs.getString(1));
+            }
+        }
+    }
+    /*
+    If the URL were using our imaginary Foo driver, DriverManager would
+    return an instance of FooConnection. Calling prepareStatement() would
+    then return an instance of FooPreparedStatement, and calling
+    executeQuery() would return an instance of FooResultSet. Since the
+    URL uses derby instead, it returns the implementations that derby has
+    provided for these interfaces.
      */
 }
