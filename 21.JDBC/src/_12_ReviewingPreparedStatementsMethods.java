0c2dc82f734a5391f368a99159c448e923e3b3b8
Borja Neumann <borjaneumann@hotmail.com>
2022-01-20 09:27:59 +0100

    Reviewing Prepared Statements Methods

---
+++ 21.JDBC/src/_12_ReviewingPreparedStatementsMethods.java
@@ -0,0 +1,18 @@
+public class _12_ReviewingPreparedStatementsMethods {
+    //Important!!!
+    /*
+    SQL runnable by the execute method
+    Method              DELETE      INSERT      SELECT      UPDATE
+    ===============================================================
+    ps.execute()        Yes         Yes         Yes         Yes
+    ps.executeQuery()   No          No          Yes         No
+    ps.executeUpdate()  Yes         Yes         No          Yes
+
+    Return types of execute methods
+    Method              Return type What is returned for SELECT     What is returned for DELETE/INSERT/UPDATE
+    =========================================================================================================
+    ps.execute()        boolean     true                            false
+    ps.executeQuery()   ResultSet   The rows and columns returned   n/a
+    ps.executeUpdate()  int         n/a                             Number of rows added/changed/removed
+     */
+}
