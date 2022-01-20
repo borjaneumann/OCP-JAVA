f369743649c2840ddc2921dac34a157308fa36b3
Borja Neumann <borjaneumann@hotmail.com>
2022-01-20 12:05:11 +0100

    Working With ResultSet() Column Numbers

--- 21.JDBC/src/_14_GettingDataFromAResultSet.java
+++ 21.JDBC/src/_14_GettingDataFromAResultSet.java
@@ -36,5 +36,46 @@ public class _14_GettingDataFromAResultSet {
     row in the ResultSet.
     Lines 22 and 23 show you the best way to get the columns for a given row.
 
+    A ResultSet has a cursor, which points to the current location in the data.
+    The cursor starts out pointing to the location before the first row
+    in the ResultSet. On the first loop iteration, rs.next() returns true, and
+    the cursor moves to point to the first row of data. On the second loop
+    iteration, rs.next() returns true again, and the cursor moves to point to
+    the second row of data. The next call to rs.next() returns false. The
+    cursor advances past the end of the data. The false signifies that there is
+    no more data available to get.
+
+    We did say the “best way.” There is another way to access the columns.
+    You can use an index instead of a column name. The column name is
+    better because it is clearer what is going on when reading the code. It also
+    allows you to change the SQL to reorder the columns.
+    On the exam, either you will be told the names of the columns in a
+    table or you can assume that they are correct. Similarly, you can
+    assume that all SQL is correct.
+
+    Rewriting this same example with column numbers looks like the
+    following:
+    */
+    public static void workingWithResultSetColumnNumbers(Connection conn) {
+
+        String sql = "SELECT id, name FROM exhibits";
+        Map<Integer, String> idToNameMap = new HashMap<>();
+
+        try (var ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                idToNameMap.put(id, name);
+            }
+            System.out.println(idToNameMap);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    /*
+
+
      */
 }
