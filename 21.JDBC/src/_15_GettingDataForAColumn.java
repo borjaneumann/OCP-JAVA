1a04819114eb71a21d732459f7071e69b4f696d6
Borja Neumann <borjaneumann@hotmail.com>
2022-01-21 09:40:45 +0100

    ResultSet get methods

---
+++ 21.JDBC/src/_15_GettingDataForAColumn.java
@@ -0,0 +1,16 @@
+public class _15_GettingDataForAColumn {
+    /*
+    There are lots of get methods on the ResultSet interface.
+    These are the getter equivalents of the setters in the previous table
+
+    ResultSet get methods
+    Method name             Return type
+    ====================================
+    getBoolean              boolean
+    getDouble               double
+    getInt                  int
+    getLong                 long
+    getObject               Object
+    getString               String
+     */
+}
