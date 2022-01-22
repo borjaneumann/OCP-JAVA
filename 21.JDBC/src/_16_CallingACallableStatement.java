1883c89c94f26a50a88f2fbe04fff32efd4fa546
Borja <borjaneumann@hotmail.com>
2022-01-22 20:07:18 +0100

    Returning An Out Parameter

--- 21.JDBC/src/_16_CallingACallableStatement.java
+++ 21.JDBC/src/_16_CallingACallableStatement.java
@@ -1,7 +1,4 @@
-import java.sql.CallableStatement;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class _16_CallingACallableStatement {
 
@@ -81,6 +78,33 @@ public class _16_CallingACallableStatement {
     with 1) or the parameter name. That means these two statements are equivalent:
     cs.setString(1, "Z");
     cs.setString("prefix", "Z");
-
      */
+    /*
+    RETURNING AN OUT PARAMETER
+    ==========================
+    In our previous examples, we returned a ResultSet. Some stored
+    procedures return other information. Luckily, stored procedures can have
+    OUT parameters for output. The magic_number() stored procedure sets its
+    OUT parameter to 42. There are a few differences here:
+    */
+    public static void returningAnOutParameter(Connection conn) throws SQLException {
+        var sql ="{?= call magic_number(?).}"; //( ?=) to specify that the
+        //stored procedure has an output value. This is optional since we have the
+        //OUT parameter, but it does aid in readability.
+        try (var cs = conn.prepareCall(sql)) {
+            cs.registerOutParameter(1, Types.INTEGER); //we register the OUT parameter. This is important. It allows
+            //JDBC to retrieve the value on line 98
+            cs.execute();
+            System.out.println(cs.getInt("num"));
+        }
+    }
+    /*
+    Remember to always call registerOutParameter() for each OUT or INOUT parameter (which we will
+    cover next). We call execute() instead of executeQuery() since we are not
+    returning a ResultSet from our stored procedure.
+     */
+
+
+
+
 }
