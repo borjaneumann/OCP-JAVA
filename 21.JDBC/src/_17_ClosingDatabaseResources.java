3b79f363993841556edddbe0e092946010ef81d6
Borja Neumann <borjaneumann@hotmail.com>
2022-01-24 10:16:37 +0100

    Writing A Resource Leak: Bad code

--- 21.JDBC/src/_17_ClosingDatabaseResources.java
+++ 21.JDBC/src/_17_ClosingDatabaseResources.java
@@ -1,3 +1,6 @@
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class _17_ClosingDatabaseResources {
     /*
     it is important to close resources when you are finished with them. This is true for JDBC
@@ -13,6 +16,28 @@ public class _17_ClosingDatabaseResources {
 
     It is important to close resources in the right order. This avoids both
     resource leaks and exceptions.
+
+    WRITING A RESOURCE LEAK
+    =======================
+    you learned that it is possible to declare a type before a try‐with‐resources statement. Do you see why this method is bad?
+    */
+    public void bad() throws SQLException {
+        var url = "jdbc:derby:zoo";
+        var sql = "SELECT not_a_column FROM names";
+        var conn = DriverManager.getConnection(url);
+        var ps = conn.prepareStatement(sql);
+        var rs = ps.executeQuery(); ///Suppose an exception is thrown on this line. The try‐with‐resources
+        //block is never entered, so we don't benefit from automatic resource closing.
+        // That means this code has a resource leak if it fails. Do not write code like this.
+
+        try (conn; ps; rs) {
+            while (rs.next()){
+                System.out.println(rs.getString(1));
+            }
+        }
+    }
+    /*
+
 
      */
 }
