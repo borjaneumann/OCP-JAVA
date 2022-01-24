baf77fc5df0ff05350ff3c3a3ddcf08043fdcfc5
Borja Neumann <borjaneumann@hotmail.com>
2022-01-24 13:11:41 +0100

    Designing a Secure Object: Restricting Accessibility marking final

--- 22.Security/src/_01_DesigningASecureObject.java
+++ 22.Security/src/_01_DesigningASecureObject.java
@@ -1,6 +1,7 @@
 import java.util.Map;
 
 public class _01_DesigningASecureObject {
+
     /*
     Java provides us with many tools to protect the objects that we create. In
     this section, we will look at access control, extensibility, validation, and
@@ -23,28 +24,74 @@ public class _01_DesigningASecureObject {
     It would be better to make the combos object
     private and write a method to provide the necessary functionality.
 
-    package animals.security;
+     package animals.security;
     */
     public class ComboLocks {
         private Map<String, String> combos;
+
         public boolean isComboValid(String animal, String combo) {
-        var correctCombo = combos.get(animal);
-        return combo.equals(correctCombo);
+            var correctCombo = combos.get(animal);
+            return combo.equals(correctCombo);
         }
     }
     /*
     This is far better; we don't expose the combinations map to any classes
     outside the ComboLocks class. For example, package‐private is better than
     public, and private is better than package‐private.
-     */
-    /*
+
     If your application is using modules, you can do even better by only exporting
     the security packages to the specific modules that should have access. Here's an example:
 
     exports animals.security to zoo.staff;
-    In this example, only the zoo.staff module can access the public classes
-    in the animals.security package.
+
+    In this example, only the zoo.staff module can access the public classes in the animals.security package.
+     */
+
+
+    /*
+    RESTRICTING EXTENSIBILITY
+    =========================
+    Marking a sensitive class as final.
+    Suppose you are working on a class that uses ComboLocks.
+    */
+    public static class GrasshopperCage {
+        public static void openLock(ComboLocks comboLocks, String
+                combo) {
+            if (comboLocks.isComboValid("grasshopper", combo))
+                System.out.println("Open!");
+        }
+    }
+    /*
+    Ideally, the first variable passed to this method is an instance of the
+    ComboLocks class. However, Hacker Harry is hard at work and has created
+    this subclass of ComboLocks.
+    */
+    public class EvilComboLocks extends ComboLocks {
+        public boolean isComboValid(String animal, String combo) {
+            var valid = super.isComboValid(animal, combo);
+            if (valid) {
+                // email the password to Hacker Harry
+            }
+            return valid;
+        }
+    }
+    /*
+    Marking a sensitive class as final prevents any subclasses.
 
      */
+
+    public final class ComboLocksWithFinal {
+        private Map<String, String> combos;
+        // instantiate combos object
+        public boolean isComboValid(String animal, String combo) {
+            var correctCombo = combos.get(animal);
+            return combo.equals(correctCombo);
+        }
+    }
+    /*
+    Hacker Harry can't create his evil class, and users of the GrasshopperCage have the assurance
+    that only the expected ComboLocks class can make an appearance.
+     */
+
 
 }
