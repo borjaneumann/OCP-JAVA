450decc5da19d3182248d3d0983ca5a7bd74d031
Borja Neumann <borjaneumann@hotmail.com>
2022-01-25 10:58:28 +0100

    IntroducingInjectionAndInputValidation: Preventing Injection With A prepareStatement(createStatement issue)

--- 22.Security/src/_2_IntroducingInjectionAndInputValidation.java
+++ 22.Security/src/_2_IntroducingInjectionAndInputValidation.java
@@ -14,7 +14,42 @@ public class _2_IntroducingInjectionAndInputValidation {
     database. In the real world, any data that did not originate from your
     program should be considered suspect.
 
+    PREVENTING INJECTION WITH A PREPAREDSTATEMENT
+    =============================================
+    Using Statement
 
+    We wrote a method that uses a Statement. We didn't use Statement because it is often unsafe.
+
+    public int getOpening(Connection conn, String day) throws SQLException {
+        String sql = "SELECT opens FROM hours WHERE day = '" + day
+        +"'";
+        try (var stmt = conn.createStatement();
+        var rs = stmt.executeQuery(sql)) {
+            if (rs.next())
+            return rs.getInt("opens");
+        }
+        return -1;
+    }
+    Then, we call the code with one of the days in the table.
+
+    int opening = attack.getOpening(conn, "monday"); // 10
+
+    So far, so good. Then Hacker Harry comes along to call the method. He writes this:
+
+    int evil = attack.getOpening(conn,
+    "monday' OR day IS NOT NULL OR day = 'sunday"); // 9
+
+    Hacker Harry's parameter results in the following SQL, which we've
+    formatted for readability:
+    SELECT opens FROM hours
+    WHERE day = 'monday'
+    OR day IS NOT NULL
+    OR day = 'sunday'
+
+    It says to return any rows where day is sunday, monday, or any value that
+    isn't null. Since none of the values in Figure 22.2 is null, this means all
+    the rows are returned. Luckily, the database is kind enough to return the
+    rows in the order they were inserted; our code reads the first row.
      */
 
 }
