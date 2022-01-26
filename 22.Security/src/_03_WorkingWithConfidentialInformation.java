9f16988cdefaba689f15b9fc8843379b0217fde2
Borja Neumann <borjaneumann@hotmail.com>
2022-01-26 09:18:45 +0100

    Working With Confidential Information: Defense in depth(security policy)

--- 22.Security/src/_03_WorkingWithConfidentialInformation.java
+++ 22.Security/src/_03_WorkingWithConfidentialInformation.java
@@ -54,6 +54,39 @@ public class _03_WorkingWithConfidentialInformation {
     The idea is to have confidential data in memory for as short a time as possible.
 
 
+    DEFENSE IN DEPTH
+    ================
+
+    It is good to apply multiple techniques to protect your application.
+    This approach is called defense in depth. If Hacker Harry gets
+    through one of your defenses, he still doesn't get the valuable
+    information inside. Instead, he is met with another defense.
+    Validation and using a security policy are good techniques to use
+    together to apply defense in depth.
+
+    For the exam you need to be able to read one to understand security implications. Luckily,
+    they are fairly self‐explanatory. Here's an example of a policy:
+
+    grant {
+        permission java.io.FilePermission
+            "C:\\water\\fish.txt",
+            "read";
+    };
+
+    This policy gives the programmer permission to read, but not update, the
+    fish.txt file. If the program is allowed to read and write the file, we specify the following:
+
+    grant {
+        permission java.io.FilePermission
+            "C:\\water\\fish.txt",
+            "read, write";
+    };
+
+    When looking at a policy, pay attention to whether the policy grants
+    access to more than is needed to run the program. If our application needs
+    to read a file, it should only have read permissions. This is the principle of
+    least privilege we showed you earlier.
+
 
      */
 
