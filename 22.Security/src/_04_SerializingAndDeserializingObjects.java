9c7bcb05f73d8095076e3ac25a9b27d1ce005486
Borja Neumann <borjaneumann@hotmail.com>
2022-01-26 11:40:11 +0100

    Serializing And Deserializing Objects: Applying writeReplace()

--- 22.Security/src/_04_SerializingAndDeserializingObjects.java
+++ 22.Security/src/_04_SerializingAndDeserializingObjects.java
@@ -202,6 +202,37 @@ public class _04_SerializingAndDeserializingObjects {
     allows any method modifiers (except static) for the readResolve()
     method including any access modifier. This rule applies to
     writeReplace(), which is up next.
+     */
+
+    /*
+    Applying writeReplace()
+    =======================
+    Now, what if we want to write an Employee record to disk but we don't
+    completely trust the instance we are holding? For example, we want to
+    always write the version of the object in the pool rather than the this
+    instance. By construction, there should be only one version of this object
+    in memory, but for this example let's pretend we're not 100 percent
+    confident of that.
+    The writeReplace() method is run before writeObject() and allows us to replace the object that gets serialized.
+
+    public class Employee implements Serializable {
+    …
+        public Object writeReplace() throws ObjectStreamException {
+            var e = pool.get(name);
+            return e != null ? e : this;
+        }
+    }
+
+    This implementation checks whether the object is found in the pool. If it is
+    found in the pool, that version is sent for serialization; otherwise, the
+    current instance is used. We could also update this example to add it to the
+    pool if it is somehow missing.
+
+    If these last few examples seemed a bit contrived, it's because they
+    are. While the exam is likely to test you on these methods,
+    implementing these advanced serialization methods in detail is way
+    beyond the scope of the exam. Besides, transient will probably
+    meet your needs for customizing what gets serialized.
 
      */
 
