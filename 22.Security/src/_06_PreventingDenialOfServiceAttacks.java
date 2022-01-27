3a57c2146495b189d4bdb8c8be181bdc847f8400
Borja Neumann <borjaneumann@hotmail.com>
2022-01-27 13:51:41 +0100

    Preventing Denial Of Service Attacks: Reading Very Large Resources

--- 22.Security/src/_06_PreventingDenialOfServiceAttacks.java
+++ 22.Security/src/_06_PreventingDenialOfServiceAttacks.java
@@ -40,5 +40,23 @@ public class _06_PreventingDenialOfServiceAttacks {
             return stream.count();
         }
     }
+    /*
+    READING VERY LARGE RESOURCES
+    ============================
+    Another source of a denial of service attacks is very large resources.
+    Suppose we have a simple method that reads a file into memory, does
+    some transformations on it, and writes it to a new file.
+     */
+    public void transform( Path in, Path out) throws IOException {
+        var list = Files.readAllLines(in);
+        list.removeIf(s -> s.trim().isBlank());
+        Files.write (out, list);
+    }
+    /*
+    On a small file, this works just fine. However, on an extremely large file,
+    your program could run out of memory and crash. Hacker Harry strikes
+    again! To prevent this problem, you can check the size of the file before
+    reading it.
+     */
 
 }
