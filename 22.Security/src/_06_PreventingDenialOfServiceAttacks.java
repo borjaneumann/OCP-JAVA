5007798a3edb658129320c0b80e7fe6f92e9d3f2
Borja Neumann <borjaneumann@hotmail.com>
2022-01-27 13:33:59 +0100

    Preventing Denial Of Service Attacks: Intro

---
+++ 22.Security/src/_06_PreventingDenialOfServiceAttacks.java
@@ -0,0 +1,20 @@
+public class _06_PreventingDenialOfServiceAttacks {
+
+    /*
+    Intro
+    =====
+    (DoS) - A denial of service (DoS) attack is when a hacker makes one or more
+    requests with the intent of disrupting legitimate requests. Most denial of
+    service attacks require multiple requests to bring down their targets.
+    Some attacks send a very large request that can even bring down the application
+    in one shot. In this book, we will focus on denial of service attacks.
+    Unless otherwise specified, a denial of service attack comes from one
+    machine. It may make many requests, but they have the same origin.
+
+    (DDoS) - By contrast, a distributed denial of service (DDoS) attack is a denial of
+    service attack that comes from many sources at once. For example, many
+    machines may attack the target. In this section, we will look at some
+    common sources of denial of service issues.
+     */
+
+}
