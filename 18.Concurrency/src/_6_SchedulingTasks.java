4d8f9422a821bd98c222e77ac340884f8a438cf4
Borja Neumann <borjaneumann@hotmail.com>
2022-01-31 15:30:55 +0100

    Concurrency: Scheduling tasks intro

---
+++ 18.Concurrency/src/_6_SchedulingTasks.java
@@ -0,0 +1,16 @@
+public class _6_SchedulingTasks {
+    /*
+    Oftentimes in Java, we need to schedule a task to happen at some future
+    time. We might even need to schedule the task to happen repeatedly, at
+    some set interval.
+    Like ExecutorService, we obtain an instance of
+    ScheduledExecutorService using a factory method in the Executors
+    class, as shown in the following snippet:
+    ScheduledExecutorService service
+    = Executors.newSingleThreadScheduledExecutor();
+    We could store an instance of ScheduledExecutorService in an
+    ExecutorService variable, although doing so would mean we'd have to
+    cast the object to call any scheduled methods.
+
+     */
+}
