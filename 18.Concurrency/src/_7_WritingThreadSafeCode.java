1c9f6013f01769ba61fd7d9f0e44a022d0e56f37
Borja Neumann <borjaneumann@hotmail.com>
2022-02-01 10:53:29 +0100

    Concurrency: Writing Thread Safe Code Intro

---
+++ 18.Concurrency/src/_7_WritingThreadSafeCode.java
@@ -0,0 +1,9 @@
+public class _7_WritingThreadSafeCode {
+
+    /*Thread‐safety is the property of an object that guarantees safe execution
+    by multiple threads at the same time. Since threads run in a shared
+    environment and memory space, how do we prevent two threads from
+    interfering with each other? We must organize access to data so that we
+    don't end up with invalid or unexpected results.*/
+
+}
