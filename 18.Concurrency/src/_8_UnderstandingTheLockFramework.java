c098863426c970625b8843c8d336481271927007
Borja Neumann <borjaneumann@hotmail.com>
2022-02-03 15:05:14 +0100

    Orchestrating Tasks With A CyclicBarrier implementation

--- 18.Concurrency/src/_8_UnderstandingTheLockFramework.java
+++ 18.Concurrency/src/_8_UnderstandingTheLockFramework.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -276,9 +278,37 @@ public class _8_UnderstandingTheLockFramework {
     number of threads to wait for. As each thread finishes, it calls the await()
     method on the cyclic barrier. Once the specified number of threads have
     each called await(), the barrier is released, and all threads can continue.
-
-     */
-
+    */
+    public static class LionPenManager1 {
+        private void removeLions() {System.out.println("Removing  lions");}
+        private void cleanPen() {System.out.println("Cleaning the  pen");}
+        private void addLions() {System.out.println("Adding lions");}
+        public void performTask(CyclicBarrier c1, CyclicBarrier c2) {
+            try {
+            removeLions();
+            c1.await();
+            cleanPen();
+            c2.await();
+            addLions();
+            } catch (InterruptedException | BrokenBarrierException e) {
+    // Handle checked exceptions here
+            }
+        }
+    public static void main(String[] args) {
+        ExecutorService service = null;
+            try {
+                service = Executors.newFixedThreadPool(4);
+                var manager = new LionPenManager1();
+                var c1 = new CyclicBarrier(4);
+                var c2 = new CyclicBarrier(4,
+                () -> System.out.println("*** Pen Cleaned!"));
+                for (int i = 0; i < 4; i++)
+                service.submit(() -> manager.performTask(c1, c2));
+            } finally {
+                if (service != null) service.shutdown();
+            }
+        }
+    }
 }
 
 
