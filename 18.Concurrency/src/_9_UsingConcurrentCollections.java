71ed63bd814fb3253984f3ca2ff576faec0ab2a6
Borja Neumann <borjaneumann@hotmail.com>
2022-02-04 09:28:23 +0100

    Using Concurrent Collections Intro

---
+++ 18.Concurrency/src/_9_UsingConcurrentCollections.java
@@ -0,0 +1,15 @@
+public class _9_UsingConcurrentCollections {
+
+    /*
+    Intro
+    =====
+    Besides managing threads, the Concurrency API includes interfaces and
+    classes that help you coordinate access to collections shared by multiple
+    tasks. By collections, we are of course referring to the Java Collections
+    Framework that we introduced in Chapter 14, “Generics and Collections.”
+    In this section, we will demonstrate many of the concurrent classes
+    available to you when using the Concurrency API.
+
+     */
+
+}
