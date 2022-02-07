76cf7270525aa42bbcbe9b36897a9feca11cfbb4
Borja Neumann <borjaneumann@hotmail.com>
2022-02-07 11:44:03 +0100

    Starvation

--- 18.Concurrency/src/_10_IdentifyingThreadingProblems.java
+++ 18.Concurrency/src/_10_IdentifyingThreadingProblems.java
@@ -108,7 +108,7 @@ public class _10_IdentifyingThreadingProblems {
     available.
 
     PREVENTING DEADLOCKS
-    ====================
+
     How do you fix a deadlock once it has occurred? The answer is that
     you can't in most situations.
 
@@ -120,5 +120,22 @@ public class _10_IdentifyingThreadingProblems {
     For example, if both foxes have a rule
     that they need to obtain food before water, then the previous
     deadlock scenario will not happen again.
+     */
+
+    /*
+    Starvation
+    ==========
+    Starvation occurs when a single thread is perpetually denied access to a
+    shared resource or lock. The thread is still active, but it is unable to
+    complete its work as a result of other threads constantly taking the
+    resource that they are trying to access.
+    In our fox example, imagine that we have a pack of very hungry, very
+    competitive foxes in our environment. Every time Foxy stands up to go
+    get food, one of the other foxes sees her and rushes to eat before her. Foxy
+    is free to roam around the enclosure, take a nap, and howl for a zookeeper
+    but is never able to obtain access to the food. In this example, Foxy
+    literally and figuratively experiences starvation. It's a good thing that this
+    is just a theoretical example!
+
      */
 }
