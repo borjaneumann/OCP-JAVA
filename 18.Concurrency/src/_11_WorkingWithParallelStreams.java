4ecfdec9512cbf2430a23eba12a9a30b012280da
Borja Neumann <borjaneumann@hotmail.com>
2022-02-08 10:32:01 +0100

    Parallel streams Intro

--- 18.Concurrency/src/_11_WorkingWithParallelStreams.java
+++ 18.Concurrency/src/_11_WorkingWithParallelStreams.java
@@ -1,2 +1,22 @@
 public class _11_WorkingWithParallelStreams {
+    /*
+    One of the most powerful features of the Stream API is built‐in
+    concurrency support. Up until now, all of the streams with which you have
+    worked have been serial streams.
+
+    A serial stream is a stream in which the
+    results are ordered, with only one entry being processed at a time.
+    A parallel stream is a stream that is capable of processing results
+    concurrently (at the same time), using multiple threads.
+
+    Using a parallel stream can change not only the performance of your
+    application but also the expected results. As you shall see, some
+    operations also require special handling to be able to be processed in a
+    parallel manner.
+
+    The number of threads available in a parallel stream is proportional
+    to the number of available CPUs in your environment.
+
+
+     */
 }
