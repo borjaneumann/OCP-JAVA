8353e7716249734ad91ad6bb2be5301de94fd572
Borja Neumann <borjaneumann@hotmail.com>
2022-02-08 10:22:31 +0100

    lambda: calculator interface implementation

---
+++ 06.LambdasAndFunctionalInterfaces/src/_0_calculatorExample/Operations.java
@@ -0,0 +1,30 @@
+package _0_calculatorExample;
+
+public class Operations {
+    public static void main(String[] args) {
+        ICalculator addition = (a,b)->{
+          double result = a + b;
+            System.out.println("addition: " + result);
+        };
+        addition.operation(5,3);
+
+        ICalculator substraction = (a,b)->{
+            double result = a - b;
+            System.out.println("substraction: " + result);
+        };
+        substraction.operation(5,3);
+
+        ICalculator multiply = (a,b)->{
+            double result = a * b;
+            System.out.println("Multiplication: " + result);
+        };
+        multiply.operation(5,3);
+
+        ICalculator division = (a,b)->{
+            double result = a / b;
+            System.out.println("division: " + result);
+        };
+        division.operation(5,3);
+    }
+
+}
