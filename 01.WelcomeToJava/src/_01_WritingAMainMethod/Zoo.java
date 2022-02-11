1ff30445009a89db8033227ca2ffc17b2c8c9689
Borja <borjaneumann@hotmail.com>
2022-02-11 16:56:01 +0100

    Compiling a java class

---
+++ 01.WelcomeToJava/src/_01_WritingAMainMethod/Zoo.java
@@ -0,0 +1,11 @@
+package _01_WritingAMainMethod;
+
+public class Zoo {
+    public static void main(String[] args) {
+        System.out.println("It works");
+    }
+}
+
+//javac Zoo.java
+//java Zoo
+//If the file is in package the name package/s have to be included
