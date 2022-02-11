5ea8e2a7c8c442cf302b39ae24a934a22be4a92a
Borja <borjaneumann@hotmail.com>
2022-02-11 16:17:06 +0100

    JRE

---
+++ 01.WelcomeToJava/src/_01_WritingAMainMethod/_01_JRE.java
@@ -0,0 +1,21 @@
+package _01_WritingAMainMethod;
+
+public class _01_JRE {
+    /*
+    WHERE DID THE JRE GO?
+    =====================
+    In previous versions of Java, you could download a Java Runtime
+    Environment (JRE) instead of the full JDK. The JRE was a subset of
+    the JDK that was used for running a program but could not compile
+    one. It was literally a subset. In fact, if you looked inside the
+    directory structure of a JDK in older versions of Java, you would see
+    a folder named jre.
+    In Java 11, the JRE is no longer available as a stand-alone download
+    or a subdirectory of the JDK. People can use the full JDK when
+    running a Java program. Alternatively, developers can supply an
+    executable that contains the required pieces that would have been in
+    the JRE. The jlink command creates this executable.
+    While the JRE is not in scope for the exam, knowing what changed
+    may help you eliminate wrong answers.
+     */
+}
