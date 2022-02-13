fa1c61effd3c2a990964bc87ceb381de5aa17e44
Borja <borjaneumann@hotmail.com>
2022-02-13 13:21:50 +0100

    Running Program With One Command

---
+++ 01.WelcomeToJava/src/_03_RunningAProgramWithOneLine/Learning.java
@@ -0,0 +1,22 @@
+package _03_RunningAProgramWithOneLine;
+
+public class Learning {
+    public static void main(String[] args) {
+//        ehhh; //DOES NOT COMPILE
+        System.out.println("This works");
+    }
+}
+/*
+Learning.java:5: error: not a statement
+        ehhh; //DOES NOT COMPILE
+        ^
+1 error
+error: compilation failed
+ */
+
+/*
+Notice how we said “in memory.” Even if the code compiles properly, no .class file is created.
+ */
+
+
+
