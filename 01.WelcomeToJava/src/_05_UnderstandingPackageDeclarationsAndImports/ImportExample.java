ab89e72e1bea900f877282cc5700e0c6d8562dab
Borja <borjaneumann@hotmail.com>
2022-02-14 17:10:44 +0100

    Imports that don't work

--- 01.WelcomeToJava/src/_05_UnderstandingPackageDeclarationsAndImports/ImportExample.java
+++ 01.WelcomeToJava/src/_05_UnderstandingPackageDeclarationsAndImports/ImportExample.java
@@ -42,4 +42,13 @@ The other answer is to import both classes explicitly.
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+Now let’s consider some imports that don’t work.
+
+import java.nio.*;      // NO GOOD - a wildcard only matches
+                        // class names, not "file.Files"
+import java.nio.*.*;    // NO GOOD - you can only have one wildcard
+                        // and it must be at the end
+import java.nio.file.Paths.*; // NO GOOD - you cannot import methods
+                        // only class names
+
  */
