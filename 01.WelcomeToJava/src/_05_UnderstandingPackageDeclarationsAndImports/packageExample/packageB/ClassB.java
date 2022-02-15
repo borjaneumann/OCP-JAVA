f9a7ddbb904e807074d3bf688ec4d931e52d8113
Borja <borjaneumann@hotmail.com>
2022-02-15 15:10:08 +0100

    Package example javac explanation

--- 01.WelcomeToJava/src/_05_UnderstandingPackageDeclarationsAndImports/packageExample/packageB/ClassB.java
+++ 01.WelcomeToJava/src/_05_UnderstandingPackageDeclarationsAndImports/packageExample/packageB/ClassB.java
@@ -8,3 +8,12 @@ public class ClassB {
         System.out.println("Got it");
     }
 }
+/*
+Very important:
+1) When compiling you need to be in the path above the packages are.
+2) javac 05_UnderstandingPackageDeclarationsAndImports.packageExample.packageB\ClassB.java
+I'm guessing that when entering the above command the path starts from C:\ to the package directory.
+3)java 05_UnderstandingPackageDeclarationsAndImports.packageExample.packageB.ClassB
+(Keep an eye on the dot between packageB and ClassB!!)
+
+ */
