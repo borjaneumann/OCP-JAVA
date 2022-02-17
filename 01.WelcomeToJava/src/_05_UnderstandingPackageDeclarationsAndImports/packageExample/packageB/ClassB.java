02e1eb54c4ed513a4e1449a11b1d6eb399f9eda9
Borja Neumann <borjaneumann@hotmail.com>
2022-02-17 10:24:09 +0100

    Compiling With JAR files

--- 01.WelcomeToJava/src/_05_UnderstandingPackageDeclarationsAndImports/packageExample/packageB/ClassB.java
+++ 01.WelcomeToJava/src/_05_UnderstandingPackageDeclarationsAndImports/packageExample/packageB/ClassB.java
@@ -143,4 +143,23 @@ class Conflicts1 {
     java.sql.Date sqlDate;
 }
 /*
+Compiling With JAR files
+========================
+ A Java archive (JAR) file is like a zip file of mainly Java class files.
+
+On Windows, you type the following:
+
+java -cp ".;C:\temp\someOtherLocation;c:\temp\myJar.jar"
+myPackage.MyClass
+
+The period (.) indicates you want to include the current directory in the
+classpath.
+
+Just like when you’re compiling, you can use a wildcard (*) to match all
+the JARs in a directory. Here’s an example:
+java -cp "C:\temp\directoryWithJars\*" myPackage.MyClass
+This command will add all the JARs to the classpath that are in
+directoryWithJars. It won’t include any JARs in the classpath that are in
+a subdirectory of directoryWithJars.
+
  */
