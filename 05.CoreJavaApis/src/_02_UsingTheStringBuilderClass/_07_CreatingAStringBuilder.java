1819be2f28def3bf7bc2da17365e2b872fc26a8b
Borja <borjaneumann@hotmail.com>
2022-02-27 20:35:55 +0100

    Adding CreatingAStringBuilder

---
+++ 05.CoreJavaApis/src/_02_UsingTheStringBuilderClass/_07_CreatingAStringBuilder.java
@@ -0,0 +1,16 @@
+package _02_UsingTheStringBuilderClass;
+
+public class _07_CreatingAStringBuilder {
+    public static void main(String[] args) {
+        //3 ways to create a StringBuilder
+
+        //the classic one containing an empty sequence of characters
+        StringBuilder sb1 =  new StringBuilder();
+
+        //Containing an specific sequence of characters
+        StringBuilder sb2 = new StringBuilder("stringBuilder");
+
+        //we only know how big the value is
+        StringBuilder sb3 = new StringBuilder(10);
+    }
+}
