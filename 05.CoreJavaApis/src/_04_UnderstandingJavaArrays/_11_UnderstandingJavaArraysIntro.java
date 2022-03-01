29b824051ad09c8bfe0dc308b7794adb208a328b
Borja <borjaneumann@hotmail.com>
2022-03-01 20:43:16 +0100

    Understanding Java Arrays Intro

---
+++ 05.CoreJavaApis/src/_04_UnderstandingJavaArrays/_11_UnderstandingJavaArraysIntro.java
@@ -0,0 +1,24 @@
+package _04_UnderstandingJavaArrays;
+
+public class _11_UnderstandingJavaArraysIntro {
+    /*Up to now, we’ve been referring to the String and StringBuilder classes
+    as a “sequence of characters.” This is true. They are implemented using an
+    array of characters.
+
+    An array is an area of memory on the heap with space
+    for a designated number of elements.
+
+    A String is implemented as an array with some methods that you might
+    want to use when dealing with characters specifically.
+
+    A StringBuilder is implemented as an array where the array object is
+    replaced with a new bigger array object when it runs out of space to store all the characters.
+
+    A big difference is that an array can be of any other Java type.
+
+    An array is an ordered list. It can contain duplicates. In this
+    section, we’ll look at creating an array of primitives and objects, sorting,
+    searching, varargs, and multidimensional arrays.
+     */
+
+}
