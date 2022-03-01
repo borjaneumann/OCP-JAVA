59fd35b2eef30eaed7476058cc58fa6e5b1a6e77
Borja <borjaneumann@hotmail.com>
2022-03-01 20:50:52 +0100

    Position of [] and multipes arrays declarations

--- 05.CoreJavaApis/src/_04_UnderstandingJavaArrays/_12_CreatingArrayOfPrimitives.java
+++ 05.CoreJavaApis/src/_04_UnderstandingJavaArrays/_12_CreatingArrayOfPrimitives.java
@@ -8,7 +8,9 @@ public class _12_CreatingArrayOfPrimitives {
         int[] numbers1 = new int[]{ 3,5,6}; //Specify all the elements it should start out with
         int[] numbers2 = {8,7,9}; //shortcut of the above
 
-
+        // you can type the [] before or after the name, and adding a space is
+        //optional. This means that all five of these statements do the exact same
+        //thing:
         int[] numAnimals;
         int [] numAnimals1;
         int []numAnimals2;
@@ -16,8 +18,8 @@ public class _12_CreatingArrayOfPrimitives {
         int numAnimals4 [];
 
         //multiples Arrays in declarations
-        int[] ids, types;
-        int ids1[], types2;
+        int[] ids, types; // ids[] and types[]
+        int ids1[], types2; //In this case we get ids1[] and int types2
     }
 
 
