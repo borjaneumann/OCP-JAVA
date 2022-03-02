fe5677b4e2d8a6934df963204f4afc2e21303038
Borja <borjaneumann@hotmail.com>
2022-03-02 19:38:43 +0100

    Rearranging Search extra info, differente scenarios

--- 05.CoreJavaApis/src/_04_UnderstandingJavaArrays/_16_Searching.java
+++ 05.CoreJavaApis/src/_04_UnderstandingJavaArrays/_16_Searching.java
@@ -4,6 +4,27 @@ import java.util.Arrays;
 
 public class _16_Searching {
     public static void main(String[] args) {
+
+        /*
+        Java also provides a convenient way to search—but only if the array is
+        already sorted.
+
+        Scenario            Result
+        -------------------------------------------------------
+        Target element      Index of match
+        found in sorted
+        array
+        ------------------------------------------------------
+        Target element      Negative value showing one smaller than the
+        not found in        negative of the index, where a match needs to be
+        sorted array        inserted to preserve sorted order
+        ---------------------------------------------------------
+        Unsorted array      A surprise—this result isn’t predictable
+
+        A binary search splits the array into two equal pieces
+        (remember 2 is binary) and determines which half the target is in. It
+        repeats this process until only one element is left.
+         */
         int[] numbers = {2,4,6,8};
         System.out.println(Arrays.binarySearch(numbers,2));
         System.out.println(Arrays.binarySearch(numbers,4));
