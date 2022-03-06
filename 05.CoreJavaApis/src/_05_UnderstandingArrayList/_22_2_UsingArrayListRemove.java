2de78ab2fe0c89c8ee53993f8760c04dc5d4bd11
Borja <borjaneumann@hotmail.com>
2022-03-06 17:29:10 +0100

    Reviewing remove() in ArrayList

--- 05.CoreJavaApis/src/_05_UnderstandingArrayList/_22_2_UsingArrayListRemove.java
+++ 05.CoreJavaApis/src/_05_UnderstandingArrayList/_22_2_UsingArrayListRemove.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 public class _22_2_UsingArrayListRemove {
     public static void main(String[] args) {
-        //boolean remove (Object object)
-        //E remove (int index)
+
+        //boolean remove (Object object) -> remove the first matching value in the ArrayList
+        //E remove (int index) -> remove the element at a specified index.
+
+        //This time the boolean return value tells us whether a match was removed.
+        //The E return type is the element that actually got removed.
 
         List<String> birds = new ArrayList<>();
         birds.add("hawk");
@@ -16,8 +20,12 @@ public class _22_2_UsingArrayListRemove {
         System.out.println(birds.remove(0));//removes hawk
         System.out.println(birds);//[]
 
+        /*
+        Since calling remove() with an int uses the index, an index that doesn’t
+        exist will throw an exception. For example, birds.remove(100) throws an
+        IndexOutOfBoundsException.
+        There is also a removeIf() method. We’ll cover it in the next chapter
+        because it uses lambda expressions (a topic in that chapter).
+         */
     }
-
-
-
 }
