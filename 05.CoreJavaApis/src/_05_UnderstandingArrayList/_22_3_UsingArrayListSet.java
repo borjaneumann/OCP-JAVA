19e9b78ba5e75fb8b3ded7a881e1f686711070d0
Borja <borjaneumann@hotmail.com>
2022-03-06 17:31:48 +0100

    Reviewing set() in ArrayList

--- 05.CoreJavaApis/src/_05_UnderstandingArrayList/_22_3_UsingArrayListSet.java
+++ 05.CoreJavaApis/src/_05_UnderstandingArrayList/_22_3_UsingArrayListSet.java
@@ -5,14 +5,15 @@ import java.util.List;
 
 public class _22_3_UsingArrayListSet {
 
-    //E set(int index, E newElement)
+    //E set(int index, E newElement) //Replace without changing the size
 
     public static void main(String[] args) {
+
         List<String> birds = new ArrayList<>();
         birds.add("blackbird");
-        System.out.println("Birds size: " + birds.size());
-        birds.set(0,"pigeon");
-        System.out.println("Birds size: " + birds.size());
+        System.out.println("Birds size: " + birds.size()); // Birds size: 1
+        birds.set(0, "pigeon");
+        System.out.println("Birds size: " + birds.size()); // Birds size: 1
         birds.set(1, "robin");//IndexOutOfBoundsException
     }
 }
