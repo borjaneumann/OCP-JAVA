79601710c01ae8b48dca087f042d39d32108df49
Borja <borjaneumann@hotmail.com>
2022-03-06 17:40:51 +0100

    Reviewing equals() in ArrayList

--- 05.CoreJavaApis/src/_05_UnderstandingArrayList/_22_7_UsingArrayListEquals.java
+++ 05.CoreJavaApis/src/_05_UnderstandingArrayList/_22_7_UsingArrayListEquals.java
@@ -7,17 +7,21 @@ public class _22_7_UsingArrayListEquals {
     public static void main(String[] args) {
 
         //boolean equals(Object object);
-        //Compare two lists to see whether they contain the same elements in the same order.
+
+        //Custom implementation of equals()
+        // Compare two lists to see whether they contain the same elements in the same order.
 
         List<String> one = new ArrayList<>();
         List<String> two = new ArrayList<>();
         System.out.println(one.equals(two));//true
+
         one.add("a");
         System.out.println(one.equals(two));//false
         two.add("a");
         System.out.println(one.equals(two));//true
+
         one.add("b");
         two.add(0,"b");
-        System.out.println(one.equals(two));//false
+        System.out.println(one.equals(two));//false, they are not in the same order
     }
 }
