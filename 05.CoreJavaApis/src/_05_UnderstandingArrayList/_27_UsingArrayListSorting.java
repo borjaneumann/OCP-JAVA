1a2c29bead28f1609c2c9566778d51f800f0cff8
Borja <borjaneumann@hotmail.com>
2022-03-06 22:18:32 +0100

    Reviewing sorting an ArrayList

--- 05.CoreJavaApis/src/_05_UnderstandingArrayList/_27_UsingArrayListSorting.java
+++ 05.CoreJavaApis/src/_05_UnderstandingArrayList/_27_UsingArrayListSorting.java
@@ -4,13 +4,15 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class UsingArrayListSorting {
+public class _27_UsingArrayListSorting {
     public static void main(String[] args) {
+        //Sorting an ArrayList is similar to sorting an array.
+        // You just use the collections interface.
         List<Integer> numbers = new ArrayList<>();
         numbers.add(99);
         numbers.add(9);
         numbers.add(109);
         Collections.sort(numbers);//it uses the collections interface.
-        System.out.println(numbers);
+        System.out.println(numbers);// [9, 99, 109]
     }
 }
