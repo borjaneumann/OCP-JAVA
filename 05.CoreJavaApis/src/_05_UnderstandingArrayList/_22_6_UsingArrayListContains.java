75a16ab7a50559770d234291b7cc2b221d5bec06
Borja <borjaneumann@hotmail.com>
2022-03-06 17:38:04 +0100

    Reviewing contains() in ArrayList

--- 05.CoreJavaApis/src/_05_UnderstandingArrayList/_22_6_UsingArrayListContains.java
+++ 05.CoreJavaApis/src/_05_UnderstandingArrayList/_22_6_UsingArrayListContains.java
@@ -6,10 +6,18 @@ import java.util.List;
 public class _22_6_UsingArrayListContains {
 
     public static void main(String[] args) {
-        //boolean contains(Object object);
+
+        //boolean contains(Object object); --> checks whether a certain value is in the
+        //ArrayList.
+
         List<String> birds = new ArrayList<>();
         birds.add("hawk");
         System.out.println(birds.contains("hawk"));//true
         System.out.println(birds.contains("robin"));//false
+
+        /*This method calls equals() on each element of the ArrayList to see
+        whether there are any matches. Since String implements equals(), this
+        works out well.
+         */
     }
 }
