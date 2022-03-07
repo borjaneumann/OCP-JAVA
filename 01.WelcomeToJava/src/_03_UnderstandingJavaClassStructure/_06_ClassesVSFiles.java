1109272ce8f8a48c865d3bc3767459c669864330
Borja <borjaneumann@hotmail.com>
2022-03-07 23:14:19 +0100

    Reviewing Classes VS Files

--- 01.WelcomeToJava/src/_03_UnderstandingJavaClassStructure/_06_ClassesVSFiles.java
+++ 01.WelcomeToJava/src/_03_UnderstandingJavaClassStructure/_06_ClassesVSFiles.java
@@ -1,8 +1,15 @@
 package _03_UnderstandingJavaClassStructure;
 
-public class ClassesVSFiles {
+public class _06_ClassesVSFiles {
+/*
+You can even put two classes in the same file. When you do so, at most
+one of the classes in the file is allowed to be public.
+
+If you do have a public class, it needs to match the filename
+ */
 
 }
+/*Interestingly, Java does not require that the class be public.*/
 class Animal {
-    String animal;
+    String name;
 }
