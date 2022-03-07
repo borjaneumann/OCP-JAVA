720cec5e4e96a803fa5c8ac4e0e55a462cf1bc91
Borja <borjaneumann@hotmail.com>
2022-03-07 21:31:25 +0100

    Downloading A JDK

---
+++ 01.WelcomeToJava/src/_01_LearningAboutTheJavaEnvironment/_02_DownloadingAJDK.java
@@ -0,0 +1,19 @@
+package _01_LearningAboutTheJavaEnvironment;
+
+public class _02_DownloadingAJDK {
+    /*
+    Every six months, the version number of Java gets incremented. Java 11
+    came out in September 2018. This means that Java 11 will not be the latest
+    version when you download the JDK to study for the exam. However, you
+    should still use Java 11 to study with since this is a Java 11 exam. The
+    rules and behavior can change with later versions of Java. You wouldn’t
+    want to get a question wrong because you studied with a different version
+    of Java!
+
+    Every three years, Oracle has a long-term support (LTS) release. Unlike
+    non-LTS versions that are supported for only six months, LTS releases
+    have patches and upgrades available for at least three years. Even after the
+    next LTS, Java 17, comes out, be sure to use Java 11 to study for the Java
+    11 certification exam.
+     */
+}
