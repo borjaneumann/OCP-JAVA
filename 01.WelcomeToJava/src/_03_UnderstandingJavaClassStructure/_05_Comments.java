3c82b0d44acb28f64388a240bc2fc8473ab744da
Borja <borjaneumann@hotmail.com>
2022-03-07 23:07:42 +0100

    Reviewing comments

--- 01.WelcomeToJava/src/_03_UnderstandingJavaClassStructure/_05_Comments.java
+++ 01.WelcomeToJava/src/_03_UnderstandingJavaClassStructure/_05_Comments.java
@@ -1,16 +1,26 @@
 package _03_UnderstandingJavaClassStructure;
 
 public class _05_Comments {
+
+    //There are 3 types
+
+    //1. single-line comment:
     //comment until the end of line
 
-    /*Multiple
-    line comment
+    //2. Multiple line comment
+    /*
+    Some text
      */
 
+    //3.Javadoc comment:
     /**
      * Javadoc multiple-line comment
      * @author Lo Bano
      */
+
+    //Exercise. , can you identify which type of comment each of the
+    //following six words is in? Is it a single-line or a multiline comment?
+
 
     /*
     * // anteater
