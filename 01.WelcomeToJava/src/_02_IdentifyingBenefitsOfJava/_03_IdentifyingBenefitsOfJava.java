2a016403141dbef78305bd3203ee2cf2d9d22aa2
Borja <borjaneumann@hotmail.com>
2022-03-07 21:29:13 +0100

    Identifying Benefits Of Java

---
+++ 01.WelcomeToJava/src/_02_IdentifyingBenefitsOfJava/_03_IdentifyingBenefitsOfJava.java
@@ -0,0 +1,36 @@
+package _02_IdentifyingBenefitsOfJava;
+
+public class _03_IdentifyingBenefitsOfJava {
+
+    /*8 Benefist Of Java
+    1) OBJECT ORIENTED. Java is an object-oriented language, which means all
+    code is defined in classes, and most of those classes can be instantiated
+    into objects.
+
+    2) ENCAPSULATION. Java supports access modifiers to protect data from
+    unintended access and modification. Most people consider encapsulation
+    to be an aspect of object-oriented languages
+
+    3) PLATFORM INDEPENDENT. Java is an interpreted language that gets compiled
+    to bytecode. A key benefit is that Java code gets compiled once rather than
+    needing to be recompiled for different operating systems. This is known as
+    “write once, run everywhere.”
+
+    4) ROBUST. One of the major advantages of Java over C++ is that it prevents
+    memory leaks. Java manages memory on its own and does garbage
+    collection automatically.
+
+    5) SIMPLE. Simpler than C++
+
+    6) SECURE. Java code runs inside the JVM. This creates a sandbox that makes
+    it hard for Java code to do evil things to the computer it is running on.
+
+    7) MULTITHREADED. allow multiple pieces of code to run at
+    the same time.
+
+    8) BACKWARD COMPATIBILITY. Deprecation is a technique to
+    accomplish this where code is flagged to indicate it shouldn’t be used.
+    This lets developers know a different approach is preferred so they can
+    start changing the code.
+     */
+}
