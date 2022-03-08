82f3b55f61e5d3bcc95ac8537cb905dfefbebaff
Borja <borjaneumann@hotmail.com>
2022-03-08 23:17:42 +0100

    Adjusting Major Running A Program Wit One Line

--- 01.WelcomeToJava/src/_04_WritingAMainMethod/_10_RunningAProgramWithOneLine/_10_RunningAProgramWithOneLine.java
+++ 01.WelcomeToJava/src/_04_WritingAMainMethod/_10_RunningAProgramWithOneLine/_10_RunningAProgramWithOneLine.java
@@ -1,15 +1,29 @@
 package _04_WritingAMainMethod._10_RunningAProgramWithOneLine;
 
-public class Learning {
+public class _10_RunningAProgramWithOneLine {
+    /*you can run a program without compiling it first—
+    well, without typing the javac command that is. it is compiled in memory
+     */
+
+
     public static void main(String[] args) {
 //        ehhh; //DOES NOT COMPILE
         System.out.println("This works");
     }
 }
+class Learning {
+    public static void main(String[] args) {
+//        UhOh; // DOES NOT COMPILE
+        System.out.println("This works!");
+    }
+}
+
 /*
-Learning.java:5: error: not a statement
-        ehhh; //DOES NOT COMPILE
-        ^
+Java is still a compiled language, which means the code is being compiled
+in memory and the java command can give you a compiler error.
+Learning.java:3: error: not a statement
+UhOh; // DOES NOT COMPILE
+^
 1 error
 error: compilation failed
  */
@@ -31,7 +45,6 @@ For any program             For programs with one file
 Can import code in          Can only import code that came
 any available               with the JDK
 Java library
-
 
  */
 
