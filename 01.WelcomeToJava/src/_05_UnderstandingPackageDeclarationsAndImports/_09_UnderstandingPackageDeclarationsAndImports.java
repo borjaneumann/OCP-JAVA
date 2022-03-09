8ea463dab7f1f12bf4ce84874aae61cba17eace9
Borja <borjaneumann@hotmail.com>
2022-03-09 23:32:54 +0100

    Understanding Package Declarations And Imports exam tips

--- 01.WelcomeToJava/src/_05_UnderstandingPackageDeclarationsAndImports/_09_UnderstandingPackageDeclarationsAndImports.java
+++ 01.WelcomeToJava/src/_05_UnderstandingPackageDeclarationsAndImports/_09_UnderstandingPackageDeclarationsAndImports.java
@@ -2,6 +2,9 @@ package _05_UnderstandingPackageDeclarationsAndImports;
 
 public class _09_UnderstandingPackageDeclarationsAndImports {
     /*
+
+    INTRO
+
     Java comes with thousands of built-in classes, and there are countless
     more from developers like you. With all those classes, Java needs a way to
     organize them. It handles this in a way similar to a file cabinet. You put all
@@ -20,5 +23,15 @@ public class _09_UnderstandingPackageDeclarationsAndImports {
     The package com.amazon.javabook is a child package of com.amazon. You can tell
     because it’s longer and thus more specific.
 
+    You’ll see package names on the exam that don’t follow this convention.
+    Don’t be surprised to see package names like a.b.c.
+
+    The rule for package names is that they are mostly letters or numbers
+    separated by periods (.).
+
+    Technically, you’re allowed a couple of other characters between the
+    periods (.). The rules are the same as for variable names.
+    The exam may try to trick you with invalid variable names.
+    Luckily, it doesn’t try to trick you by giving invalid package names.
      */
 }
