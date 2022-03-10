2e3286755ae3d2744cc07e63330a6f656a76b7a8
Borja <borjaneumann@hotmail.com>
2022-03-10 22:52:45 +0100

    Using An Alternate Directory

---
+++ 01.WelcomeToJava/src/_05_UnderstandingPackageDeclarationsAndImports/_15_UsingAnAlternateDirectory.java
@@ -0,0 +1,24 @@
+package _05_UnderstandingPackageDeclarationsAndImports;
+
+public class _15_UsingAnAlternateDirectory {
+    /*
+    By default, the javac command places the compiled classes in the same
+    directory as the source code.
+
+    It also provides an option to place the class files into a different directory.
+    The -d option specifies this target directory.
+
+    Java options are case sensitive. This means you cannot pass -D
+    instead of -d (directory).
+
+    javac -d classes packagea/ClassA.java packageb/ClassB.java
+
+    This commands create a folder named class and creates inside the same structure with the two packages.
+
+    To run the program we have three options:
+    java -cp classes packageb.ClassB
+    java -classpath classes packageb.ClassB
+    java --class-path classes packageb.ClassB
+
+     */
+}
