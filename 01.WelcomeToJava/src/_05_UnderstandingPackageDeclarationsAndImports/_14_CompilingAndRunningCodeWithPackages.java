c89b3cf38035a41a22e2559c4f920d4959903379
Borja <borjaneumann@hotmail.com>
2022-03-10 21:59:05 +0100

    Compiling and Running Code with packages/setup procedure by operative system

---
+++ 01.WelcomeToJava/src/_05_UnderstandingPackageDeclarationsAndImports/_14_CompilingAndRunningCodeWithPackages.java
@@ -0,0 +1,14 @@
+package _05_UnderstandingPackageDeclarationsAndImports;
+
+public class _14_CompilingAndRunningCodeWithPackages {
+    /*
+    Setup procedure by operating system
+    Step                    Windows                         Mac/Linux
+    ================================================================================
+    1. Create first class.  C:\temp\packagea\ClassA.java    /tmp/packagea/ClassA.java
+    2. Create second class. C:\temp\packageb\ClassB.java    /tmp/packageb/ClassB.java
+    3. Go to directory.     cd C:\temp                      cd /tmp
+
+     */
+
+}
