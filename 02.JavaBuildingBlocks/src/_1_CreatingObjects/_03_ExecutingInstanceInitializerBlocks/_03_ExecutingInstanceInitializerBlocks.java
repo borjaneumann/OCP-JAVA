ca34be1df305bc26d615428fffd622eafc390101
Borja <borjaneumann@hotmail.com>
2022-03-14 22:57:22 +0100

    Executing Instance Initializer Blocks

---
+++ 02.JavaBuildingBlocks/src/_1_CreatingObjects/_03_ExecutingInstanceInitializerBlocks/_03_ExecutingInstanceInitializerBlocks.java
@@ -0,0 +1,14 @@
+package _1_CreatingObjects._03_ExecutingInstanceInitializerBlocks;
+
+public class _03_ExecutingInstanceInitializerBlocks {
+    /*
+    - code block. The code between the braces. Anywhere you see braces is a code block.
+    - code block location:
+        1) in the Method
+        2) Outside a method. Instance initializer. Cannot exist inside a method.
+    - balanced parentheses problem. when brackets amount do not match.
+
+
+
+     */
+}
