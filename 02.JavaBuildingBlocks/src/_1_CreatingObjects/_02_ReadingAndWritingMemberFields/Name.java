b6282a0bf62e17bc7dc5fae33e379d4346164427
Borja <borjaneumann@hotmail.com>
2022-03-14 22:49:34 +0100

    Updating Name class

--- 02.JavaBuildingBlocks/src/_1_CreatingObjects/_02_ReadingAndWritingMemberFields/Name.java
+++ 02.JavaBuildingBlocks/src/_1_CreatingObjects/_02_ReadingAndWritingMemberFields/Name.java
@@ -4,4 +4,8 @@ public class Name {
     String first = "Theodore";
     String last = "Moose";
     String full = first + last;
+    /*
+    Lines 4 and 5 both write to fields. Line 5 both reads and writes data.
+    It reads the fields first and last. It then writes the field full.
+     */
 }
