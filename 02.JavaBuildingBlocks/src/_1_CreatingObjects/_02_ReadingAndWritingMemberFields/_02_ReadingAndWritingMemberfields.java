0a15f8b07cc3cd7d1660ee3c0255dcc5fdabed92
Borja <borjaneumann@hotmail.com>
2022-03-14 22:47:50 +0100

    Reading And Writing Member fields

---
+++ 02.JavaBuildingBlocks/src/_1_CreatingObjects/_02_ReadingAndWritingMemberFields/_02_ReadingAndWritingMemberfields.java
@@ -0,0 +1,20 @@
+package _1_CreatingObjects._02_ReadingAndWritingMemberFields;
+
+public class _02_ReadingAndWritingMemberfields {
+    /*
+    public class Swan {
+        int numberEggs; // instance
+        variable
+        public static void main(String[] args) {
+            Swan mother = new Swan();
+            mother.numberEggs = 1; // set variable
+            System.out.println(mother.numberEggs); // read variable
+        }
+    }
+
+    - Reading a variable is known as getting it.
+    - Writing to a variable is known as setting it.
+
+     */
+}
+
