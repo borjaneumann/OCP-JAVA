8b5244b9a9b87faacb8c9d86ab41efd8c4e12339
Borja <borjaneumann@hotmail.com>
2022-03-15 21:06:23 +0100

    Using Reference Types

---
+++ 02.JavaBuildingBlocks/src/_2_UnderstandingDataTypes/_06_UsingReferenceTypes.java
@@ -0,0 +1,19 @@
+package _2_UnderstandingDataTypes;
+
+public class _06_UsingReferenceTypes {
+    /*
+    A reference type refers to an object (an instance of a class). Unlike
+    primitive types that hold their values in the memory where the variable is
+    allocated, references do not hold the value of the object they refer to.
+    Instead, a reference “points” to an object by storing the memory address
+    where the object is located, a concept referred to as a pointer. Unlike other
+    languages, Java does not allow you to learn what the physical memory
+    address is. You can only use the reference to refer to the object.
+
+    A value is assigned to a reference in one of two ways:
+    1) A reference can be assigned to another object of the same or compatible type.
+    2) A reference can be assigned to a new object using the new keyword.
+
+     */
+
+}
