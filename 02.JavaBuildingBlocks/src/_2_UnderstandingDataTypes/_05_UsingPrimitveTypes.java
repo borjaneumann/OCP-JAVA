ec9d9b6681cbae59ac45581b17e07ebe6bd10092
Borja <borjaneumann@hotmail.com>
2022-03-15 20:34:53 +0100

    Primitive types table

--- 02.JavaBuildingBlocks/src/_2_UnderstandingDataTypes/_05_UsingPrimitveTypes.java
+++ 02.JavaBuildingBlocks/src/_2_UnderstandingDataTypes/_05_UsingPrimitveTypes.java
@@ -9,6 +9,18 @@ public class _05_UsingPrimitveTypes {
     represent an object. A primitive is just a single value in memory, such as a
     number or character.
 
+    Primitive types
+    Keyword         Type                        Example
+    ======================================================
+    boolean         true or false               true
+    byte            8-bit integral value        123
+    short           16-bit integral value       123
+    int             32-bit integral value       123
+    long            64-bit integral             value 123L
+    float           32-bit floating-point       value 123.45f
+    double          64-bit floating-point value 123.456
+    char            16-bit Unicode value        'a'
+
      */
 
 }
