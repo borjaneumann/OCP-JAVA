b6f5d3678e2ad3d6e2c37c42825116c740955b66
Borja <borjaneumann@hotmail.com>
2022-03-15 21:17:48 +0100

    Declaring and Initializing variables

---
+++ 02.JavaBuildingBlocks/src/_3_DeclaringVariables/_08_DeclaringVariablesIntro.java
@@ -0,0 +1,32 @@
+package _3_DeclaringVariables;
+
+public class _08_DeclaringVariablesIntro {
+    /*
+    DECLARING A VARIABLE
+    --------------------
+    A variable is a name for a piece of memory that stores data. When you declare
+    a variable, you need to state the variable type along with giving it a name.
+
+    String zooName;
+    int numberAnimals;
+
+    INITIALIZING A VARIABLE
+    -----------------------
+    Now that we’ve declared a variable, we can give it a value. This is called
+    initializing a variable. To initialize a variable, you just type the variable
+    name followed by an equal sign, followed by the desired value:
+
+    zooName = "The Best Zoo";
+    numberAnimals = 100;
+
+
+    Since you often want to initialize a variable right away, you can do so in
+    the same statement as the declaration. For example, here we merge the
+    previous declarations and initializations into more concise code:
+
+    String zooName = "The Best Zoo";
+    int numberAnimals = 100;
+
+
+     */
+}
