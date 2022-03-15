4dc97bbb658bac065f1513eb290d49cb98c0c62f
Borja <borjaneumann@hotmail.com>
2022-03-15 21:12:53 +0100

    Distinguish between Primitives and Reference types(3)

---
+++ 02.JavaBuildingBlocks/src/_2_UnderstandingDataTypes/_07_DistinguishBetweenPrimitivesAndReferenceTypes.java
@@ -0,0 +1,16 @@
+package _2_UnderstandingDataTypes;
+
+public class _07_DistinguishBetweenPrimitivesAndReferenceTypes {
+    /*
+    1) Reference types can be assigned null, which means
+    they do not currently refer to an object. But what if you don’t know
+    the value of an int and want to assign it to null? In that case, you
+    should use a numeric wrapper class, such as Integer, instead of int.
+
+    2) Reference types can be used to call methods, assuming the reference
+    is not null. Primitives do not have methods declared on them.
+
+    3) All the primitive types have lowercase type names. All classes that
+    come with Java begin with uppercase.
+     */
+}
