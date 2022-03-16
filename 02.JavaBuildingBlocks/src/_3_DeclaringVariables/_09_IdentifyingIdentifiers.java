de8e7ec7bbc1390a170c4a2285649e3f9308bd8a
Borja <borjaneumann@hotmail.com>
2022-03-16 10:32:44 +0100

    identifier 4 rules (naming)

---
+++ 02.JavaBuildingBlocks/src/_3_DeclaringVariables/_09_IdentifyingIdentifiers.java
@@ -0,0 +1,15 @@
+package _3_DeclaringVariables;
+
+public class _09_IdentifyingIdentifiers {
+    /*
+    An identifier is the name of a variable, method, class, interface, or package.
+
+    There are only four rules to remember for legal identifiers:
+
+    1) Identifiers must begin with a letter, a $ symbol, or a _ symbol.
+    2) Identifiers can include numbers but not start with them.
+    3) Since Java 9, a single underscore _ is not allowed as an identifier.
+    4) You cannot use the same name as a Java reserved word.
+     */
+
+}
