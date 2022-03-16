1b709878ef7674319bf1a666c5263a799e853108
Borja <borjaneumann@hotmail.com>
2022-03-16 11:00:20 +0100

    Creating Local Variables Intro

---
+++ 02.JavaBuildingBlocks/src/_4_InitializingVariables/_11_CreatingLocalVariables.java
@@ -0,0 +1,11 @@
+package _4_InitializingVariables;
+
+public class _11_CreatingLocalVariables {
+    /*
+    Intro:
+    Before you can use a variable, it needs a value. Some types of variables
+    get this value set automatically, and others require the programmer to
+    specify it.
+     */
+
+}
