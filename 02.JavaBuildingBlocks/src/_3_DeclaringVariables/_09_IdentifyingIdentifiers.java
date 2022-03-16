1540b186cba5e3662d3d88e2c6d75f4df2d5ef8e
Borja <borjaneumann@hotmail.com>
2022-03-16 10:39:44 +0100

    Reserved words

--- 02.JavaBuildingBlocks/src/_3_DeclaringVariables/_09_IdentifyingIdentifiers.java
+++ 02.JavaBuildingBlocks/src/_3_DeclaringVariables/_09_IdentifyingIdentifiers.java
@@ -10,6 +10,23 @@ public class _09_IdentifyingIdentifiers {
     2) Identifiers can include numbers but not start with them.
     3) Since Java 9, a single underscore _ is not allowed as an identifier.
     4) You cannot use the same name as a Java reserved word.
+
+     Reserved words
+     -----------------------------------------------------------------------------
+    abstract    assert      boolean     break       byte    case    catch       char    class
+    const*      continue    default     do          double  else    enum        extends false**
+    final       finally     float       for         goto*   if      implements
+    import      instanceof  int         interface   long    native  new         null**
+    package     private     protected   public      return  short   static      strictfp
+    super       switch      synchronized this       throw   throws  transient   true**  try
+    void        volatile    while       _ (underscore)
+    -------------------------------------------------------------------------------------------
+    * The reserved words const and goto aren’t actually used in Java. They are reserved so that people
+    coming from other programming languages don’t use them by accident—and in theory, in case Java
+    wants to use them one day.
+    ** true/false/null are not actually reserved words, but literal values. Since they cannot be used as
+    identifier names, we include them in this table.
+
      */
 
 }
