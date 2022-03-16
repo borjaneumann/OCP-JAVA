e909e264b6ef90115d55b08bc96518d80e38e3e9
Borja <borjaneumann@hotmail.com>
2022-03-16 11:13:20 +0100

    Creating Local Variables

--- 02.JavaBuildingBlocks/src/_4_InitializingVariables/_11_CreatingLocalVariables.java
+++ 02.JavaBuildingBlocks/src/_4_InitializingVariables/_11_CreatingLocalVariables.java
@@ -6,6 +6,29 @@ public class _11_CreatingLocalVariables {
     Before you can use a variable, it needs a value. Some types of variables
     get this value set automatically, and others require the programmer to
     specify it.
-     */
+
+    CREATING LOCAL VARIABLES
+    A local variable is a variable defined within a constructor, method, or
+    initializer block.
+
+    * Local variables do not have a default value and must be initialized before use.
+    * The compiler is only concerned if you try to use uninitialized local variables;
+    it doesn’t mind the ones you never use.
+    */
+    public void findAnswer(boolean check) {
+        int answer;
+        int otherAnswer;
+        int onlyOneBranch;
+        if (check) {
+            onlyOneBranch = 1;
+            answer = 1;
+        } else {
+            answer = 2;
+        }
+        System.out.println(answer);
+//        System.out.println(onlyOneBranch); // DOES NOT COMPILE. The compiler knows there is the possibility for check to be false,
+        // resulting in uninitialized code, and gives a compiler error.
+    }
+
 
 }
