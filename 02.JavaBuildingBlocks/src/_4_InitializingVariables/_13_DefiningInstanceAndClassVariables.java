8a2c3e703d17d1dfc44ebcb22f3ca0e932ba1dc2
Borja <borjaneumann@hotmail.com>
2022-03-16 12:40:16 +0100

    Instance and Class variables

---
+++ 02.JavaBuildingBlocks/src/_4_InitializingVariables/_13_DefiningInstanceAndClassVariables.java
@@ -0,0 +1,25 @@
+package _4_InitializingVariables;
+
+public class _13_DefiningInstanceAndClassVariables {
+    /*
+    Variables that are not local variables are defined either as instance
+    variables or as class variables.
+
+    Instance Variable
+    -----------------
+    An instance variable, often called a field, is a value defined within a
+    specific instance of an object.
+    Two instances could have the same value for name, but changing the value
+    for one does not modify the other.
+
+    Class Variable
+    --------------
+    A class variable is one that is defined on the class level
+    and shared among all instances of the class.
+    It can even be publicly accessible to classes outside
+    the class without requiring an instance to use.
+    You can tell a variable is a class variable because it has the keyword static before it.
+
+
+     */
+}
