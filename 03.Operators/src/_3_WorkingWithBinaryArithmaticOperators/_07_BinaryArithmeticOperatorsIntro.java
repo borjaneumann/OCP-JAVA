0e4384f1140df8a3855ecdfcc32099d975d43cd4
Borja <borjaneumann@hotmail.com>
2022-03-17 21:50:48 +0100

    Binary Arithmetic Operators Intro

--- 03.Operators/src/_3_WorkingWithBinaryArithmaticOperators/_07_BinaryArithmeticOperatorsIntro.java
+++ 03.Operators/src/_3_WorkingWithBinaryArithmaticOperators/_07_BinaryArithmeticOperatorsIntro.java
@@ -1,4 +1,23 @@
 package _3_WorkingWithBinaryArithmaticOperators;
 
 public class _07_BinaryArithmeticOperatorsIntro {
+    /*
+    Binary operators take two arguments.
+
+    Binary operators are often combined in complex expressions
+    with other binary operators; therefore, operator precedence is very
+    important in evaluating expressions containing binary operators.
+
+    Binary arithmetic operators
+    Operator                    Description
+    --------------------------------------------------------------------------------
+    +                           Adds two numeric values
+    -                           Subtracts two numeric values
+    *                           Multiplies two numeric values
+    /                           Divides one numeric value by another
+    %                           Modulus operator returns the remainder after division of one
+                                numeric value by another
+
+     */
 }
+
