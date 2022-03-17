df0df5f332f1b217e8eda2f1c634f4d8ea857e1c
Borja <borjaneumann@hotmail.com>
2022-03-17 20:59:01 +0100

    Unary Operators Intro

---
+++ 03.Operators/src/_2_ApplyingUnaryOperators/_04_UnaryOperatorsIntro.java
@@ -0,0 +1,18 @@
+package _2_ApplyingUnaryOperators;
+
+public class _04_UnaryOperatorsIntro {
+    /*
+    Requires exactly one operand, or variable, to function.
+
+    (Unary operators)
+    Operator        Description
+    ------------------------------------------------
+    !               Inverts a boolean’s logical value
+    +               Indicates a number is positive, although numbers are assumed to
+                    be positive in Java unless accompanied by a negative unary operator
+    -               Indicates a literal number is negative or negates an expression
+    ++              Increments a value by 1
+    --              Decrements a value by 1
+    (type)          Casts a value to a specific type.
+     */
+}
