e6289f8e3b2b381b061fe7a37210610a263fdf69
Borja <borjaneumann@hotmail.com>
2022-03-17 10:20:11 +0100

    Reviewing Scope

---
+++ 02.JavaBuildingBlocks/src/_5_ManagingVariableScope/_20_ReviewingScope.java
@@ -0,0 +1,14 @@
+package _5_ManagingVariableScope;
+
+public class _20_ReviewingScope {
+    /*
+    Local variables: In scope from declaration to end of block
+
+    Instance variables: In scope from declaration until object eligible for
+    garbage collection
+
+    Class variables: In scope from declaration until program ends
+     */
+
+
+}
