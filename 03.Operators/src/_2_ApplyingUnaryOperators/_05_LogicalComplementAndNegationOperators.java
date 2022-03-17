deecdf2831d61c6539be62e7a39716c43e633c5b
Borja <borjaneumann@hotmail.com>
2022-03-17 21:06:29 +0100

    Logical Complement And Negation Operators

---
+++ 03.Operators/src/_2_ApplyingUnaryOperators/_05_LogicalComplementAndNegationOperators.java
@@ -0,0 +1,18 @@
+package _2_ApplyingUnaryOperators;
+
+public class _05_LogicalComplementAndNegationOperators {
+    /*
+    - (!) The logical complement operator flips the value of a boolean expression.
+    - (-) The negation operator reverses the sign of a numeric expression
+
+    in Java, 1 and true are not related in any way, just as 0 and false are not related.
+
+    double zooTemperature = 1.21;
+    System.out.println(zooTemperature); // 1.21
+    zooTemperature = -zooTemperature;
+    System.out.println(zooTemperature); // -1.21
+    zooTemperature = -(-zooTemperature);
+    System.out.println(zooTemperature); // -1.21
+     */
+
+}
