11930511a94855f4812cc517086392f24b9d37ca
Borja <borjaneumann@hotmail.com>
2022-03-17 22:30:29 +0100

    Assignment Operator

---
+++ 03.Operators/src/_4_AssigningValues/_10_AssignmentOperator.java
@@ -0,0 +1,14 @@
+package _4_AssigningValues;
+
+public class _10_AssignmentOperator {
+    /*
+    An assignment operator is a binary operator that modifies, or assigns, the
+    variable on the left side of the operator, with the result of the value on the
+    right side of the equation. The simplest assignment operator is the =
+    assignment.
+
+    Java will throw a compiler exception if it detects that you are trying to convert from larger
+    to smaller data types without casting.
+     */
+
+}
