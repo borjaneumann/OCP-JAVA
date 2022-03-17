4f0e3ef503dfc0aa95f42cc2fa40bfffefc298d6
Borja <borjaneumann@hotmail.com>
2022-03-17 16:29:56 +0100

    Understanding Operators Intro

--- 03.Operators/src/_1_UnderstandingJavaOperators/_01_UnderstandingJavaOperatorsIntro.java
+++ 03.Operators/src/_1_UnderstandingJavaOperators/_01_UnderstandingJavaOperatorsIntro.java
@@ -1,5 +1,15 @@
 package _1_UnderstandingJavaOperators;
 
 public class _01_UnderstandingJavaOperatorsIntro {
+    /*
+    A Java operator is a special symbol that can be applied to a set of variables,
+    values, or literals—referred to as operands—and that returns a result.
 
+    The term operand, which we’ll use throughout this chapter, refers to the value
+    or variable the operator is being applied to.
+
+    The output of the operation is simply referred to as the result.
+
+    c (result) = a (operand)  + (operator) b (operand)
+     */
 }
