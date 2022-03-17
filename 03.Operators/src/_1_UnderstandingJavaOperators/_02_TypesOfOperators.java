f884b7aa42607e9f56327452083ae630cb168c6a
Borja <borjaneumann@hotmail.com>
2022-03-17 16:35:34 +0100

    Types of operators

---
+++ 03.Operators/src/_1_UnderstandingJavaOperators/_02_TypesOfOperators.java
@@ -0,0 +1,13 @@
+package _1_UnderstandingJavaOperators;
+
+public class _02_TypesOfOperators {
+    /*
+    - Types: unary, binary,and ternary.
+    Java operators are not necessarily evaluated from left-to-right order.
+
+    int cookies = 4;
+    double reward = 3 + 2 * --cookies;
+    System.out.print("Zoo animal receives: "+reward+" reward points"); //9
+     */
+
+}
