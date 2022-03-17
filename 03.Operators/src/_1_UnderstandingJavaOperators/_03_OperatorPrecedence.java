41423f515fd128dae96d3fc8d5dfb2550114fbf3
Borja <borjaneumann@hotmail.com>
2022-03-17 17:00:05 +0100

    Operators precedence

---
+++ 03.Operators/src/_1_UnderstandingJavaOperators/_03_OperatorPrecedence.java
@@ -0,0 +1,37 @@
+package _1_UnderstandingJavaOperators;
+
+public class _03_OperatorPrecedence {
+    /*
+    Determining which operators are evaluated in what order is
+    referred to as operator precedence. In this manner, Java more closely
+    follows the rules for mathematics.
+
+    var perimeter = 2 * height + 2 * length;
+
+    The multiplication operator (*) has a higher precedence than the addition
+    operator (+), so the height and length are both multiplied by 2 before being
+    added together. The assignment operator (=) has the lowest order of
+    precedence, so the assignment to the perimeter variable is performed last.
+
+    If two operators have the same level of precedence, then Java guarantees
+    left-to-right evaluation.
+
+    Order of operator precedence
+    Operator                            Symbols and examples
+    ===============================================================
+    Post-unary operators                expression++, expression--
+    Pre-unary operators                 ++expression, --expression
+    Other unary operators               -, !, ~, +, (type)
+    Multiplication/division/modulus     *, /, %
+    Addition/subtraction                +, -
+    Shift operators                     <<, >>, >>>
+    Relational operators                <, >, <=, >=, instanceof
+    Equal to/not equal to               ==, !=
+    Logical operators                   &, ^, |
+    Short-circuit logical operators     &&, ||
+    Ternary operators                   boolean expression ? expression1 : expression2
+    Assignment operators                =, +=, -=, *=, /=, %=, &=, ^=, |=, <<=, >>=, >>>=
+
+     */
+
+}
