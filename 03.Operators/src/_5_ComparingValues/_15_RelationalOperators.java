edadfc2cd4ed800b4d833ca2e07d1a2015948353
Borja <borjaneumann@hotmail.com>
2022-03-18 10:46:33 +0100

    Relational Operators Table

---
+++ 03.Operators/src/_5_ComparingValues/_15_RelationalOperators.java
@@ -0,0 +1,36 @@
+package _5_ComparingValues;
+
+public class _15_RelationalOperators {
+    /*
+    Relational operators compare two expressions and return a boolean value.
+
+    Relational operators
+    Operator    Description
+    ===============================================================================
+    <           Returns true if the value on the left is strictly less than the
+                value on the right
+    -------------------------------------------------------------------------------
+    <=          Returns true if the value on the left is less than or equal to the
+                value on the right
+    -------------------------------------------------------------------------------
+    >           Returns true if the value on the left is strictly greater than the
+                value on the right
+    -------------------------------------------------------------------------------
+    >=          Returns true if the value on the left is greater than or equal to
+                the value on the right
+    -------------------------------------------------------------------------------
+    a           Returns true if the reference that a points to is an instance of a
+    instance    class, subclass, or class that implements a particular interface,
+    of b        as named in b
+    -------------------------------------------------------------------------------
+
+    If the two numeric operands are not of the same data type, the
+    smaller one is promoted as previously discussed.
+
+
+
+
+
+
+     */
+}
