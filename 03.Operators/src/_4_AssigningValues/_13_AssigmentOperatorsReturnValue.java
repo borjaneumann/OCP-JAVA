639ee4996da08f9cff09ecf61ab68d03eb3b9e8e
Borja <borjaneumann@hotmail.com>
2022-03-18 09:59:04 +0100

    Assigment Operators Return Value

---
+++ 03.Operators/src/_4_AssigningValues/_13_AssigmentOperatorsReturnValue.java
@@ -0,0 +1,25 @@
+package _4_AssigningValues;
+
+public class _13_AssigmentOperatorsReturnValue {
+    /*
+    The result of an assignment is an expression in and of itself,
+    equal to the value of the assignment.
+
+    long wolf = 5;
+    long coyote = (wolf=3);
+    System.out.println(wolf); // 3
+    System.out.println(coyote); // 3
+
+    The key here is that (wolf=3) does two things. First, it sets the value of
+    the variable wolf to be 3. Second, it returns a value of the assignment,
+    which is also 3.
+
+    KEEP IN MIND!!
+    boolean healthy = false;
+    if(healthy = true)
+    System.out.print("Good!");
+    While this may look like a test if healthy is true, it’s actually assigning
+    healthy a value of true. The result of the assignment is the value of the
+    assignment, which is true, resulting in this snippet printing Good!
+     */
+}
