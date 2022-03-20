b869b3c707d7f59fc27f0abd866066400982e59c
Borja <borjaneumann@hotmail.com>
2022-03-20 12:23:42 +0100

    Statements and Blocks

---
+++ 04.Making_decisions/src/_01_CreatingDecisionMakingStatements/_01_StatementsAndBlocks.java
@@ -0,0 +1,45 @@
+package _01_CreatingDecisionMakingStatements;
+
+public class _01_StatementsAndBlocks {
+    /*
+    A Java statement is a complete unit of execution in Java, terminated with a
+    semicolon (;).
+
+    Control flow statements break up the flow of execution by using decision-making,
+    looping, and branching, allowing the application to selectively execute
+    particular segments of code.
+
+    These statements can be applied to single expressions as well as a block of
+    Java code.
+
+    A Block of code in Java is a group of zero or more statements between
+    balanced braces ({}) and can be used anywhere a single statement is allowed.
+    For example, the following two snippets are equivalent, with the first being a
+    single expression and the second being a block of statements:
+
+    // Single statement
+    patrons++;
+    // Statement inside a block
+    {
+    patrons++;
+    }
+
+    A statement or block often functions as the target of a decision-making
+    statement. For example, we can prepend the decision-making if statement
+    to these two examples:
+
+    // Single statement
+    if(ticketsTaken > 1)
+        patrons++;
+    // Statement inside a block
+    if(ticketsTaken > 1)
+    {
+        patrons++;
+    }
+    Again, both of these code snippets are equivalent. Just remember that the
+    target of a decision-making statement can be a single statement or block of
+    statements. For the rest of the chapter, we will use both forms to better
+    prepare you for what you will see on the exam.
+
+     */
+}
