c45d4c1828de98a84d628e3961fcfaf1f76b01be
borjaneumann <borjaneumann@hotmail.com>
2022-03-21 10:22:39 +0100

    The switch statement adjustment

--- 04.Making_decisions/src/_01_CreatingDecisionMakingStatements/_04_TheSwitchStatement.java
+++ 04.Making_decisions/src/_01_CreatingDecisionMakingStatements/_04_TheSwitchStatement.java
@@ -29,6 +29,27 @@ public class _04_TheSwitchStatement {
    Switch Data Types: all primitive numeric types(int,byte, short,char),
    their wrapper classes, String, ENUMS and var if refer to the already mentioned ones.
 
+   Acceptable Case Values:
+    - only literals,
+    - enum constants, or
+    - final constant variables of the same data type. By final constant, we mean that the variable must be marked with
+    the final modifier and initialized with a literal value in the same expression in which it is declared.
+
+    final int getCookies() { return 4; }
+        void feedAnimals() {
+            final int bananas = 1;
+            int apples = 2;
+            int numberOfAnimals = 3;
+            final int cookies = getCookies();
+            switch (numberOfAnimals) {
+                case bananas:
+                case apples: // DOES NOT COMPILES
+                case getCookies(): // DOES NOT COMPILE
+                case cookies : // DOES NOT COMPILE
+                case 3 * 5 :
+        }
+    }
+
     Pay attention to this!!!
     int month = 5;
     switch (month) // DOES NOT COMPILE no brackets
@@ -81,14 +102,65 @@ public class _04_TheSwitchStatement {
                 System.out.println("Sunday");
             default:
                 System.out.println("Weekday");
-
+            case 10:
+                System.out.println("Imaginary day");
             case 6:
                 System.out.println("Saturday");
                 break;
         }
         /*
-        This prints out Weekday and Saturday. it jumps directly to the default case and executed the cases with a break.
-        The order of the default case is important!
+        This prints out Weekday and Saturday. it jumps directly to the default case and executed the cases until it finds a break.
+        The default is executed because there was no break in the preceeding statement.
+        If there is no break statement, executes all of them.
+        The order of the case and default statements is now important since
+        placing the default statement at the end of the switch statement would
+        cause only one word to be output.
+        if day of the week is 6, the result would be "Saturday", even though the default case is before.
          */
     }
+    /* A More Complex Example
+    private int getSortOrder(String firstName, final String
+            lastName) {
+        String middleName = "Patricia";
+        final String suffix = "JR";
+        int id = 0;
+        switch(firstName) {
+            case "Test":
+                return 52;
+            case middleName: // DOES NOT COMPILE
+                id = 5;
+                break;
+            case suffix:
+                id = 0;
+                break;
+            case lastName: // DOES NOT COMPILE
+                id = 8;
+                break;
+            case 5: // DOES NOT COMPILE
+                id = 7;
+                break;
+            case 'J': // DOES NOT COMPILE
+                id = 10;
+                break;
+            case java.time.DayOfWeek.SUNDAY: // DOES NOT COMPILE
+                id=15;
+                break;
+        }
+        return id;
+    }
+    Numeric Promotion and Casting
+    Last but not least, switch statements support numeric promotion that does
+    not require an explicit cast. For example, see if you can understand why
+    only two of these case statements compile:
+
+    short size = 4;
+    final int small = 15;
+    final int big = 1_000_000;
+    switch(size) {
+        case small:
+        case 1+2 :
+        case big: // DOES NOT COMPILE
+    }
+
+     */
 }
