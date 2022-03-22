d00b6f8aa94dc794642d53ff8acb900f8b857c64
borjaneumann <borjaneumann@hotmail.com>
2022-03-22 11:21:55 +0100

    Functional interfaces: Consumer

--- 06.LambdasAndFunctionalInterfaces/src/_02_IntroducingFunctionalInterfaces/_04_Consumer/_05_Consumer.java
+++ 06.LambdasAndFunctionalInterfaces/src/_02_IntroducingFunctionalInterfaces/_04_Consumer/_05_Consumer.java
@@ -2,11 +2,21 @@ package _02_IntroducingFunctionalInterfaces._04_Consumer;
 
 import java.util.function.Consumer;
 
-public class ConsumerExample {
+public class _05_Consumer {
+    /*
+    To consume some input and perform required operation.It won’t return anything.
+    It is a passive consumer that's why accepts().
+
+    void accept(T t)
+
+     */
     public static void main(String[] args) {
         Consumer<String> consumer = x -> System.out.println(x);
         print(consumer, "Hello world");
         consumer.accept("Hello Universe");
+
+        Consumer<String> another = s -> System.out.println(s.length());
+        another.accept("java");
     }
     public static void print(Consumer<String> consumer, String value){
         consumer.accept(value);
