f78c0d2af60fe58315003d7bca0fd9216f4fae8b
borjaneumann <borjaneumann@hotmail.com>
2022-03-22 08:50:03 +0100

    Reviewing Branching

--- 04.Making_decisions/src/_04_ControllingFlowWithBranching/_18_ReviewingBranching.java
+++ 04.Making_decisions/src/_04_ControllingFlowWithBranching/_18_ReviewingBranching.java
@@ -1,4 +1,13 @@
 package _04_ControllingFlowWithBranching;
 
-public class _17_ReviewingBranching {
+public class _18_ReviewingBranching {
+    /*
+    Advanced flow control usage
+    Loop Type   Allows optional labels  Allows break statement  Allows continue statement
+    =====================================================================================
+    while       Yes                     Yes                     Yes
+    do/while    Yes                     Yes                     Yes
+    for         Yes                     Yes                     Yes
+    switch      Yes                     Yes                     No
+     */
 }
