38fbf9091a463deb74a0bd3975bb842d5b97626a
borjaneumann <borjaneumann@hotmail.com>
2022-03-22 11:39:36 +0100

    Functional interfaces: Supplier

--- 06.LambdasAndFunctionalInterfaces/src/_02_IntroducingFunctionalInterfaces/_05_Supplier/_06_Supplier.java
+++ 06.LambdasAndFunctionalInterfaces/src/_02_IntroducingFunctionalInterfaces/_05_Supplier/_06_Supplier.java
@@ -3,18 +3,23 @@ package _02_IntroducingFunctionalInterfaces._05_Supplier;
 import java.util.Random;
 import java.util.function.Supplier;
 
-public class SupplierExample {
+public class _06_Supplier {
+    /*
+    To supply some value base on requirement. Gets a result and Returns a result
+
+    T get()
+
+     */
     public static void main(String[] args) {
         Supplier<Integer> number = () -> 42;
         Supplier<Integer> random = () -> new Random().nextInt();
+        System.out.println(number.get());
+        System.out.println(random.get());
+
         System.out.println(returnNumber(number));
         System.out.println(returnNumber(random));
-
     }
     private static int returnNumber(Supplier<Integer> supplier){
-        return supplier.get();
-    }
-    private static int returnRandom(Supplier<Integer> supplier){
         return supplier.get();
     }
 }
