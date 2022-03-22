6e90157e6e933a66af841ca4ad18c9c865791c31
borjaneumann <borjaneumann@hotmail.com>
2022-03-22 14:12:38 +0100

    Parameter list: Lambdas

--- 06.LambdasAndFunctionalInterfaces/src/_03_workingWithVariablesInLambdas/_08_WorkingWithVariablesInLambdasIntro.java
+++ 06.LambdasAndFunctionalInterfaces/src/_03_workingWithVariablesInLambdas/_08_WorkingWithVariablesInLambdasIntro.java
@@ -1,9 +1,10 @@
 package _03_workingWithVariablesInLambdas;
 
-public class _08_WorkingWithVAriablesInLambdasIntro {
+public class _08_WorkingWithVariablesInLambdasIntro {
     /*
     Variables can appear in three places with respect to lambdas:
-        1) the parameter list,
+
+        1) The parameter list,
         2) local variables declared inside the lambda body
         3) Variables referenced from the lambda body.
 
--- 06.LambdasAndFunctionalInterfaces/src/_03_workingWithVariablesInLambdas/_09_ParameterList.java
+++ 06.LambdasAndFunctionalInterfaces/src/_03_workingWithVariablesInLambdas/_09_ParameterList.java
@@ -3,11 +3,18 @@ package _03_workingWithVariablesInLambdas;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
-public class VariablesInParameterList {
-    //these 3 parameters are interchangeable
+public class _09_ParameterList {
+    /*
+    Earlier in this chapter, you learned that specifying the type of parameters
+    is optional. Additionally, var can be used in place of the specific type.
+    That means that all three of these statements are interchangeable:
+
     Predicate<String> p = x -> true;
-//    Predicate<String> p = (var x) -> true;
-//    Predicate<String> p = (String s) -> true;
+    Predicate<String> p = (var x) -> true;
+    Predicate<String> p = (String x) -> true;
+
+    The type of the lambda parameter is String
+     */
 
     public void whatAmI(){
         consume((var x) -> System.out.println(x),123);
@@ -15,8 +22,9 @@ public class VariablesInParameterList {
     public void consume(Consumer<Integer> c, int num){
         c.accept(num);
     }
+
     public static void main(String[] args) {
-        VariablesInParameterList variablesInParameterList = new VariablesInParameterList();
+        _09_ParameterList variablesInParameterList = new _09_ParameterList();
         variablesInParameterList.whatAmI();
     }
 
