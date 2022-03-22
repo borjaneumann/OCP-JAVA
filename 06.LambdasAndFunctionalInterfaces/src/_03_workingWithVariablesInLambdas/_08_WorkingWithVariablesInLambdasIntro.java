caedb5ec6858b26490d91e514513a8bc2cecffc0
borjaneumann <borjaneumann@hotmail.com>
2022-03-22 13:41:56 +0100

    Working With Variables In Lambdas Intro

---
+++ 06.LambdasAndFunctionalInterfaces/src/_03_workingWithVariablesInLambdas/_08_WorkingWithVariablesInLambdasIntro.java
@@ -0,0 +1,13 @@
+package _03_workingWithVariablesInLambdas;
+
+public class _08_WorkingWithVAriablesInLambdasIntro {
+    /*
+    Variables can appear in three places with respect to lambdas:
+        1) the parameter list,
+        2) local variables declared inside the lambda body
+        3) Variables referenced from the lambda body.
+
+    All three of these are opportunities for the exam to trick you.
+    */
+
+}
