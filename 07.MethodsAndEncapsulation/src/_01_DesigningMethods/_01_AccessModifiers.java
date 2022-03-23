f4f6fee962bcf5cbf5b002a8002325e64196b754
borjaneumann <borjaneumann@hotmail.com>
2022-03-23 09:57:15 +0100

    Access Modifiers

--- 07.MethodsAndEncapsulation/src/_01_DesigningMethods/_01_AccessModifiers.java
+++ 07.MethodsAndEncapsulation/src/_01_DesigningMethods/_01_AccessModifiers.java
@@ -1,2 +1,24 @@
-package _01_DesigningMethods;public class _01_AccessModifiers {
+package _01_DesigningMethods;
+
+public class _01_AccessModifiers {
+    /*
+    * private -> The private modifier means the method can be called only from
+    within the same class.
+
+    * Default -> (Package-Private) Access With default access, the method can
+    be called only from classes in the same package. This one is tricky
+    because there is no keyword for default access. You simply omit the
+    access modifier.
+
+    * protected The protected modifier means the method can be called only
+    from classes in the same package or subclasses. You’ll learn about
+    subclasses in Chapter 8, “Class Design.”
+
+    * public The public modifier means the method can be called from any class.
+
+    public void walk1() {}
+    default void walk2() {} // DOES NOT COMPILE
+    void public walk3() {} // DOES NOT COMPILE
+    void walk4() {}
+     */
 }
