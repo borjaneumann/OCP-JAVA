3710230f2558fffefdcb5128b69060d0c861d768
borjaneumann <borjaneumann@hotmail.com>
2022-03-23 10:40:36 +0100

    Method NAme

--- 07.MethodsAndEncapsulation/src/_01_DesigningMethods/_04_MethodName.java
+++ 07.MethodsAndEncapsulation/src/_01_DesigningMethods/_04_MethodName.java
@@ -1,2 +1,14 @@
-package _01_DesigningMethods;public class _04_MethodName {
+package _01_DesigningMethods;
+
+public class _04_MethodName {
+    /*
+    Method names follow the same rules as we practiced with variable names
+
+    public void walk1(){}
+    public void 2walk(){} //Does not compile
+    public walk3 void(){} //Does not compile
+    public void walk_$(){}
+    public _() {}    //Does not compile
+    public void() {} //Does not compile
+     */
 }
