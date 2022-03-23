a03cb5f498765b1369a43366957fd60cb7152c49
borjaneumann <borjaneumann@hotmail.com>
2022-03-23 10:47:52 +0100

    Optional Exception List

--- 07.MethodsAndEncapsulation/src/_01_DesigningMethods/OpcionalExceptionList.java
+++
@@ -1,7 +0,0 @@
-package designingMethods;
-
-public class OpcionalExceptionList {
-    public void zeroExceptions(){}
-    public void oneException() throws IllegalArgumentException{}
-    public void twoExceptions() throws IllegalArgumentException, InterruptedException {}
-}
--- 07.MethodsAndEncapsulation/src/_01_DesigningMethods/_06_OptionalExceptionList.java
+++ 07.MethodsAndEncapsulation/src/_01_DesigningMethods/_06_OptionalExceptionList.java
@@ -1,2 +1,15 @@
-package _01_DesigningMethods;public class _06_OptionalExceptionList {
+package _01_DesigningMethods;
+
+public class _06_OptionalExceptionList {
+    /*
+    In Java, code can indicate that something went wrong by throwing an exception.
+
+    it is optional and where in the method declaration it
+    goes if present. For example, InterruptedException is a type of
+    Exception. You can list as many types of exceptions as you want in this
+    clause separated by commas.
+     */
+    public void zeroExceptions(){}
+    public void oneException() throws IllegalArgumentException{}
+    public void twoExceptions() throws IllegalArgumentException, InterruptedException {}
 }
