4d253087f5a65aed1ba0d0ccc467d4a8d9b1532e
borjaneumann <borjaneumann@hotmail.com>
2022-03-23 13:01:46 +0100

    Access Modifiers Table 2

--- 07.MethodsAndEncapsulation/src/_03_ApplyingAccessModifiers/_09_AccessModifiersTable.java
+++ 07.MethodsAndEncapsulation/src/_03_ApplyingAccessModifiers/_09_AccessModifiersTable.java
@@ -1,2 +1,13 @@
-package _03_ApplyingAccessModifiers;public class _09_AccessModifiersTable {
+package _03_ApplyingAccessModifiers;
+
+public class _09_AccessModifiersTable {
+    /*
+    A method in _________ can access            |  private | Deafault)        | protected    |   public
+    a _________ member                          |          |package-private)  |              |
+    ===============================================================================================
+    The same class                               Yes       Yes                  Yes             Yes
+    another class in the same package            No        Yes                  Yes             Yes
+    in a subclass in a different package         No        No                   Yes             Yes
+    an unrelated class in a different package    No        No                   No              Yes
+     */
 }
