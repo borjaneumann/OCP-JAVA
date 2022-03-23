404e45d616545bab10f4a61eea47b78e11096682
borjaneumann <borjaneumann@hotmail.com>
2022-03-23 10:49:58 +0100

    Method Body

--- 07.MethodsAndEncapsulation/src/_01_DesigningMethods/MethodBody.java
+++
@@ -1,7 +0,0 @@
-package designingMethods;
-
-public class MethodBody {
-    public void walk1(){};
-    public void walk2() //Does not compile
-    public void walk3(int a){int name = 5;}
-}
--- 07.MethodsAndEncapsulation/src/_01_DesigningMethods/_07_MethodBody.java
+++ 07.MethodsAndEncapsulation/src/_01_DesigningMethods/_07_MethodBody.java
@@ -1,2 +1,13 @@
-package _01_DesigningMethods;public class _07_MethodBody {
+package _01_DesigningMethods;
+
+public class _07_MethodBody {
+    /*
+    The final part of a method declaration is the method body (except for
+    abstract methods and interfaces.
+    A method body is simply a code block. It has braces that
+    contain zero or more Java statements.
+     */
+    public void walk1(){};
+//    public void walk2() //Does not compile
+    public void walk3(int a){int name = 5;}
 }
