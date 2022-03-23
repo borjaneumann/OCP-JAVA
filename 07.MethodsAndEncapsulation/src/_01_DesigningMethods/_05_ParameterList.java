d01b32b37cc732374475e4e5e99fdc53b1419810
borjaneumann <borjaneumann@hotmail.com>
2022-03-23 10:45:05 +0100

    Parameter List

--- 07.MethodsAndEncapsulation/src/_01_DesigningMethods/_05_ParameterList.java
+++ 07.MethodsAndEncapsulation/src/_01_DesigningMethods/_05_ParameterList.java
@@ -1,2 +1,17 @@
-package _01_DesigningMethods;public class _05_ParameterList {
+package _01_DesigningMethods;
+
+public class _05_ParameterList {
+    /*
+    Although the parameter list is required, it doesn’t have to contain any
+    parameters. This means you can just have an empty pair of parentheses
+    after the method name, as follows:
+
+    void nap(){}
+
+    public void walk1(){}
+    public void walk2 {} // Does not compile
+    public void walk3(int a){}
+    public void walk4 (int a;int b){} // Does not compile
+    public void walk5 (int a, int b) {}
+     */
 }
