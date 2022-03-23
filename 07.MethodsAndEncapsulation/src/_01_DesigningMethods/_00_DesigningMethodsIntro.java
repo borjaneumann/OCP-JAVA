23f202b077bbdcfa723ad8dab94027c1b5cb8df6
borjaneumann <borjaneumann@hotmail.com>
2022-03-23 09:51:21 +0100

    Designing Methods Intro

---
+++ 07.MethodsAndEncapsulation/src/_01_DesigningMethods/_00_DesigningMethodsIntro.java
@@ -0,0 +1,28 @@
+package _01_DesigningMethods;
+
+public class _00_DesigningMethodsIntro {
+    /*
+
+    public final void nap (int minutes) throws InterruptedException {
+        //take a nap
+    }
+
+    Parts of a method declaration
+    Element                         Value in nap() example          Required?
+    Access modifier                 public                          No
+    Optional specifier              final                           No
+    Return type                     void                            Yes
+    Method name                     nap                             Yes
+    Parameter list                  (int minutes)                   Yes, but can be empty parentheses
+    Optional exception list         throws InterruptedException     No
+    Method body*                    {// take a nap}                 Yes, but can be empty braces
+
+    * Body omitted for abstract methods, which we will cover in the later in the book.
+
+    To call this method, just type its name, followed by a single int value in parentheses:
+    nap(10);
+
+    Method signature = Method name + parameter list
+
+     */
+}
