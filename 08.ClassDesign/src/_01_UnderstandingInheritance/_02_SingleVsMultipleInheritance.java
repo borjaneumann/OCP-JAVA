bacfc31b1b122cfc63cf1f9bb984967386851ad4
borjaneumann <borjaneumann@hotmail.com>
2022-03-24 11:51:54 +0100

    Single Vs Multiple Inheritance

--- 08.ClassDesign/src/_01_UnderstandingInheritance/_02_SingleVsMultipleInheritance.java
+++ 08.ClassDesign/src/_01_UnderstandingInheritance/_02_SingleVsMultipleInheritance.java
@@ -1,4 +1,18 @@
 package _01_UnderstandingInheritance;
 
 public class _02_SingleVsMultipleInheritance {
+    /*
+    Java supports single inheritance, by which a class may inherit from only
+    one direct parent class. Java also supports multiple levels of inheritance,
+    by which one class may extend another class, which in turn extends
+    another class. You can have any number of levels of inheritance, allowing
+    each descendant to gain access to its ancestor’s members.
+    Each child has exactly one parent.
+
+    Preventing inheritance
+    ----------------------
+    It is possible in Java to prevent a class from being extended by marking
+    the class with the final modifier. If you try to define a class that inherits
+    from a final class, then the class will fail to compile.
+     */
 }
