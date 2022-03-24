476706eba5daca8a03c8dfa39f63f2d4a53c5b9d
borjaneumann <borjaneumann@hotmail.com>
2022-03-24 11:08:00 +0100

    Arrays

--- 07.MethodsAndEncapsulation/src/_06_OverloadingMethods/_24_Arrays.java
+++ 07.MethodsAndEncapsulation/src/_06_OverloadingMethods/_24_Arrays.java
@@ -1,4 +1,12 @@
 package _06_OverloadingMethods;
 
-public class _21_Arrays {
+public class _24_Arrays {
+    /*
+    Arrays have been around since the beginning of Java. They specify their
+    actual types and don’t participate in type erasure.
+
+    Unlike the previous example, this code is just fine:
+        public static void walk(int[] ints) {}
+        public static void walk(Integer[] integers) {}
+     */
 }
