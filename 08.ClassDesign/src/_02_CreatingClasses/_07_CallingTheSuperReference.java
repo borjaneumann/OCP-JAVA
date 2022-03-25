149bb3833d2052ec4d109abadf85b5846f1b7eec
borjaneumann <borjaneumann@hotmail.com>
2022-03-25 09:38:05 +0100

    Calling The Super Reference extended

--- 08.ClassDesign/src/_02_CreatingClasses/_07_CallingTheSuperReference.java
+++ 08.ClassDesign/src/_02_CreatingClasses/_07_CallingTheSuperReference.java
@@ -20,9 +20,20 @@ public class _07_CallingTheSuperReference {
             return super.type + ":" + this.type;
         }
         public static void main(String... zoo) {
-            System.out.print(new Bat().getType());
+            System.out.print(new Bat().getType()); //mammal:bat
         }
     }
+
+    if the super reference was dropped? The program would then print bat:bat.
+    Java uses the narrowest scope it can—in this case, the type variable
+    defined in the Bat class. Note that the this reference in the previous
+    example was optional, with the program printing the same output as it
+    would if this was dropped.
+
+    Since this includes inherited members, you often only use super when
+    you have a naming conflict via inheritance. For example, you have a
+    method or variable defined in the current class that matches a method or
+    variable in a parent class.
 
      */
 
