cb25a779d0fe4bdfdea22607cb56948378c007c1
Borja <borjaneumann@hotmail.com>
2022-04-02 22:54:02 +0200

    Interfaces: Intro and definition

---
+++ 09.AdvancedClassDesign/src/_02_ImplementingInterfaces/_05_DefiningAnInterface.java
@@ -0,0 +1,27 @@
+package _02_ImplementingInterfaces;
+
+public class _05_DefiningAnInterface {
+    /*
+    Intro and Definition
+    =======================
+    Although Java doesn’t allow multiple inheritance of state, it does allow a
+    class to implement any number of interfaces. An interface is an abstract
+    data type are that declares a list of abstract methods that any class
+    implementing the interface must provide. An interface can also include
+    constant variables. Both abstract methods and constant variables included
+    with an interface are implicitly assumed to be public.
+
+    With Java 8, interfaces were updated to include static and default methods.
+    A default method is one in which the interface method has a body
+    and is not marked abstract. It was added for backward
+    compatibility, allowing an older class to use a new version of an
+    interface that contains a new method, without having to modify the
+    existing class.
+    In Java 9, interfaces were updated to support private and private
+    static methods. Both of these types were added for code reusability
+    within an interface declaration and cannot be called outside the
+    interface definition.
+
+
+     */
+}
