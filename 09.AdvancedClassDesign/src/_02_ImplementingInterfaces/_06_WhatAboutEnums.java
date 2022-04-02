52a354f538e7fdcf3b81df6d07f12421df76705a
Borja <borjaneumann@hotmail.com>
2022-04-02 23:19:52 +0200

    Interfaces: What About Enums?

---
+++ 09.AdvancedClassDesign/src/_02_ImplementingInterfaces/_06_WhatAboutEnums.java
@@ -0,0 +1,19 @@
+package _02_ImplementingInterfaces;
+
+public class _06_WhatAboutEnums {
+    /*
+    A Java class can have at most one public top-level element, a class or interface.
+    This public top-level element could also be an enumeration, or enum for short.
+
+    An enum is a specialized type that defines a set of fixed values.
+    It is declared with the enum keyword.
+    The following demonstrates a simple example of an enum for Color:
+
+    public enum Color {
+        RED, YELOW, BLUE, GREEN, ORANGE, PURPLE
+    }
+    Like classes and interfaces, enums can have more complex
+    formations including methods, private constructors, and instance
+    variables.
+     */
+}
