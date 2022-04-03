c1b624c32791cc1c18ea4c8b03acd90e0e606bd9
Borja <borjaneumann@hotmail.com>
2022-04-03 22:54:42 +0200

    Conflicting Modifiers

--- 09.AdvancedClassDesign/src/_02_ImplementingInterfaces/_06_InsertingImplicitModifiers.java
+++ 09.AdvancedClassDesign/src/_02_ImplementingInterfaces/_06_InsertingImplicitModifiers.java
@@ -27,6 +27,27 @@ public class _06_InsertingImplicitModifiers {
         public abstract double dive();
     }
 
+    Conflicting Modifiers
+    =====================
+    When working with class members, omitting the access modifier
+    indicates default (package-private) access.
+    When working with interface members, though, the lack of access
+    modifier always indicates public access. if private or protected
+    it will not compile. Variables cannot be marked abstract.
+
+    On the exam, you are likely to get at least one question in which an
+    interface includes a member that contains an invalid modifier.
+
+    1: private final interface Crawl {
+    2:  String distance;
+    3:  private int MAXIMUM_DEPTH = 100;
+    4:  protected abstract boolean UNDERWATER = false;
+    5:  private void dig(int depth);
+    6:  protected abstract double depth();
+    7:  public final void surface(); }
+
+
+
 
 
 
