c032b136bf8e86e6401dcb7d31b8fd45a9a23f76
borjaneumann <borjaneumann@hotmail.com>
2022-04-04 10:53:02 +0200

    Polymorphism and Interfaces : Abstract Reference Types

---
+++ 09.AdvancedClassDesign/src/_02_ImplementingInterfaces/_08_PolymorphismAndInterfaces.java
@@ -0,0 +1,24 @@
+package _02_ImplementingInterfaces;
+
+public class _08_PolymorphismAndInterfaces {
+    /*
+    While many of the same rules apply, the fact that a class can inherit
+    multiple interfaces limits some of the checks the compiler can perform.
+
+    Abstract Reference Types
+    ========================
+    When working with abstract types, you may prefer to work with the
+    abstract reference types, rather than the concrete class. This is especially
+    common when defining method parameters.
+    public class Zoo {
+        public void sortAndPrintZooAnimals(List<String> animals) {
+        Collections.sort(animals);
+        for(String a : animals) {
+            System.out.println(a);
+    }}}
+
+
+
+
+     */
+}
