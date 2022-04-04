2dfdca89a29e27dc1bdca428b01e9b9b2612e177
borjaneumann <borjaneumann@hotmail.com>
2022-04-04 11:54:51 +0200

    Understanding Exceptions Intro

---
+++ 10.Exceptions/src/_01_UnderstandingExceptions/_01_UnderstandingExceptionsIntro.java
@@ -0,0 +1,15 @@
+package _01_UnderstandingExceptions;
+
+public class _01_UnderstandingExceptionsIntro {
+    /*
+    A program can fail for just about any reason. Here are just a few
+    possibilities:
+    - The code tries to connect to a website, but the Internet connection is down.
+    - You made a coding mistake and tried to access an invalid index in an array.
+    - One method calls another with a value that the method doesn’t support.
+
+    Some of these are coding mistakes. Others are completely
+    beyond your control. Your program can’t help it if the Internet connection
+    goes down. What it can do is deal with the situation.
+     */
+}
