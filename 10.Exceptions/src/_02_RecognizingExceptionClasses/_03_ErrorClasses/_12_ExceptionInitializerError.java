0c72f884e285b16909270e8a525ac566a03f8335
borjaneumann <borjaneumann@hotmail.com>
2022-04-04 14:13:59 +0200

    Error classes

--- 10.Exceptions/src/_02_RecognizingExceptionClasses/_03_ErrorClasses/_12_ExceptionInitializerError.java
+++ 10.Exceptions/src/_02_RecognizingExceptionClasses/_03_ErrorClasses/_12_ExceptionInitializerError.java
@@ -2,8 +2,11 @@ package _02_RecognizingExceptionClasses._03_ErrorClasses;
 
 public class _12_ExceptionInitializerError {
 
-    //Thrown when a static initializer throws
-    //an exception and doesn’t handle it
+    /*Thrown when a static initializer throws an exception and doesn’t handle it.
+    Java runs static initializers the first time a class is used. If one of the
+    static initializers throws an exception, Java can’t start using the class.
+     */
+
     static {
         int[] numberOfFingersInOneHand = new int[5];
         int num = numberOfFingersInOneHand[6];
--- 10.Exceptions/src/_02_RecognizingExceptionClasses/_03_ErrorClasses/_13_StackOverflowError.java
+++ 10.Exceptions/src/_02_RecognizingExceptionClasses/_03_ErrorClasses/_13_StackOverflowError.java
@@ -1,6 +1,11 @@
 package _02_RecognizingExceptionClasses._03_ErrorClasses;
 
 public class _13_StackOverflowError {
+    /*
+    Thrown when a method calls itself too many times
+    (This is called infinite recursion because the method typically calls itself
+    without end.)
+     */
     public static void doNotCodeThis(int num){
         doNotCodeThis(1);
     }
