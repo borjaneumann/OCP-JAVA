e2decf8d8d2d35749387dd97fd92d7844888f328
borjaneumann <borjaneumann@hotmail.com>
2022-04-04 12:38:56 +0200

    Declaring an exception

--- 10.Exceptions/src/_01_UnderstandingExceptions/_02_UnderstandingExceptionTypes.java
+++ 10.Exceptions/src/_01_UnderstandingExceptions/_02_UnderstandingExceptionTypes.java
@@ -20,5 +20,44 @@ public class _02_UnderstandingExceptionTypes {
     generally mean any class that inherits Throwable, although we are almost
     always working with the Exception class or subclasses of it.
 
+    Checked Exceptions
+    ==================
+    A checked exception is an exception that must be declared or handled by
+    the application code where it is thrown. In Java, checked exceptions all
+    inherit Exception but not RuntimeException. Checked exceptions tend to
+    be more anticipated—for example, trying to read a file that doesn’t exist.
+
+    Checked exceptions also include any class that inherits Throwable,
+    but not Error or RuntimeException. For example, a class that
+    directly extends Throwable would be a checked exception. For the
+    exam, though, you just need to know about checked exceptions that
+    extend Exception.
+
+    Checked exceptions? What are we checking? Java has a rule called the
+    handle or declare rule. The handle or declare rule means that all checked
+    exceptions that could be thrown within a method are either wrapped in
+    compatible try and catch blocks or declared in the method signature.
+    Because checked exceptions tend to be anticipated, Java enforces the rule
+    that the programmer must do something to show the exception was
+    thought about.
+
+    Declaring an exception
+    ======================
+    The distinction is that checked exceptions must be handled or declared,
+    while unchecked exceptions can be optionally handled or declared.
+
+    void fall(int distance) throws IOException { // the method might throw an Exception
+        if(distance > 10) {
+        throw new IOException(); // I want to throw an exception
+        }
+    }
+    Notice that you’re using two different keywords here. The throw keyword
+    tells Java that you want to throw an Exception, while the throws keyword
+    simply declares that the method might throw an Exception.
+
+
+
+
+
      */
 }
