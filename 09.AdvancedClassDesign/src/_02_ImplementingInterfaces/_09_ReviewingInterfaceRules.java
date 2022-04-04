2d8c5517283a48c8d6b843095136348c84a0582f
borjaneumann <borjaneumann@hotmail.com>
2022-04-04 11:23:58 +0200

    Polymorphism and Interfaces : USING AN INTERFACE VS. IMPLEMENTING AN INTERFACE

--- 09.AdvancedClassDesign/src/_02_ImplementingInterfaces/_09_ReviewingInterfaceRules.java
+++ 09.AdvancedClassDesign/src/_02_ImplementingInterfaces/_09_ReviewingInterfaceRules.java
@@ -51,7 +51,22 @@ public class _09_ReviewingInterfaceRules {
     implicit modifiers, do not contain constructors, do not participate in the
     instance initialization process, and support multiple inheritance.
 
-
+    USING AN INTERFACE VS. IMPLEMENTING AN INTERFACE
+    ================================================
+    An interface provides a way for one individual to develop code that
+    uses another individual’s code, without having access to the other
+    individual’s underlying implementation. Interfaces can facilitate
+    rapid application development by enabling development teams to
+    create applications in parallel, rather than being directly dependent
+    on each other.
+    For example, two teams can work together to develop a one-page
+    standard interface at the start of a project. One team then develops
+    code that uses the interface, while the other team develops code that
+    implements the interface. The development teams can then combine
+    their implementations toward the end of the project, and as long as
+    both teams developed with the same interface, they will be
+    compatible. Of course, testing will still be required to make sure that
+    the class implementing the interface behaves as expected.
 
      */
 }
