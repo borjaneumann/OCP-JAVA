53ad3f512d6e5efb1faac8fe063a1e9940e06ab1
borjaneumann <borjaneumann@hotmail.com>
2022-04-06 13:02:06 +0200

    Scope Of Try With Resources

--- 10.Exceptions/src/_03_HandlingExceptions/_19_ScopeOfTryWithResources.java
+++ 10.Exceptions/src/_03_HandlingExceptions/_19_ScopeOfTryWithResources.java
@@ -2,9 +2,14 @@ package _03_HandlingExceptions;
 
 import java.util.Scanner;
 
-public class TryWithResourcesScope {
-
+public class _19_ScopeOfTryWithResources {
     public void scanResourcesOutOfScope(){
+        /*
+        The resources created in the try clause are in scope only within the try
+        block. This is another way to remember that the implicit finally runs
+        before any catch/finally blocks that you code yourself. The implicit
+        close has run already, and the resource is no longer available.
+         */
         try (Scanner sc = new Scanner(System.in)) {
             sc.nextLine();
         } catch (Exception e) {
@@ -13,7 +18,6 @@ public class TryWithResourcesScope {
 //            sc.nextInt(); //doesn not compile, out of the scope
         }
     }
-
     //in order to be able to use it!
     public void scanResourcesInScope(){
         Scanner sc = new Scanner(System.in);
