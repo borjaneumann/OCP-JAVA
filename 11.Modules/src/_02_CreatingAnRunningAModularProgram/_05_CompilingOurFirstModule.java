b047e56a34fc5a20ae3f2f5f6e63f70e18207569
borjaneumann <borjaneumann@hotmail.com>
2022-04-07 10:45:16 +0200

    Compiling OUr First Module

---
+++ 11.Modules/src/_02_CreatingAnRunningAModularProgram/_05_CompilingOurFirstModule.java
@@ -0,0 +1,25 @@
+package _02_CreatingAnRunningAModularProgram;
+
+public class __CompilingOurFirstModule {
+    /*
+    3) Before we can run modular code, we need to compile it. Other than the
+    module-path option, this code should look familiar from Chapter 1:
+
+    javac --module-path mods
+    -d feeding
+    feeding/zoo/animal/feeding/*.java
+    feeding/module-info.java
+
+    As a review, the -d option specifies the directory to place the class files in.
+    The end of the command is a list of the .java files to compile. You can
+    list the files individually or use a wildcard for all .java files in a
+    subdirectory.
+
+
+    When you’re entering commands at the command line, they should
+    be typed all on one line. We use line breaks in the book to make the
+    commands easier to read and study. If you wanted to use multiple
+    lines at the command prompt, the approach varies by operating
+    system. Linux uses a backslash (\) as the line break.
+     */
+}
