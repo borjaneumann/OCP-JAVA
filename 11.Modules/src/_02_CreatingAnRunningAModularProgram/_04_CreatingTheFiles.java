61f09e0c687c4758e3715fb88bb6f611575de20f
borjaneumann <borjaneumann@hotmail.com>
2022-04-07 10:28:28 +0200

    Creating The Files

---
+++ 11.Modules/src/_02_CreatingAnRunningAModularProgram/_04_CreatingTheFiles.java
@@ -0,0 +1,25 @@
+package _02_CreatingAnRunningAModularProgram;
+
+public class _04_CreatingTheFiles {
+    /*
+    1) Java files and module-info.java file.
+
+    There are a few key differences between a module-info file and a regular Java class:
+    1.1) The module-info file must be in the root directory of your module.
+    Regular Java classes should be in packages.
+    1.2) The module-info file must use the keyword module instead of class,
+    interface, or enum.
+    1.3) The module name follows the naming rules for package names. It often
+    includes periods (.) in its name. Regular class and package names are not
+    allowed to have dashes (-). Module names follow the same rule.
+
+    The module-info.java file can be empty!
+
+    2) The next step is to make sure the files are in the right directory structure.
+    Also, note that we created a directory called mods at the same level as the
+    module. We will use it for storing the module artifacts a little later in the
+    chapter. This directory can be named anything, but mods is a common
+    name. If you are following along with the online code example, note that
+    the mods directory is not included, because it is empty.
+     */
+}
