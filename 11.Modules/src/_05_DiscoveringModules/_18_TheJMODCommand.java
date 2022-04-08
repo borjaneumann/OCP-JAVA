f3a0ac9fac9325d1405ecca8245b9808e6856108
borjaneumann <borjaneumann@hotmail.com>
2022-04-08 09:23:19 +0200

    The JMOD command

--- 11.Modules/src/_05_DiscoveringModules/_18_TheJMODCommand.java
+++ 11.Modules/src/_05_DiscoveringModules/_18_TheJMODCommand.java
@@ -1,2 +1,21 @@
-package _05_DiscoveringModules;public class _18_TheJMODCommand {
+package _05_DiscoveringModules;
+
+public class _18_TheJMODCommand {
+    /*
+    The most important thing to remember is that jmod is only for working
+    with the JMOD files.
+    JMOD files are recommended only when you have native libraries or something
+    that can’t go inside a JAR file. This is unlikely to affect you in the real world.
+
+    Conveniently, you don’t have to memorize the syntax for jmod.
+
+    Modes using jmod
+    Operation       Description
+    ===========================
+    create          Creates a JMOD file.
+    extract         Extracts all files from the JMOD. Works like unzipping.
+    describe        Prints the module details such as requires.
+    list            Lists all files in the JMOD file.
+    hash            Shows a long string that goes with the file
+     */
 }
