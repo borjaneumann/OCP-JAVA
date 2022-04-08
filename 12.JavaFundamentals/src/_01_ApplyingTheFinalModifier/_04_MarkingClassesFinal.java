cb76789c91de95af18218923296a1777191b4b32
borjaneumann <borjaneumann@hotmail.com>
2022-04-08 10:58:09 +0200

    Marking Classes Final

--- 12.JavaFundamentals/src/_01_ApplyingTheFinalModifier/_04_MarkingClassesFinal.java
+++ 12.JavaFundamentals/src/_01_ApplyingTheFinalModifier/_04_MarkingClassesFinal.java
@@ -1,12 +1,25 @@
 package _01_ApplyingTheFinalModifier;
 
-public class MarkingClassesFinal {
-    //A final class cannot be extended
+public class _04_MarkingClassesFinal {
+    /*
+    Lastly, the final modifier can be applied to class declarations as well.
+    A final class is one that cannot be extended.
+    For example, the following does not compile:
+
     public final class Reptile {}
-//    public class Snake extends Reptile {} //It will not compile...
 
-//    public abstract final class Eagle {} //Does not compile
+    public class Snake extends Reptile {} //It will not compile...
 
-//    public final interface Hawk {} // Does not compile. The compiler automatically applies the implicit abstract modifier to each
-    //interface declaration. Just like abstract classes, interfaces cannot bemarked final.
+    Like we saw with final methods, classes cannot be marked both
+    abstract and final. For example, the following two declarations do not
+    compile:
+
+    public abstract final class Eagle {} //Does not compile
+
+    public final interface Hawk {} // Does not compile.
+
+    The compiler automatically applies the implicit abstract modifier to each
+    interface declaration. Just like abstract classes, interfaces cannot be marked final.
+     */
+//
 }
