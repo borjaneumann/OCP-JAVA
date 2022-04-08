5a98e6a1f8d7a58cb625aecaec7242a59e4a24ad
borjaneumann <borjaneumann@hotmail.com>
2022-04-08 10:50:12 +0200

    Writing Final Methods

--- 12.JavaFundamentals/src/_01_ApplyingTheFinalModifier/_03_WritingFinalMethods.java
+++ 12.JavaFundamentals/src/_01_ApplyingTheFinalModifier/_03_WritingFinalMethods.java
@@ -1,6 +1,16 @@
 package _01_ApplyingTheFinalModifier;
 
-public class WritingFinalMethods {
+public class _03_WritingFinalMethods {
+    /*
+    Methods marked final cannot be overridden by a subclass. This
+    essentially prevents any polymorphic behavior on the method call and
+    ensures that a specific version of the method is always called.
+
+    Remember that methods can be assigned an abstract or final modifier.
+    An abstract method is one that does not define a method body and can
+    appear only inside an abstract class or interface. A final method is one
+    that cannot be overridden by a subclass.
+     */
     public abstract class Animal {
         abstract void chew();
     }
@@ -8,7 +18,7 @@ public class WritingFinalMethods {
         final void chew(){} // when we declare it final cannot be overriden.
     }
     public class PygmyHippo extends Hippo {
-//        void chew( {}) // Does not compile
+//        void chew() {} // Does not compile
     }
     abstract class ZooKeeper {
 //        public abstract final void openZoo(); //abstract and final dont get along...
