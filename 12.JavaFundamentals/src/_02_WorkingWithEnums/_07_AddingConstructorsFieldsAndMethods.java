446db08ae3a008cdbbfaf1753630a05872a71b78
Borja <borjaneumann@hotmail.com>
2022-04-09 20:36:38 +0200

    Adding constructor fields and methods to an Enum 2

--- 12.JavaFundamentals/src/_02_WorkingWithEnums/_07_AddingConstructorsFieldsAndMethods.java
+++ 12.JavaFundamentals/src/_02_WorkingWithEnums/_07_AddingConstructorsFieldsAndMethods.java
@@ -33,6 +33,76 @@ public enum _07_AddingConstructorsFieldsAndMethods {
     Notice how we don't appear to call the constructor. We just say that we
     want the enum value.
 
+    The first time that we ask for any of the enum
+    values, Java constructs all of the enum values. After that, Java just returns
+    the already constructed enum values. Given that explanation, you can see
+    why this calls the constructor only once:
+
+    public enum OnlyOne {
+        ONCE(true);
+        private OnlyOne(boolean b) {
+            System.out.print("constructing,");
+        }
+    }
+    public class PrintTheOne {
+        public static void main(String[] args) {
+            System.out.print("begin,");
+            OnlyOne firstCall = OnlyOne.ONCE; // prints constructing,
+            OnlyOne secondCall = OnlyOne.ONCE; // doesn't print anything
+            System.out.print("end");
+        }
+    }
+
+    This class prints the following: begin,constructing,end
+
+    If the OnlyOne enum was used earlier, and therefore initialized sooner,
+    then the line that declares the firstCall variable would not print
+    anything.
+    This technique of a constructor and state allows you to combine logic with
+    the benefit of a list of values. Sometimes, you want to do more. For
+    example, our zoo has different seasonal hours. It is cold and gets dark
+    early in the winter. We could keep track of the hours through instance
+    variables, or we can let each enum value manage hours itself.
+
+    public enum Season {
+        WINTER {
+            public String getHours() { return "10am-3pm"; }
+        },
+        SPRING {
+            public String getHours() { return "9am-5pm"; }
+        },
+        SUMMER {
+            public String getHours() { return "9am-7pm"; }
+        },
+        FALL {
+            public String getHours() { return "9am-5pm"; }
+        };
+        public abstract String getHours();
+    }
+    What's going on here? It looks like we created an abstract class and a
+    bunch of tiny subclasses. In a way we did. The enum itself has an
+    abstract method. This means that each and every enum value is required
+    to implement this method. If we forget to implement the method for one of
+    the values, then we get a compiler error.
+    The enum constant WINTER must implement the abstract method
+    getHours()
+    If we don't want each and every enum value to have a method, we can
+    create a default implementation and override it only for the special cases.
+
+    public enum Season {
+        WINTER {
+                public String getHours() { return "10am-3pm"; }
+        },
+        SUMMER {
+            public String getHours() { return "9am-7pm"; }
+        },
+        SPRING, FALL;
+            public String getHours() { return "9am-5pm"; }
+    }
+    This one looks better. We only code the special cases and let the others
+    use the enum‐provided implementation. Of course, overriding getHours()
+    is possible only if it is not marked final
+
     CREATING IMMUTABLE OBJECTS
     ==========================
     The immutable objects pattern is an object‐oriented design pattern in
