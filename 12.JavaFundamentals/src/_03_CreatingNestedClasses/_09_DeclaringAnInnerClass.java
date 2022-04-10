749f8df9e162b400bc4591eebe018ed464e13c28
Borja <borjaneumann@hotmail.com>
2022-04-10 22:03:41 +0200

    Declaring An Inner Class

--- 12.JavaFundamentals/src/_03_CreatingNestedClasses/_09_DeclaringAnInnerClass.java
+++ 12.JavaFundamentals/src/_03_CreatingNestedClasses/_09_DeclaringAnInnerClass.java
@@ -1,2 +1,15 @@
-package _03_CreatingNestedClasses;public class _09_DeclaringAnInnerClass {
+package _03_CreatingNestedClasses;
+
+public class _09_DeclaringAnInnerClass {
+    /*
+    An inner class, also called a member inner class, is a non‐ static type
+    defined at the member level of a class (the same level as the methods,
+    instance variables, and constructors). Inner classes have the following
+    properties:
+    1) Can be declared public, protected, package‐private (default), or private
+    2) Can extend any class and implement interfaces
+    3) Can be marked abstract or final
+    4) Cannot declare static fields or methods, except for static final fields
+    5) Can access members of the outer class including private members
+     */
 }
