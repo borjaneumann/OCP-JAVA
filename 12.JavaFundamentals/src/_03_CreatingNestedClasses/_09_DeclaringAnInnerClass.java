f43fea36781ecf1f64468d1ed6d4ca182fb4ddbd
borjaneumann <borjaneumann@hotmail.com>
2022-04-11 09:55:10 +0200

    Inner classes require an instance

--- 12.JavaFundamentals/src/_03_CreatingNestedClasses/_09_DeclaringAnInnerClass.java
+++ 12.JavaFundamentals/src/_03_CreatingNestedClasses/_09_DeclaringAnInnerClass.java
@@ -82,5 +82,32 @@ public class _09_DeclaringAnInnerClass {
     17:         c.allTheX(); // All that has to be done to call the method in the class C
     18: }}
 
+    INNER CLASSES REQUIRE AN INSTANCE
+    Take a look at the following and see whether you can figure out why
+    two of the three constructor calls do not compile:
+    public class Fox {
+        private class Den {}
+        public void goHome() {
+            new Den();
+    }
+    public static void visitFriend() {
+        new Den(); // DOES NOT COMPILE
+        }
+    }
+    public class Squirrel {
+        public void visitFox() {
+            new Den(); // DOES NOT COMPILE
+        }
+    }
+    The first constructor call compiles because goHome() is an instance
+    method, and therefore the call is associated with the this instance.
+    The second call does not compile because it is called inside a static
+    method. You can still call the constructor, but you have to explicitly
+    give it a reference to a Fox instance.
+    The last constructor call does not compile for two reasons. Even
+    though it is an instance method, it is not an instance method inside
+    the Fox class. Adding a Fox reference would not fix the problem
+    entirely, though. Den is private and not accessible in the Squirrel
+    class.
      */
 }
