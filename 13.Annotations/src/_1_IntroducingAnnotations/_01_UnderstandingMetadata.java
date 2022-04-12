1604f4b171e5cc2f8681d7a13923d1951859a9a6
borjaneumann <borjaneumann@hotmail.com>
2022-04-12 14:02:40 +0200

    Understanding Metadata

---
+++ 13.Annotations/src/_1_IntroducingAnnotations/_01_UnderstandingMetadata.java
@@ -0,0 +1,27 @@
+package _1_IntroducingAnnotations;
+
+public class _01_UnderstandingMetadata {
+    /*
+    Annotations are all about metadata.
+    Metadata is data that provides information about other data.
+
+    Example:
+    Imagine our zoo is having a sale on tickets.
+    The attribute data includes the price, the expiration date, and the number of tickets
+    purchased. In other words, the attribute data is the transactional
+    information that makes up the ticket sale and its contents.
+
+    On the other hand, the metadata includes the rules, properties, or
+    relationships surrounding the ticket sales. Patrons must buy at least one
+    ticket, as a sale of zero or negative tickets is silly. Maybe the zoo is having
+    a problem with scalpers, so they add a rule that each person can buy a
+    maximum of five tickets a day. These metadata rules describe information
+    about the ticket sale but are not part of the ticket sale.
+
+    Note:
+    While annotations allow you to insert rules around data, it does not
+    mean the values for these rules need to be defined in the code, aka
+    “hard‐coded.” In many frameworks, you can define the rules and
+    relationships in the code but read the values from elsewhere.
+     */
+}
