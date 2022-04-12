cdb3a42e9d601c962b3e80d3271537c5e1c43311
borjaneumann <borjaneumann@hotmail.com>
2022-04-12 12:28:23 +0200

    Implementing Functional Interfaces With Lambdas

---
+++ 12.JavaFundamentals/src/_05_IntroducingFunctionalProgramming/_03_ImplFunctionalInterfacesWithLambdas/_23_ImplFunctionalInterfacesWithLambdas.java
@@ -0,0 +1,17 @@
+package _05_IntroducingFunctionalProgramming._03_ImplFunctionalInterfacesWithLambdas;
+
+public class _23_ImplFunctionalInterfacesWithLambdas {
+    /*
+    <T> allows the interface to take an object of a specified type.
+    Now that we have a functional interface, we'll show
+    you how to implement it using a lambda expression.
+
+    The relationship between functional interfaces and lambda expressions
+    is as follows: any functional interface can be implemented as a lambda expression.
+
+    Lambda expressions rely on the notion of deferred execution.
+    Deferred execution means that code is specified now but runs later.
+
+
+     */
+}
