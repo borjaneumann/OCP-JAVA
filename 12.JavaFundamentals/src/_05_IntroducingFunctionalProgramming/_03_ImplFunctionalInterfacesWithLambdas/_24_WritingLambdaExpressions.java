8d43323569a622b91664a2e2276b7dcdc43fe199
borjaneumann <borjaneumann@hotmail.com>
2022-04-12 13:38:53 +0200

    Writing Lambda Expressions

---
+++ 12.JavaFundamentals/src/_05_IntroducingFunctionalProgramming/_03_ImplFunctionalInterfacesWithLambdas/_24_WritingLambdaExpressions.java
@@ -0,0 +1,7 @@
+package _05_IntroducingFunctionalProgramming._03_ImplFunctionalInterfacesWithLambdas;
+
+public class _24_WritingLambdaExpressions {
+    /*
+    Exactly as in chapter 6.
+     */
+}
