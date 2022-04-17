587d4d19c141e2aadc6bc07d4c06a659faba82de
Borja <borjaneumann@hotmail.com>
2022-04-17 21:00:23 +0200

    Working With An INOUT Parameter

--- 21.JDBC/src/_06_CallingACallableStatement/_17_WorkingWithAnINOUTParameter.java
+++ 21.JDBC/src/_06_CallingACallableStatement/_17_WorkingWithAnINOUTParameter.java
@@ -1,4 +1,29 @@
 package _06_CallingACallableStatement;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Types;
+
 public class _17_WorkingWithAnINOUTParameter {
+    /*
+   WORKING WITH AN INOUT PARAMETER
+   ===============================
+   Finally, it is possible to use the same parameter for both input and output.
+   As you read this code, see whether you can spot which lines are required
+   for the IN part and which are required for the OUT part.
+   */
+    public static void workingWithInOutParameter(Connection conn) throws SQLException {
+        var sql = "{call double_number(?)}"; //For an IN parameter is required since it passes the parameter.
+        try (var cs = conn.prepareCall(sql)) {
+            cs.setInt(1, 8); // It's required since it sets the parameter.
+            cs.registerOutParameter(1, Types.INTEGER);// It's required to register the parameter.
+            cs.execute(); //uses execute() again because we are not returning a ResultSet.
+            System.out.println(cs.getInt("num"));
+        }
+    }
+
+    /*
+    Remember that an INOUT parameter acts as both an IN parameter and an
+    OUT parameter, so it has all the requirements of both.
+     */
 }
