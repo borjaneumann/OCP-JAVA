09ffbd1e259267f517ff8538fc94f2a8fb72f403
Borja <borjaneumann@hotmail.com>
2022-04-18 20:31:38 +0200

    Comparing Callable Statement Parameters

--- 21.JDBC/src/_06_CallingACallableStatement/_18_ComparingCallableStatementParameters.java
+++ 21.JDBC/src/_06_CallingACallableStatement/_18_ComparingCallableStatementParameters.java
@@ -1,4 +1,16 @@
 package _06_CallingACallableStatement;
 
 public class _18_ComparingCallableStatementParameters {
+    /*
+    COMPARING CALLABLE STATEMENT PARAMETERS (You need to know this well for the exam.)
+    =======================================
+    Stored procedure parameter types
+                                                    IN      OUT         INOUT
+    Used for input                                  Yes     No          Yes
+    Used for output                                 No      Yes         Yes
+    Must set parameter value                        Yes     No          Yes
+    Must call registerOutParameter()                No      Yes         Yes
+    Can include ?=                                  No      Yes         Yes
+
+     */
 }
