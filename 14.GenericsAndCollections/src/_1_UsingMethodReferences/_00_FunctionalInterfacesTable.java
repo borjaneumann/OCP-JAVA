ad03b39c566b95834a8758742eadf428a7f6efec
borjaneumann <borjaneumann@hotmail.com>
2022-04-21 09:08:18 +0200

    Functional Interfaces Table

---
+++ 14.GenericsAndCollections/src/_1_UsingMethodReferences/_00_FunctionalInterfacesTable.java
@@ -0,0 +1,17 @@
+package _1_UsingMethodReferences;
+
+public class _00_FunctionalInterfacesTable {
+    /*
+    Functional interfaces       Return type     Method name     # parameters
+    =========================================================================
+    Supplier<T>                 T               get()           0
+    Consumer<T>                 void            accept(T)       1 (T)
+    BiConsumer<T, U>            void            accept(T,U)     2 (T, U)
+    Predicate<T>                boolean         test(T)         1 (T)
+    BiPredicate<T, U>           boolean         test(T, U)      2 (T, U)
+    Function<T, R>              R               apply(T)        1 (T)
+    BiFunction<T, U, R>         R               apply(T,U)      2 (T, U)
+    UnaryOperator<T>            T               apply(T)        1 (T)
+
+     */
+}
