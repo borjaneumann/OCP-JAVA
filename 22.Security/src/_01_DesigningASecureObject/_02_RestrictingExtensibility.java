1ac51c368aadfa1e18b544e0cad6afe18140a42e
borjaneumann <borjaneumann@hotmail.com>
2022-04-26 14:19:17 +0200

    Restricting Extensibility

--- 22.Security/src/_01_DesigningASecureObject/_02_RestrictingExtensibility.java
+++ 22.Security/src/_01_DesigningASecureObject/_02_RestrictingExtensibility.java
@@ -1,10 +1,6 @@
 package _01_DesigningASecureObject;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-public class _01_DesigningASecureObject {
+public class _02_RestrictingExtensibility {
 
     /*
     RESTRICTING EXTENSIBILITY
@@ -13,7 +9,7 @@ public class _01_DesigningASecureObject {
     Suppose you are working on a class that uses ComboLocks.
     */
     public static class GrasshopperCage {
-        public static void openLock(ComboLocks comboLocks, String
+        public static void openLock(_01_LimitingAccessibility.ComboLocks comboLocks, String
                 combo) {
             if (comboLocks.isComboValid("grasshopper", combo))
                 System.out.println("Open!");
@@ -31,74 +27,4 @@ public class _01_DesigningASecureObject {
 
     In this example, only the zoo.staff module can access the public classes in the animals.security package.
      */
-
-    /*
-    Java provides us with many tools to protect the objects that we create. In
-    this section, we will look at access control, extensibility, validation, and
-    creating immutable objects. All of these techniques can protect your
-    objects
-     */
-    /*
-    LIMITING ACCESSIBILITY
-    ======================
-    package animals.security;
-    public class ComboLocks {
-        public Map<String, String> combos;
-    }
-
-    This is terrible because the combos object has public access. This is also
-    poor encapsulation. A key security principle is to limit access as much as
-    possible. Think of it as “need to know” for objects.
-    This is called the principle of least privilege.
-
-    It would be better to make the combos object
-    private and write a method to provide the necessary functionality.
-
-     package animals.security;
-    */
-    public class ComboLocks {
-        private Map<String, String> combos;
-
-        public boolean isComboValid(String animal, String combo) {
-            var correctCombo = combos.get(animal);
-            return combo.equals(correctCombo);
-        }
-    }
-
-    /*
-    Ideally, the first variable passed to this method is an instance of the
-    ComboLocks class. However, Hacker Harry is hard at work and has created
-    this subclass of ComboLocks.
-    */
-    public class EvilComboLocks extends ComboLocks {
-        public boolean isComboValid(String animal, String combo) {
-            var valid = super.isComboValid(animal, combo);
-            if (valid) {
-                // email the password to Hacker Harry
-            }
-            return valid;
-        }
-    }
-    /*
-    Marking a sensitive class as final prevents any subclasses.
-
-     */
-
-    public final class ComboLocksWithFinal {
-        private Map<String, String> combos;
-
-        // instantiate combos object
-        public boolean isComboValid(String animal, String combo) {
-            var correctCombo = combos.get(animal);
-            return combo.equals(correctCombo);
-        }
-    }
-    /*
-    Hacker Harry can't create his evil class, and users of the GrasshopperCage have the assurance
-    that only the expected ComboLocks class can make an appearance.
-     */
-
-    
-
-
 }
