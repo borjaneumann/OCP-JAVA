42f5db115b6d57c1a941c351ea4d0693af46c17e
borjaneumann <borjaneumann@hotmail.com>
2022-04-28 14:39:56 +0200

    Reviewing Serialization Methods

--- 22.Security/src/_04_SerializingAndDeserializingObjects/_13_ReviewingSerializationMethods.java
+++ 22.Security/src/_04_SerializingAndDeserializingObjects/_13_ReviewingSerializationMethods.java
@@ -1,2 +1,15 @@
-package _04_SerializingAndDeserializingObjects;public class _13_ReviewingSerializationMethods {
+package _04_SerializingAndDeserializingObjects;
+
+public class _13_ReviewingSerializationMethods {
+    /*
+    REVIEWING SERIALIZATION METHODS
+    ===============================
+    Methods for serialization and deserialization
+    Return type     Method              Parameters          Description
+    ========================================================================================================
+    Object          writeReplace()      None                Allows replacement of object before serialization
+    void            writeObject()       ObjectInputStream   Serializes optionally using PutField
+    void            readObject()        ObjectOutputStream  Deserializes optionally using GetField
+    Object          readResolve()       None                Allows replacement of object after deserialization
+     */
 }
