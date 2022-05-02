90ca495bb77988a16166c549f9ad6f4ed979f2bd
borjaneumann <borjaneumann@hotmail.com>
2022-05-02 08:57:47 +0200

    Reading Very Large Resources

--- 22.Security/src/_06_PreventingDenialOfServiceAttacks/_18_ReadingVeryLargeResources.java
+++ 22.Security/src/_06_PreventingDenialOfServiceAttacks/_18_ReadingVeryLargeResources.java
@@ -1,2 +1,26 @@
-package _06_PreventingDenialOfServiceAttacks;public class _18_ReadingVeryLargeResources {
+package _06_PreventingDenialOfServiceAttacks;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class _18_ReadingVeryLargeResources {
+    /*
+    READING VERY LARGE RESOURCES
+    ============================
+    Another source of a denial of service attacks is very large resources.
+    Suppose we have a simple method that reads a file into memory, does
+    some transformations on it, and writes it to a new file.
+     */
+    public void transform(Path in, Path out) throws IOException {
+        var list = Files.readAllLines(in);
+        list.removeIf(s -> s.trim().isBlank());
+        Files.write (out, list);
+    }
+    /*
+    On a small file, this works just fine. However, on an extremely large file,
+    your program could run out of memory and crash. Hacker Harry strikes
+    again! To prevent this problem, you can check the size of the file before
+    reading it.
+     */
 }
