583f41ac72ac237892b57a6278c79854e03c96ba
Borja <borjaneumann@hotmail.com>
2022-05-13 20:10:59 +0200

    Introducing The File Class

--- 19.I_O/src/_01_UnderstandingFilesAndDirectories/_03_IntroducingTheFileClass.java
+++ 19.I_O/src/_01_UnderstandingFilesAndDirectories/_03_IntroducingTheFileClass.java
@@ -1,3 +1,5 @@
+package _01_UnderstandingFilesAndDirectories;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -12,11 +14,34 @@ public class _03_IntroducingTheFileClass {
     classes to read or write data, as you will see in the next section.
 
     Remember, a File instance can represent a file or a directory.
-     */
 
-    //For convenience, Java offers two options to
-    //retrieve the local separator character: a system property and a static
-    //variable defined in the File class.
+    Creating a File Object
+    ======================
+    A File object often is initialized with a String containing either an
+    absolute or relative path to the file or directory within the file system. The
+    absolute path of a file or directory is the full path from the root directory
+    to the file or directory, including all subdirectories that contain the file or
+    directory. Alternatively, the relative path of a file or directory is the path
+    from the current working directory to the file or directory. For example,
+    the following is an absolute path to the stripes.txt file:
+
+    /home/tiger/data/stripes.txt
+
+    The following is a relative path to the same file, assuming the user's
+    current directory is set to /home/tiger:
+
+    data/stripes.txt
+
+    Different operating systems vary in their format of pathnames. For
+    example, Unix‐based systems use the forward slash, /, for paths, whereas
+    Windows‐based systems use the backslash, \, character. That said, many
+    programming languages and file systems support both types of slashes
+    when writing path statements.
+
+    For convenience, Java offers two options to
+    retrieve the local separator character: a system property and a static
+    variable defined in the File class.*/
+
     public static void main(String[] args) throws IOException {
         System.out.println(System.getProperty("file.separator")); // \
         System.out.println(File.separator); // \
@@ -34,7 +59,10 @@ public class _03_IntroducingTheFileClass {
     File parent = new File("/home/tiger"); //This one and the one below work together.
     File zooFile4 = new File(parent, "data/stripes.txt");
 
-    /* File Methods
+    /*The File Object vs. the Actual File
+    =====================================
+
+    File Methods
     Method Name                 Description
     ====================================================================================================
     boolean delete()            Deletes the file or directory and returns true only if successful.
@@ -54,8 +82,12 @@ public class _03_IntroducingTheFileClass {
     boolean mkdirs()            Creates the directory named by this path including any nonexistent parent directories
     boolean renameTo(File dest) Renames the file or directory denoted by this path to dest and returns true only if successful
 
+    The following is a sample program that given a file path outputs
+    information about the file or directory, such as whether it exists, what files
+    are contained within it, and so forth:
+
      */
-        var file = new File("C:\\Users\\Borja Neumann\\Desktop\\Home\\prueba.txt");
+        var file = new File("C:\\Users\\Neu\\Desktop\\Home\\prueba.txt");
         System.out.println(("File Exists: " + file.exists()));
         if (file.exists()) {
             System.out.println("Absolute Path: " + file.getAbsolutePath());
@@ -70,4 +102,40 @@ public class _03_IntroducingTheFileClass {
             }
         }
     }
+    /*
+    If the path provided did not point to a file, it would output the following:
+
+    File Exists: false
+    If the path provided pointed to a valid file, it would output something
+    similar to the following:
+    File Exists: true
+    Absolute Path: c:\data\zoo.txt
+    Is Directory: false
+    Parent Path: c:\data
+    Size: 12382
+    Last Modified: 1606860000000
+
+    Finally, if the path provided pointed to a valid directory, such as c:\data,
+    it would output something similar to the following:
+
+    File Exists: true
+    Absolute Path: c:\data
+    Is Directory: true
+    Parent Path: c:\
+    employees.txt
+    zoo.txt
+    zoo-backup.txt
+
+    In these examples, you see that the output of an I/O‐based program is
+    completely dependent on the directories and files available at runtime in
+    the underlying file system.
+    On the exam, you might get paths that look like files but are directories, or
+    vice versa. For example, /data/zoo.txt could be a file or a directory,
+    even though it has a file extension. Don't assume it is either unless the
+    question tells you it is!
+
+    In the previous example, we used two backslashes ( \\) in the path
+    String, such as c:\\data\\zoo.txt. When the compiler sees a \\
+    inside a String expression, it interprets it as a single \ value.
+     */
 }
