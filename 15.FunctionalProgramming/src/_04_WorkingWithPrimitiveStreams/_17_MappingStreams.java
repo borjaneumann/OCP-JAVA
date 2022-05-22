e81537c2c83a9ad9ffdc8be445a2daf0d6113238
Borja <borjaneumann@hotmail.com>
2022-05-22 20:06:14 +0200

    Mapping Streams

--- 15.FunctionalProgramming/src/_04_WorkingWithPrimitiveStreams/_17_MappingStreams.java
+++ 15.FunctionalProgramming/src/_04_WorkingWithPrimitiveStreams/_17_MappingStreams.java
@@ -1,15 +1,19 @@
 package _04_WorkingWithPrimitiveStreams;
 
+import java.util.ArrayList;
+import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
-public class _18_MappingStreams {
+public class _17_MappingStreams {
     //Another way to create a primitive stream is by mapping from another
     //stream type
 
     public static void main(String[] args) {
         //Mapping methods between types of streams
         /*Source stream class       To create Stream To Create DoubleStream To create IntStream To create LongStream
+        ============================================================================================================
         Stream<T>                   map()            mapToDouble()          mapToInt()          mapToLong()
         DoubleStream                mapToObject()    map()                  mapToInt()          mapToLong()
         IntStream                   mapToObject()    mapToDouble()          map()               mapToLong()
@@ -30,6 +34,7 @@ public class _18_MappingStreams {
 
         //Function parameters when mapping between types of streams
         /*Source stream class       To create Stream To Create DoubleStream To create IntStream To create LongStream
+        ============================================================================================================
         Stream<T>                   Function<T,R>     ToDoubleFunction<T>   ToIntFunction<T>    ToLongFunction<T>
         DoubleStream                DoubleFunction<R> DoubleUnaryOperator   DoubleToIntFunction DoubleToLongFunction
         IntStream                   IntFunction<R>    IntToDoubleFunction   IntUnaryOperator    IntToLongFunction
@@ -41,6 +46,32 @@ public class _18_MappingStreams {
         instead of a function for the primitive streams.
          */
 
+
+    /*The flatMap() method exists on primitive streams as well. It works
+    the same way as on a regular Stream except the method name is
+    different. Here's an example:*/
+
+        var integerList = new ArrayList<Integer>();
+        IntStream ints = integerList.stream()
+                .flatMapToInt(x -> IntStream.of(x));
+        DoubleStream doubles = integerList.stream()
+                .flatMapToDouble(x -> DoubleStream.of(x));
+        LongStream longs = integerList.stream()
+                .flatMapToLong(x -> LongStream.of(x));
+
+    }
+    //Additionally, you can create a Stream from a primitive stream.
+    //These methods show two ways of accomplishing this:
+
+    private static Stream<Integer> mapping (IntStream stream) {
+        return stream.mapToObj(x -> x);
+    }
+    private static Stream<Integer> boxing(IntStream stream) {
+        return stream.boxed();
     }
 
+    /*The first one uses the mapToObj() method we saw earlier. The second one
+    is more succinct. It does not require a mapping function because all it does
+    is autobox each primitive to the corresponding wrapper object. The
+    boxed() method exists on all three types of primitive streams.*/
 }
