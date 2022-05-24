99acf36800dcfc76bc52e9b6e761e5e04f606ace
borjaneumann <borjaneumann@hotmail.com>
2022-05-24 13:48:05 +0200

    Putting Together The Pipeline

--- 15.FunctionalProgramming/src/_03_UsingStreams/_14_PuttingTogetherThePipeline.java
+++ 15.FunctionalProgramming/src/_03_UsingStreams/_14_PuttingTogetherThePipeline.java
@@ -1,3 +1,5 @@
+package _03_UsingStreams;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -85,6 +87,31 @@ public class _14_PuttingTogetherThePipeline {
         long count1 = helper.stream().count();
         System.out.println(count1);
 
+        var infinite = Stream.iterate(1, x -> x + 1);
+        infinite.limit(5)
+                .filter(x -> x % 2 == 1)
+                .forEach(System.out::println);// 135 (1, 3, 5)
 
+        //Now what do you think this prints?
+        var infinite2 = Stream.iterate(1, x -> x + 1);
+        infinite2.limit(5)
+                .peek(System.out::print)
+                .filter(x -> x % 2 == 1)
+                .forEach(System.out::print); //11233455
+
+        //Reversing the order of the intermediate operations changes the result.
+        System.out.println();
+        var infinite3 = Stream.iterate(1, x -> x + 1);
+        infinite3.filter(x -> x % 2 == 1)
+                .limit(5)
+                .forEach(System.out::print); // 13579
+
+        //Finally, what do you think this prints?
+        System.out.println();
+        var infinite4 = Stream.iterate(1, x-> x + 1);
+        infinite4.filter(x -> x % 2 == 1)
+                .peek(System.out::print)
+                .limit(5)
+                .forEach(System.out::print);// 1133557799.
     }
 }
