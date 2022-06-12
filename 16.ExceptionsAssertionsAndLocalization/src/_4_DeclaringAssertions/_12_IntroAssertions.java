e3d6e31d94f6d633a9ce4863271e20269a3af5f0
borjaneumann <borjaneumann@hotmail.com>
2022-06-12 10:43:50 +0200

    Intro Assertions

--- 16.ExceptionsAssertionsAndLocalization/src/_4_DeclaringAssertions/_12_IntroAssertions.java
+++ 16.ExceptionsAssertionsAndLocalization/src/_4_DeclaringAssertions/_12_IntroAssertions.java
@@ -1,19 +1,19 @@
 package _4_DeclaringAssertions;
 
 public class _12_IntroAssertions {
-    //An assertion is a boolean expression that you place at a point in your code
-    //where you expect something to be true. An assert statement contains this
-    //statement along with an optional message.
+    /*An assertion is a boolean expression that you place at a point in your code
+    where you expect something to be true. An assert statement contains this
+    statement along with an optional message.
 
     //Syntax
 
-//    1. assert test_value; test_value -> boolean expression
-//    2. assert test_value: message; -> message is optional
+    1. assert test_value; test_value -> boolean expression
+    2. assert test_value: message; -> message is optional
 
-    //When assertions are enabled and the boolean expression evaluates to
-    //false, then an AssertionError will be thrown at runtime. Since programs
-    //aren't supposed to catch an Error, this means that assertion failures are
-    //fatal and end the program!
+    When assertions are enabled and the boolean expression evaluates to
+    false, then an AssertionError will be thrown at runtime. Since programs
+    aren't supposed to catch an Error, this means that assertion failures are
+    fatal and end the program!*/
     public static void main(String[] args) {
         int age = 2;
         int height = 1;
@@ -27,11 +27,11 @@ public class _12_IntroAssertions {
     }
 
     /*
-    * If assertions are disabled, Java skips the assertion and goes on in the code.
-    * If assertions are enabled and the boolean expression is true, then our
+    - If assertions are disabled, Java skips the assertion and goes on in the code.
+    - If assertions are enabled and the boolean expression is true, then our
     assertion has been validated and nothing happens. The program continues
     to execute in its normal manner.
-    * If assertions are enabled and the boolean expression is false, then our
+    - If assertions are enabled and the boolean expression is false, then our
     assertion is invalid and an AssertionError is thrown.
 
     Presuming assertions are enabled, an assertion is a shorter way of writing
