f780caed7a9e5544b2b6805f8d8c0f32a2d83d55
borjaneumann <borjaneumann@hotmail.com>
2022-06-14 08:38:27 +0200

    Applying Assertions

--- 16.ExceptionsAssertionsAndLocalization/src/_4_DeclaringAssertions/_14_ApplyingAssertions.java
+++ 16.ExceptionsAssertionsAndLocalization/src/_4_DeclaringAssertions/_14_ApplyingAssertions.java
@@ -1,2 +1,30 @@
-package _4_DeclaringAssertions;public class _14_ApplyingAssertions {
+package _4_DeclaringAssertions;
+
+public class _14_ApplyingAssertions {
+    /*
+    You won't be asked to identify the type of assertion on the exam.
+    This is just to give you some ideas of how they can be used.
+
+    Assertion applications
+    Usage               Description
+    ===============================================================================
+    Internal            Assert that a value is within a certain constraint, such as
+    invariants          assert x < 0.
+    --------------------------------------------------------------------------------
+    Class               Assert the validity of an object's state. Class invariants are
+    invariants          typically private methods within the class that return a boolean.
+    --------------------------------------------------------------------------------
+    Control             Assert that a line of code you assume is unreachable is
+    flow                never reached.
+    invariants
+    --------------------------------------------------------------------------------
+    Pre‐                Assert that certain conditions are met before a method is
+    conditions          invoked.
+    --------------------------------------------------------------------------------
+    Post‐               Assert that certain conditions are met after a method
+    conditions          executes successfully.
+
+
+
+     */
 }
