d23ed79ba4c9fd42d92333e70ac2bca5e1e9d614
Borja <borjaneumann@hotmail.com>
2022-06-19 09:44:01 +0200

    Automatic Modules

--- 17.ModularApplications/src/_2_ComparingTypesOfModules/_04_AutomaticModules.java
+++ 17.ModularApplications/src/_2_ComparingTypesOfModules/_04_AutomaticModules.java
@@ -1,4 +1,11 @@
 package _2_ComparingTypesOfModules;
 
-public class AutomaticModules {
+public class _04_AutomaticModules {
+    /*
+    An automatic module appears on the module path but does not contain a
+    module‐info file. It is simply a regular JAR file that is placed on the
+    module path and gets treated as a module.
+    As a way of remembering this, Java automatically determines the module
+    name.
+     */
 }
