8aa28519f047cd8a64dc38bb5b96eaf78f5d51fc
borjaneumann <borjaneumann@hotmail.com>
2022-06-27 10:06:13 +0200

    Splitting A Big Project Into Modules

--- 17.ModularApplications/src/_4_MigratingAnApplication/_12_SplittingABigProjectIntoModules.java
+++ 17.ModularApplications/src/_4_MigratingAnApplication/_12_SplittingABigProjectIntoModules.java
@@ -1,4 +1,17 @@
 package _4_MigratingAnApplication;
 
-public class SplittingABigProjectIntoModules {
+public class _12_SplittingABigProjectIntoModules {
+    /*
+    For the exam, you need to understand the basic process of splitting up a
+    big project into modules.
+
+    1) Suppose you start with an application that has a number of packages. The
+    first step is to break them up into logical groupings and draw the
+    dependencies between them.
+
+    The Java Platform Module System does not allow for cyclic dependencies.
+    A cyclic dependency, or circular dependency, is when two things directly or
+    indirectly depend on each other. A common fixing technique is to introduce another
+    module. That module contains the code that the other two modules share.
+     */
 }
