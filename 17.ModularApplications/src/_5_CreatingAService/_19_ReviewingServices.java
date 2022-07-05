3086cb7de650dd117f159df7ef8a907fedf76bf0
borjaneumann <borjaneumann@hotmail.com>
2022-07-05 08:42:21 +0200

    Reviewing Services

--- 17.ModularApplications/src/_5_CreatingAService/_19_ReviewingServices.java
+++ 17.ModularApplications/src/_5_CreatingAService/_19_ReviewingServices.java
@@ -1,4 +1,13 @@
-package _5_CreatingAService.zoo;
+package _5_CreatingAService;
 
 public class _19_ReviewingServices {
+
+        /*
+    Artifact                        Part of the service         Directives required in module‐info.java
+    =====================================================================================================
+    Service provider interface      Yes                         exports
+    Service provider                No                          requires provides
+    Service locator                 Yes                         exports  requires  uses
+    Consumer                        No                          requires
+     */
 }
