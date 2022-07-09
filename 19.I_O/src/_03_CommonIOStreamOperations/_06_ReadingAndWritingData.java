531a0eab6780c9fa05fbb7d14e2a6f0bbb3e0a17
Borja <borjaneumann@hotmail.com>
2022-07-09 21:57:48 +0200

    Reading And Writing Data

--- 19.I_O/src/_03_CommonIOStreamOperations/_06_ReadingAndWritingData.java
+++ 19.I_O/src/_03_CommonIOStreamOperations/_06_ReadingAndWritingData.java
@@ -1,2 +1,58 @@
-package _03_CommonIOStreamOperations;public class _06_ReadingAndWritingData {
+package _03_CommonIOStreamOperations;
+
+import java.io.*;
+
+public class _06_ReadingAndWritingData {
+    public static void main(String[] args) {
+
+    /*READING AND WRITING DATA
+    ================================================
+    InputStream and Reader
+    public int read() throws IOException
+    OutputStream and Writer
+    public void write(int b) throws IOException*/
+
+    }
+    /*The following copyStream() methods show an example of reading all of
+        the values of an InputStream and Reader and writing them to an
+        OutputStream and Writer, respectively. In both examples, ‐1 is used to
+        indicate the end of the stream.*/
+
+    void copyStream(InputStream in, OutputStream out) throws IOException {
+        int b;
+        while ((b = in.read()) != -1) {
+            out.write(b);
+        }
+    }
+    void copyStream(Reader in, Writer out) throws IOException {
+        int b;
+        while ((b = in.read()) != -1) {
+            out.write(b);
+        }
+    }
+
+    //Most I/O stream methods declare a checked IOException.
+    //The byte stream classes also include overloaded methods for reading and
+    //writing multiple bytes at a time.
+
+    // InputStream
+    //public int read(byte[] b) throws IOException
+    //public int read(byte[] b, int offset, int length) throws IOException
+    // OutputStream
+    //public void write(byte[] b) throws IOException
+    //public void write(byte[] b, int offset, int length) throws IOException
+
+    //The offset and length are applied to the array itself. For example, an
+    //offset of 5 and length of 3 indicates that the stream should read up to 3
+    //bytes of data and put them into the array starting with position 5.
+
+    //equivalent methods for the character stream classes that use
+    //char instead of byte.
+
+    // Reader
+    //public int read(char[] c) throws IOException
+    //public int read(char[] c, int offset, int length) throws IOException
+    // Writer
+    //public void write(char[] c) throws IOException
+    //public void write(char[] c, int offset, int length) throws IOException
 }
