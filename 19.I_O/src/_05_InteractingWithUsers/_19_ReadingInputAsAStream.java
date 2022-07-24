e2f61b106f7973f193308e183ddfee200c6246c7
Borja <borjaneumann@hotmail.com>
2022-07-24 09:57:27 +0200

    Reading Input As A Stream

--- 19.I_O/src/_05_InteractingWithUsers/_19_ReadingInputAsAStream.java
+++ 19.I_O/src/_05_InteractingWithUsers/_19_ReadingInputAsAStream.java
@@ -1,2 +1,19 @@
-package _05_InteractingWithUsers;public class _19_ReadingInputAsAStream {
+package _05_InteractingWithUsers;
+
+public class _19_ReadingInputAsAStream {
+    /*
+    The System.in returns an InputStream and is used to retrieve text input
+    from the user. It is commonly wrapped with a BufferedReader via an
+    InputStreamReader to use the readLine() method.
+
+    var reader = new BufferedReader(new
+    InputStreamReader(System.in));
+    String userInput = reader.readLine();
+    System.out.println("You entered: " + userInput);
+
+    When executed, this application first fetches text from the user until the
+    user presses the Enter key. It then outputs the text the user entered to the
+    screen.
+
+     */
 }
