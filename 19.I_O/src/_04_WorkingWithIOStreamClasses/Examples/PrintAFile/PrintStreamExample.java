5f72b0d39802745a55af8273378504d4cec8d843
borjaneumann <borjaneumann@hotmail.com>
2022-07-29 09:37:02 +0200

    PrintStream Example update

--- 19.I_O/src/_04_WorkingWithIOStreamClasses/Examples/PrintAFile/PrintStreamExample.java
+++ 19.I_O/src/_04_WorkingWithIOStreamClasses/Examples/PrintAFile/PrintStreamExample.java
@@ -1,15 +1,16 @@
 package _04_WorkingWithIOStreamClasses.Examples.PrintAFile;
 
 import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.io.PrintWriter;
 
-public class PrintWriteExanple {
+public class PrintStreamExample {
     public static void main(String[] args) {
         String[] names = { "Rob", "John", "Mark"};
-        try( PrintWriter printWriter = new PrintWriter("printWriter.txt")){
-            printWriter.println("This is some text for printWriter");
+        try( PrintStream printStream = new PrintStream("printStream.txt")){
+            printStream.println("This is some text for printStream");
             for (String name : names) {
-                printWriter.printf("Hello: " + name +"\n");
+                printStream.printf("Hello: " + name +"\n");
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
