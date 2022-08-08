af589c1ab60f9e61e3340887b0e654aafeb3411a
borjaneumann <borjaneumann@hotmail.com>
2022-08-08 09:32:29 +0200

    Cpu intensive Task

--- 18.Concurrency/src/_02_CreatingThreadsWithTheConcurrencyAPI/Examples/CpuIntensiveTask.java
+++ 18.Concurrency/src/_02_CreatingThreadsWithTheConcurrencyAPI/Examples/CpuIntensiveTask.java
@@ -1,2 +1,22 @@
-package _02_CreatingThreadsWithTheConcurrencyAPI.Examples;public class CpuIntensiveTask {
+package _02_CreatingThreadsWithTheConcurrencyAPI.Examples;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CpuIntensiveTask implements Runnable{
+    public static void main(String[] args) {
+        // get count of available cores
+        int coreCount = Runtime.getRuntime().availableProcessors();
+        ExecutorService service = Executors.newFixedThreadPool(coreCount);
+        // submit the tasks for execution
+        for (int i = 0; i < 100; i++) {
+            service.execute(new CpuIntensiveTask());
+        }
+
+    }
+
+    @Override
+    public void run() {
+        //some CPU intensive operations
+    }
 }
